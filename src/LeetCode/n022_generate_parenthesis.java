88a05a3764bba86a2fff3c0628f6db0fcd01de58
C3vin <spacemonkey3@gmail.com>
2016-07-31 16:57:24 -0700

    add n22

---
+++ src/LeetCode/n022_generate_parenthesis.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class n022_generate_parenthesis {
+	public List<String> generateParenthesis(int n) {
+		
+	    ArrayList<String> result = new ArrayList<String>();
+	    dfs(result, "", n, n);
+	    return result;
+	}
+
+	public void dfs(ArrayList<String> result, String s, int left, int right) {
+		if(left > right) {//why > not < 
+			//System.out.println("l:" + left + " r: " + right);
+			return;
+		}
+		if(left == 0 && right == 0) {
+			result.add(s);
+			System.out.println(result);
+			return; 	//why
+		}
+		
+		if(left > 0) {
+			dfs(result, s+"(", left-1, right);
+		}
+		
+		if(right > 0) {
+			dfs(result, s+")", left, right-1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		n022_generate_parenthesis obj = new n022_generate_parenthesis();
+		obj.generateParenthesis(3);
+	}
+}
