3d9bf78b924b65bb30d133e6292b0bb9278e2508
root <root@n15-214-133-h205.arst.usa.hp.com>
2016-08-02 15:49:14 -0700

    added n005 DP

--- src/LeetCode/n005_longest_palindromic_substring.java
+++ src/LeetCode/n005_longest_palindromic_substring.java
@@ -14,14 +14,14 @@ public class n005_longest_palindromic_substring {
 	        if(i%2 == 1) { 
 	            right++;  
 	        }
-	        System.out.println("left: " + left + " right: " + right);
+	        //System.out.println("left: " + left + " right: " + right);
 	        String str = lengthOfPalindrome(s,left,right);  
-	        System.out.println("str: " + str);
+	        //System.out.println("str: " + str);
 	        if(maxLen < str.length())  
 	        {  
 	        	//System.out.println("maxLen1: " + maxLen);
 	            maxLen = str.length();  
-	            System.out.println("maxLen2: " + maxLen + " str: " + str);
+	            //System.out.println("maxLen2: " + maxLen + " str: " + str);
 	            res = str;  
 	        }  
 	    }  
@@ -30,16 +30,43 @@ public class n005_longest_palindromic_substring {
 	}  
 	private String lengthOfPalindrome(String s, int left, int right) {  
 	    while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)) {  
-	    	System.out.println("charAt(left): " + s.charAt(left) + " charAt(right): " + s.charAt(right));
+	    	//System.out.println("charAt(left): " + s.charAt(left) + " charAt(right): " + s.charAt(right));
 	    	left--;  
 	        right++;  
-	        System.out.println("#left: " + left + " #right: " + right);
+	        //System.out.println("#left: " + left + " #right: " + right);
 	    }  
 	    return s.substring(left+1,right);  
+	} 
+	
+	public String longestPalindromeDP(String s) {  
+	    if(s == null || s.length()==0)  
+	        return "";  
+	    
+	    boolean[][] palin = new boolean[s.length()][s.length()];  
+	    String res = "";  
+	    int maxLen = 0;  
+	    
+	    // i(<---) | j(--->)
+	    for(int i=s.length()-1; i>=0; i--) {  
+	        for(int j=i; j<s.length(); j++) {  
+	            if(s.charAt(i) == s.charAt(j) && (j-i<=2 || palin[i+1][j-1])) {  
+	            	System.out.println("charAt(left): " + s.charAt(i) + " charAt(right): " + s.charAt(j) + " i: " + i + " j: " + j);
+	            	palin[i][j] = true;  
+	                if(maxLen < j-i+1) {  
+	                	System.out.println("@: " + palin[i][j]);
+	                    maxLen = j-i+1;  
+	                    res = s.substring(i,j+1);  
+	                    System.out.println("maxLen: " + maxLen + " res: " + res);
+	                }  
+	            }  
+	        }  
+	    }  
+	    return res;  
 	} 
 	
 	public static void main(String[] args) {
 		n005_longest_palindromic_substring obj = new n005_longest_palindromic_substring();
 		System.out.println(obj.longestPalindrome("abba"));
+		System.out.println(obj.longestPalindromeDP("abba"));
 	}
 }
