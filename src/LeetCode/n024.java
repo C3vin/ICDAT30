f1d39317dc67d327103dfc74326d8d589695a5d2
root <root@n15-214-133-h205.arst.usa.hp.com>
2016-08-22 11:15:04 -0700

    add n24 with commenrt

---
+++ src/LeetCode/n024.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+public class n024 {
+	public ListNode swapPairs(ListNode head) {
+	      if(head == null || head.next == null)
+	        return head;
+	    
+	      ListNode fakehead = new ListNode(-1);
+	      fakehead.next = head;
+	      
+	      ListNode ptr1 = fakehead;
+	      ListNode ptr2 = head;
+	      
+	      while(ptr2!=null && ptr2.next!=null){
+	          ListNode nextstart = ptr2.next.next;
+	          ptr2.next.next = ptr2;	//2->1
+	          ptr1.next = ptr2.next;	//0->2
+	          ptr2.next = nextstart;	//1->3
+	          
+	          ptr1 = ptr2;
+	          ptr2 = ptr2.next;
+	      }
+	    return fakehead.next;
+	  }
+	public static void main(String[] args) {
+		n024 obj = new n024();
+		System.out.println(obj.swapPairs(ListNode.create(1234)));
+	}
+}
