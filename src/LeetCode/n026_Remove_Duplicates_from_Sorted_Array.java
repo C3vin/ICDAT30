7efb58c7e0d2af67c765c5bf784c61840a695c03
C3vin <spacemonkey3@gmail.com>
2016-08-24 01:18:40 -0700

    add n26 need to fix

--- src/LeetCode/n026_Remove_Duplicates_from_Sorted_Array.java
+++ src/LeetCode/n026_Remove_Duplicates_from_Sorted_Array.java
@@ -1,5 +1,31 @@
 package LeetCode;
 
-public class n026_Remove_Duplicates_from_Sorted_Array {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class n026_Remove_Duplicates_from_Sorted_Array {
+	public int removeDuplicates(int[] nums) {
+		if(nums.length == 0 || nums == null) return 0;
+		if(nums.length < 2) return nums.length;
+
+		int count = 1;
+		for(int i=1; i<nums.length; i++) {
+			//TODO: change 
+			if(nums[i] != nums[i-1]) {
+				if(count != i)
+					nums[i-1] = nums[i];
+				count++;
+			}
+		}
+		System.out.print(Arrays.toString(nums));
+
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		n026_Remove_Duplicates_from_Sorted_Array obj = new n026_Remove_Duplicates_from_Sorted_Array();
+		int[] nums = {1,1,1,2}; // test {1,2}
+		System.out.println("\nCount: "+obj.removeDuplicates(nums));
+	}
 }
