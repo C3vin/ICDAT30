c1f83217b3de281b2a66e4efdd88ab00562e71ff
C3vin <spacemonkey3@gmail.com>
2016-08-25 02:00:31 -0700

    update n28 BM

--- src/LeetCode/n028_Implement_strStr.java
+++ src/LeetCode/n028_Implement_strStr.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 public class n028_Implement_strStr {
+	//Brute Force
 	public int strStr(String haystack, String needle) {
         if(haystack == null || needle == null) return -1;
 		if(needle.length() > haystack.length()) return -1;
@@ -8,17 +9,45 @@ public class n028_Implement_strStr {
 		
 		for(int i=0; i<=haystack.length() - needle.length(); i++) {
 			int j=0;
-			while(j<needle.length() && haystack.charAt(i+j) == needle.charAt(j)) {
+			while(j < needle.length() && haystack.charAt(i+j) == needle.charAt(j)) {
 				j++;
+				System.out.println("j: " +j);
 			}
-			if(j == needle.length()) return i;
+			//System.out.println("j: " +j);
+			if(j == needle.length()) 
+				return i;
 		}
-		
 		return -1;
 	}
 
+	//BM (Boyer-Moore)
+	public int strStrBM(String haystack, String needle) {
+	    int hlen = haystack.length();
+	    int nlen = needle.length();
+	    int[] jump = new int[256];  // hashmap char-> index, assume ASCII
+	    for(int i=0; i<jump.length; i++) {
+	        jump[i]=-1;
+	    }
+	    for(int i=0; i<nlen; i++) {
+	        jump[needle.charAt(i)] = i; // index of last occurrence
+	    }
+	    int skip=0;
+	    for(int i=0; i<hlen-nlen+1; i+=skip) { // !!! not i<hlen 
+	        skip=0;
+	        for(int j=nlen-1; j>=0; j--) {
+	            if(haystack.charAt(i+j)!=needle.charAt(j)) {
+	                skip =Math.max( 1, j-jump[haystack.charAt(i+j)] );    // max is j+1, min is 1 (do not allow <0);
+	                break;
+	            }
+	        }
+	        if(skip==0) return i;
+	    }
+	    return -1;
+	}
+	
 	public static void main(String[] args) {
 		n028_Implement_strStr obj = new n028_Implement_strStr();
-		System.out.println(obj.strStr("21112341", "234"));
+		//System.out.println(obj.strStr("111234", "234"));
+		System.out.println(obj.strStrBM("111234", "234"));
 	}
 }
