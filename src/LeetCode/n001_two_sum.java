d82a5387d475f07fa0202b38f04154193eab92b2
C3vin <spacemonkey3@gmail.com>
2016-08-28 14:45:00 -0700

    update n01 two sum. wrong ans

--- src/LeetCode/n001_two_sum.java
+++ src/LeetCode/n001_two_sum.java
@@ -6,17 +6,17 @@ import java.util.Map;
 
 public class n001_two_sum {
 	//Brute Force
-	public int[] twoSum(int[] nums, int target) {
+	//Wrong ans
+/*	public int[] twoSum(int[] nums, int target) {
 		for (int i=0; i<nums.length; i++) {
-			for (int j=i+1; j<nums.length-1; j++) {
-				if (nums[j] == target - nums[i]) {		//==
-					System.out.println("Found it!");
-					 return new int[] { i, j };
+			for (int j=i+1; j<nums.length-1; j++) {    	//F: -1
+				if (nums[j] == target - nums[i]) {		//F: ==
+					 return new int[] { i, j };			//F: no need var name
 				}
 			}
 		}
 		 throw new IllegalArgumentException("No two sum solution");
-	}
+	}*/
 	
 	//Hash Table#1
 	public int[] twoSum_hash1(int[] nums, int target) {
@@ -49,13 +49,13 @@ public class n001_two_sum {
 	
 	public static void main(String[] args) {
 		n001_two_sum obj = new n001_two_sum();
-		int [] nums = {2, 17, 7, 15};
-		int target = 9;
-		int [] res = obj.twoSum(nums, target);
+		int [] nums = {3,2,4};
+		int target = 6;
+		//int [] res = obj.twoSum(nums, target);
 		int [] res_hash1= obj.twoSum_hash1(nums, target);
 		int [] res_hash2= obj.twoSum_hash2(nums, target);
 		
-		System.out.println(Arrays.toString(res));		
+		//System.out.println(Arrays.toString(res));		
 		System.out.println(Arrays.toString(res_hash1));	
 		System.out.println(Arrays.toString(res_hash2));	
 		//Ref:http://stackoverflow.com/questions/409784/whats-the-simplest-way-to-print-a-java-array
