3f713bf8b4e575bb626d56f029391d485992aecf
C3vin <spacemonkey3@gmail.com>
2016-09-09 15:35:38 -0700

    update n002

--- src/LeetCode/n002_add_two_numbers.java
+++ src/LeetCode/n002_add_two_numbers.java
@@ -1,36 +1,30 @@
 package LeetCode;
 
 public class n002_add_two_numbers {
-	class ListNode {
-		int val;
-		ListNode next;
-		ListNode(int x) { 
-			val = x; 
-		}
-	}
-	
 	public ListNode addTwoNumber(ListNode l1, ListNode l2) {
-		   ListNode dummyHead = new ListNode(0);
-		    ListNode p = l1, q = l2, curr = dummyHead;
-		    int carry = 0;
-		    while (p != null || q != null) {
-		        int x = (p != null) ? p.val : 0;
-		        int y = (q != null) ? q.val : 0;
-		        int sum = carry + x + y;
-		        carry = sum / 10;
-		        curr.next = new ListNode(sum % 10);
-		        curr = curr.next;
-		        if (p != null) p = p.next;
-		        if (q != null) q = q.next;
-		    }
-		    if (carry > 0) {
-		        curr.next = new ListNode(carry);
-		    }
-		    return dummyHead.next;
+		ListNode dummyHead = new ListNode(0);
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		while (p != null || q != null) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null) p = p.next;		//don't forget != null
+			if (q != null) q = q.next;
+		}
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
 	}
-	
+
 	public static void main(String[] args) {
 		n002_add_two_numbers obj = new n002_add_two_numbers();
-
+		ListNode l1 = ListNode.create(243);
+		ListNode l2 = ListNode.create(564);
+		System.out.println(obj.addTwoNumber(l1, l2));
 	}
 }
