df02b8633786c847c9a271144482c66efeb6c4d7
C3vin <spacemonkey3@gmail.com>
2016-09-12 02:29:43 -0700

    update n104

--- src/LeetCode/n104_Maximum_Depth_of_Binary_Tree.java
+++ src/LeetCode/n104_Maximum_Depth_of_Binary_Tree.java
@@ -16,37 +16,34 @@ public class n104_Maximum_Depth_of_Binary_Tree {
 		if(root == null) return 0;
 		return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;		//why +1
 	}
-	
+
 	public int maxDepth2(TreeNode root) {
-	    if(root == null)  
-	        return 0;  
-	    int level = 0;  
-	    LinkedList<TreeNode> queue = new LinkedList<TreeNode>();  
-	    queue.add(root);  
-	    int curNum = 1; //num of nodes left in current level  
-	    int nextNum = 0; //num of nodes in next level  
-	    while(!queue.isEmpty())  
-	    {  
-	        TreeNode n = queue.poll();  
-	        curNum--;  
-	        if(n.left!=null)  
-	        {  
-	            queue.add(n.left);  
-	            nextNum++;  
-	        }  
-	        if(n.right!=null)  
-	        {  
-	            queue.add(n.right);  
-	            nextNum++;  
-	        }  
-	        if(curNum == 0)  
-	        {  
-	            curNum = nextNum;  
-	            nextNum = 0;  
-	            level++;  
-	        }  
-	    }  
-	    return level;  
+		if(root == null)  
+			return 0;  
+		int level = 0;  
+		LinkedList<TreeNode> queue = new LinkedList<TreeNode>();  
+		queue.add(root);  
+		int curNum = 1; //num of nodes left in current level  
+		int nextNum = 0; //num of nodes in next level  
+		
+		while(!queue.isEmpty()) {  
+			TreeNode n = queue.poll();  
+			curNum--;  
+			if(n.left!=null) {  
+				queue.add(n.left);  
+				nextNum++;  
+			}  
+			if(n.right!=null) {  
+				queue.add(n.right);  
+				nextNum++;  
+			}  
+			if(curNum == 0) {  
+				curNum = nextNum;  
+				nextNum = 0;  
+				level++;  
+			}  
+		}  
+		return level;  
 	}
 	public static void main(String[] args) {
 		n104_Maximum_Depth_of_Binary_Tree obj = new n104_Maximum_Depth_of_Binary_Tree();
