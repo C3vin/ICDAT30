5f719dc5db4e9f847bbf052bb73d397eebbb9508
C3vin <spacemonkey3@gmail.com>
2016-09-14 14:26:21 -0700

    update n104

--- src/LeetCode/n104_Maximum_Depth_of_Binary_Tree.java
+++ src/LeetCode/n104_Maximum_Depth_of_Binary_Tree.java
@@ -14,7 +14,7 @@ public class n104_Maximum_Depth_of_Binary_Tree {
 
 	public int maxDepth(TreeNode root) {
 		if(root == null) return 0;
-		return Math.max(maxDepth(root.left), maxDepth(root.right)) +1;		//why +1
+		return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));	//root
 	}
 
 	public int maxDepth2(TreeNode root) {
