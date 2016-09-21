a87503f6fca876a592c4da43bf6fc444cea6e678
C3vin <spacemonkey3@gmail.com>
2016-09-21 10:54:20 -0700

    update n94 iterate sol

--- src/LeetCode/n094_Binary_Tree_Inorder_Traversal.java
+++ src/LeetCode/n094_Binary_Tree_Inorder_Traversal.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class n094_Binary_Tree_Inorder_Traversal {
@@ -23,11 +24,25 @@ public class n094_Binary_Tree_Inorder_Traversal {
 		res.add(root.val);
 		helper(root.right, res);
 	}
-	
+
 	//iterate
-/*	public List<Integer> inorderTraversal2(TreeNode root) {
-		
-	}*/
+	public List<Integer> inorderTraversal2(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();  
+		LinkedList<TreeNode> stack = new LinkedList<TreeNode>();  
+
+		while(root != null || !stack.isEmpty()) //F: Need while!!!
+			if(root != null) {   
+				stack.push(root);
+				root = root.left;
+			} 
+			else {
+				root = stack.pop();  //F: need root
+				res.add(root.val);
+				root = root.right;
+			}
+		return res;
+	}
+
 	public static void main(String[] args) {
 		n094_Binary_Tree_Inorder_Traversal obj = new n094_Binary_Tree_Inorder_Traversal();
 		TreeNode p1 = obj.new TreeNode(1);
@@ -35,7 +50,8 @@ public class n094_Binary_Tree_Inorder_Traversal {
 		TreeNode p3 = obj.new TreeNode(3);
 		p1.right = p2;
 		p2.left = p3;
-		
+
 		System.out.println(obj.inorderTraversal(p1));
+		System.out.println(obj.inorderTraversal2(p1));
 	}
 }
