e53c18ba1546d019cbdb1f1d668dd8f710d9e21a
C3vin <spacemonkey3@gmail.com>
2016-09-26 01:54:51 -0700

    update n003 ||

--- src/LeetCode/n003_longest_substring_without_repeating_characters.java
+++ src/LeetCode/n003_longest_substring_without_repeating_characters.java
@@ -1,11 +1,10 @@
 package LeetCode;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
 
 public class n003_longest_substring_without_repeating_characters {
 	//Brute Force
-	public int lengthOfLongestSubstring(String s) {
+	/*	public int lengthOfLongestSubstring(String s) {
 		int ans = 0;
 		for (int i=0; i<s.length(); i++)
 			for (int j=i+1; j<s.length()-1; j++)		//j<=n
@@ -24,6 +23,26 @@ public class n003_longest_substring_without_repeating_characters {
 			set.add(ch);
 		}
 		return true;
+	}*/
+	
+	public int lengthOfLongestSubstring(String s) {
+		if (s.length()==0) return 0;
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		//i = current point, j = start point (non repeating) 
+		int max=0;
+		int start=0;  
+		for (int current = 0; current<s.length(); current++) {
+			if (map.containsKey(s.charAt(current))) {
+				start = Math.max(start, map.get(s.charAt(current)) + 1);		//Need + 1
+			}
+			map.put(s.charAt(current), current);
+			max = Math.max(max, current - start + 1);
+		}
+		return max;
 	}
-
+	public static void main(String[] args) {
+		n003_longest_substring_without_repeating_characters obj = new n003_longest_substring_without_repeating_characters();
+		String s = "abcabcbb";
+		System.out.println(obj.lengthOfLongestSubstring(s));
+	}
 }
