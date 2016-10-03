8d8cd954912865964ab50dbbc59ba6a9eba4d51f
C3vin <spacemonkey3@gmail.com>
2016-10-03 10:13:19 -0700

    update n077

--- src/LeetCode/n077_Combinations.java
+++ src/LeetCode/n077_Combinations.java
@@ -8,11 +8,11 @@ public class n077_Combinations {
 	public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();  
 		List<Integer> tmp = new ArrayList<Integer>();  
-		dfs(res, tmp, k, n, 1);  
+		dfs(res, tmp, k, n, 1);  //because it need to begin from 1
 		return res;  
 	}
-	public void dfs(List<List<Integer>> res, List<Integer> tmp, int k, int n, int pos){  
-		for(int i=pos; i<=n; i++) {  
+	public void dfs(List<List<Integer>> res, List<Integer> tmp, int k, int n, int start){  
+		for(int i=start; i<=n; i++) {  
 			tmp.add(i); 
 			if(tmp.size() == k) {
 				res.add(new ArrayList<Integer>(tmp));  
