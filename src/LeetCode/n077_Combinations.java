08743801af6e536e7bdac20fe1ba79cf91c4f4c1
C3vin <spacemonkey3@gmail.com>
2016-10-03 11:22:25 -0700

    update 077

--- src/LeetCode/n077_Combinations.java
+++ src/LeetCode/n077_Combinations.java
@@ -1,7 +1,6 @@
 package LeetCode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class n077_Combinations {
@@ -12,13 +11,13 @@ public class n077_Combinations {
 		return res;  
 	}
 	public void dfs(List<List<Integer>> res, List<Integer> tmp, int k, int n, int start){  
-		for(int i=start; i<=n; i++) {  
+		for(int i=start; i<=n; i++) {  //F: need start can't use '1', // try each possibility number in current position
 			tmp.add(i); 
 			if(tmp.size() == k) {
 				res.add(new ArrayList<Integer>(tmp));  
 			}
-			dfs(res,tmp,k,n,i+1);  
-			tmp.remove(tmp.size()-1);  
+			dfs(res,tmp,k,n,i+1);  // after selecting number for current position, process next position 
+			tmp.remove(tmp.size()-1); //clear the current position to try next possible number 
 		}  
 	} 
 	public static void main(String[] args) {
