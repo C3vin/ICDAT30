f5ac12c083581848a03f86b41095ffc2acf530b4
C3vin <spacemonkey3@gmail.com>
2016-10-04 18:20:56 -0700

    add 276

---
+++ src/LeetCode/n276_Paint_Fence.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class n276_Paint_Fence {
+	 public int numWays(int n, int k) {
+		 int[] dp = {0, k, k*k, 0};			//? need 0
+		 
+		 if(n <= 2) 
+			 return dp[n];
+		 
+		 for(int i=2; i<n; i++) {
+			 dp[3] = (dp[1]+dp[2]) * (k-1);
+			 dp[1] = dp[2];
+			 dp[2] = dp[3];
+		 }
+		 return dp[3];
+	 }
+	 
+	 public static void main(String[] args) {
+		 n276_Paint_Fence obj = new n276_Paint_Fence();
+		 System.out.println(obj.numWays(4, 3));
+	 }
+}
