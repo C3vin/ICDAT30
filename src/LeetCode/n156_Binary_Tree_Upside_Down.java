d907eace3105daf9e109b0ff8de39eb733d45b1e
C3vin <spacemonkey3@gmail.com>
2016-10-11 17:27:12 -0700

    add 156

---
+++ src/LeetCode/n156_Binary_Tree_Upside_Down.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+/*a                         b
+ / \        ----->         / \
+b   c                     c   a*/
+public class n156_Binary_Tree_Upside_Down {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	public TreeNode upsideDownBinaryTree(TreeNode root) {
+		if(root == null || root.left == null) 
+			return root;
+		//e.g. root:1 -> root:2 -> root:4, root 4 has no left node, return root (back to root 2)
+		TreeNode newRoot = upsideDownBinaryTree(root.left);	//will get the latest left node, so it will go deeper and deeper e.g. 
+		TreeNode rightMostIterator = newRoot;
+		
+		while(rightMostIterator.right != null) {
+			rightMostIterator = rightMostIterator.right;
+		}
+		rightMostIterator.left = root.right;
+		rightMostIterator.right = new TreeNode(root.val);
+		
+		return newRoot;
+	}
+	
+	public static void main(String[] args) {
+		n156_Binary_Tree_Upside_Down obj = new n156_Binary_Tree_Upside_Down();
+		TreeNode p1 = obj.new TreeNode(1);
+		TreeNode p2 = obj.new TreeNode(2);
+		TreeNode p3 = obj.new TreeNode(3);
+		TreeNode p4 = obj.new TreeNode(4);
+		TreeNode p5 = obj.new TreeNode(5);
+		TreeNode p6 = obj.new TreeNode(6);
+		p1.left = p2;
+		p1.right = p3;
+		p2.left = p4;
+		p2.right = p5;
+		p3.left = p6;
+		System.out.println(obj.upsideDownBinaryTree(p1));
+	}
+}
