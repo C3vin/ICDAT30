ac578f20b75c52d8e4e40dfe4f871456efd1da25
C3vin <spacemonkey3@gmail.com>
2016-10-30 15:06:52 -0700

    add 237

---
+++ src/LeetCode/n237_Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class n237_Delete_Node_in_a_Linked_List {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	public void deleteNode(ListNode node) {
+		if(node == null) 
+			return;
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+	
+	public static void main(String[] args) {
+		n237_Delete_Node_in_a_Linked_List obj = new n237_Delete_Node_in_a_Linked_List();
+		ListNode p1 = obj.new ListNode(1);
+		ListNode p2 = obj.new ListNode(2);
+		ListNode p3 = obj.new ListNode(3);
+		ListNode p4 = obj.new ListNode(4);
+		p1.next = p2;
+		p2.next = p3;
+		p3.next = p4;
+	}
+}
