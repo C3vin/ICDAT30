38eea5f12035a87c0a3ab90a014608ea4e742677
C3vin <spacemonkey3@gmail.com>
2016-11-02 15:45:52 -0700

    update 111

--- src/LeetCode/n111_Minimum_Depth_of_Binary_Tree.java
+++ src/LeetCode/n111_Minimum_Depth_of_Binary_Tree.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.LinkedList;
 
 public class n111_Minimum_Depth_of_Binary_Tree {
 	public class TreeNode {
@@ -22,17 +23,52 @@ public class n111_Minimum_Depth_of_Binary_Tree {
             
         return Math.min(minleft, minright)+1;
     }
+    
+    //sol2: Non-recursive
+    public int minDepth2(TreeNode root) {
+    	if(root == null) return 0;
+    	
+    	int depth = 1;		//root, start depth at 1
+    	LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+    	queue.add(root);
+    	int curNum = 1;
+    	int nextNum = 0;
+    	
+    	while(!queue.isEmpty()) {
+    		TreeNode tmp = queue.poll();
+    		curNum--;
+    		
+    		if(tmp.left == null && tmp.right == null) {			//F: && 
+    			return depth;
+    		}
+    		
+    		if(tmp.left != null) {
+    			queue.add(tmp.left);
+    			nextNum++;
+    		}
+    		if(tmp.right != null) {
+    			queue.add(tmp.right);
+    			nextNum++;
+    		}
+    		if(curNum == 0) {
+    			curNum = nextNum;
+    			nextNum = 0;
+    			depth++;		//!!! if curNum didn't 0, you won't do depth++
+    		}
+    	}
+    	return depth;
+    }
+    
 	public static void main(String[] args) {
 		n111_Minimum_Depth_of_Binary_Tree obj = new n111_Minimum_Depth_of_Binary_Tree();
-		TreeNode p1 = obj.new TreeNode(3);
-		TreeNode p2 = obj.new TreeNode(9);
-		TreeNode p3 = obj.new TreeNode(20);
-		TreeNode p4 = obj.new TreeNode(1);
-		TreeNode p5 = obj.new TreeNode(3);
+		TreeNode p1 = obj.new TreeNode(1);
+		TreeNode p2 = obj.new TreeNode(2);
+		TreeNode p3 = obj.new TreeNode(3);
+		TreeNode p4 = obj.new TreeNode(4);
 		p1.left = p2;
-		//p1.right = p3;
-		//p2.right = p5;
-		//p2.left = p4;
+		p1.right = p3;
+		p2.left = p4;
 		System.out.println(obj.minDepth(p1));
+		System.out.println(obj.minDepth2(p1));
 	}
 }
