0257242e8fc822ee308bd9274a6a6a0fe2616cc7
C3vin <spacemonkey3@gmail.com>
2016-11-02 18:22:16 -0700

    add 47

---
+++ src/LeetCode/n047_Permutations_II.java
@@ -0,0 +1,46 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class n047_Permutations_II {
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		 List<List<Integer>> res = new ArrayList<List<Integer>>();
+		 boolean[] used = new boolean[nums.length];
+		 List<Integer> tmp = new LinkedList<Integer>();
+		 Arrays.sort(nums);
+		 helper(nums, res, tmp, used);
+		 return res;
+	}
+
+	private void helper(int[] nums, List<List<Integer>> res, List<Integer> tmp, boolean[] used) {
+		if(tmp.size() == nums.length) {
+			List<Integer> list = new LinkedList<Integer>(tmp);			//don't miss tmp
+			res.add(list);
+		} else {
+			for(int i=0; i<nums.length; i++) {
+				if(used[i])
+					continue;
+				
+				used[i] = true;
+				tmp.add(nums[i]);
+				helper(nums, res, tmp, used);
+				tmp.remove(tmp.size()-1);
+				used[i] = false;
+				
+				//!!!??????
+				while(i<nums.length-1 && nums[i] == nums[i+1]) {
+					i++;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		n047_Permutations_II obj = new n047_Permutations_II();
+		int[] nums = {1,1,2};
+		System.out.println(obj.permuteUnique(nums));
+	}
+}
