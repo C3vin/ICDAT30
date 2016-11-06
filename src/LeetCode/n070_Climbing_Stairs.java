6598f1f3f0a718d8aab8eeba1508fb090dcf4c8a
C3vin <spacemonkey3@gmail.com>
2016-11-06 10:36:04 -0800

    update 70

--- src/LeetCode/n070_Climbing_Stairs.java
+++ src/LeetCode/n070_Climbing_Stairs.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+@Alg(type="DP", com="A", level="easy", num=70)
 public class n070_Climbing_Stairs {
 	public int climbStairs(int n) {
 		int f1 = 1;
@@ -16,8 +17,21 @@ public class n070_Climbing_Stairs {
 		return f2;
 	}
 	
+	//DP
+	public int climbStairs2(int n) {
+		if(n==0 || n==1 || n==2) return n;
+		int[] dp = new int[n+1];			//n+1, cuz dp[0]
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 2;
+		for(int i=3; i<n+1; i++) {			//n+1
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		return dp[n];
+	}
 	public static void main(String[] args) {
 		n070_Climbing_Stairs obj = new n070_Climbing_Stairs();
-		System.out.println(obj.climbStairs(3));
+		System.out.println(obj.climbStairs(5));
+		System.out.println(obj.climbStairs2(5));
 	}
 }
