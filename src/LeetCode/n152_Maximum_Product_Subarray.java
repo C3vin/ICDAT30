3a4ec0f768f6c1646310f2148693d736716e3ce9
C3vin <spacemonkey3@gmail.com>
2016-11-06 22:00:38 -0800

    update 152

--- src/LeetCode/n152_Maximum_Product_Subarray.java
+++ src/LeetCode/n152_Maximum_Product_Subarray.java
@@ -1,23 +1,27 @@
 package LeetCode;
 
+@Alg(type="DP", com="LL", level="med", num=152)
+//switch max and min when Neg
 public class n152_Maximum_Product_Subarray {
 	public int maxProduct(int[] nums) {
-		int[] max = new int[nums.length];
-		int[] min = new int[nums.length];
-		int res = nums[0];
-		max[0] = min[0] = nums[0];
-
-		for(int i=1; i<nums.length; i++) {
+		int[] Max_local = new int[nums.length];
+		int[] min_local = new int[nums.length];
+		int global = nums[0];
+		Max_local[0] = min_local[0] = nums[0];
+		//careful about the max and min
+		for(int i=1; i<nums.length; i++) {			//i=1, cuz we don't need to check first element  
 			if(nums[i] > 0) {
-				max[i] = Math.max(nums[i], nums[i]*max[i-1]);			//F: is max[i-1]
-				min[i] = Math.min(nums[i], nums[i]*min[i-1]);
+				Max_local[i] = Math.max(nums[i], nums[i]*Max_local[i-1]);			//F: is max[i-1]
+				min_local[i] = Math.min(nums[i], nums[i]*min_local[i-1]);
+				System.out.println(i+ " : "+Max_local[i] + " : " + min_local[i]);
 			} else {
-				max[i] = Math.max(nums[i], nums[i]*min[i-1]);			 
-				min[i] = Math.min(nums[i], nums[i]*max[i-1]);
+				Max_local[i] = Math.max(nums[i], nums[i]*min_local[i-1]);			//when deal with Neg, switch min and Max local
+				min_local[i] = Math.min(nums[i], nums[i]*Max_local[i-1]);
+				System.out.println(i+ " : "+Max_local[i] + " : " + min_local[i]);
 			}
-			res = Math.max(res, max[i]);
+			global = Math.max(global, Max_local[i]);
 		}
-		return res;
+		return global;
 	}
 	public static void main(String[] args) {
 		n152_Maximum_Product_Subarray obj = new n152_Maximum_Product_Subarray();
