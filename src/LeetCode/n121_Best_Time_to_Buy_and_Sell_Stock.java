8649a5fb2491b9701c2aeba3f1a0f945de40d0e4
C3vin <spacemonkey3@gmail.com>
2016-11-07 03:05:17 -0800

    update 121

--- src/LeetCode/n121_Best_Time_to_Buy_and_Sell_Stock.java
+++ src/LeetCode/n121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,13 +1,13 @@
 package LeetCode;
 
+@Alg(type="DP", com="AA,M$,FB", level="easy", num=121)
 public class n121_Best_Time_to_Buy_and_Sell_Stock {
 	public int maxProfit(int[] prices) {
 		if(prices.length == 0) return 0;
 
 		int local = 0;
 		int global = 0;
-
-		for(int i=0; i<prices.length-1; i++) {
+		for(int i=0; i<prices.length-1; i++) {		//cuz prices[i+1]
 			local = Math.max(local + prices[i+1]-prices[i], 0);
 			global = Math.max(local, global);
 		}
@@ -15,14 +15,13 @@ public class n121_Best_Time_to_Buy_and_Sell_Stock {
 	}
 
 	/**
-	 * Go through the string and maintain one min sell price, and keep one largest profit 
-	 * 
-	 * 
+	 * Go through the string and maintain one min sell price, and keep one largest profit.
+	 * Better to understand
 	 */
 	public int maxProfit2(int[] prices) {
-		int min = Integer.MAX_VALUE;
+		int min = Integer.MAX_VALUE;		//MAX_VALUE
 		int max = 0;
-
+		
 		for(int i=0; i<prices.length; i++) {
 			min = Math.min(min, prices[i]);
 			max = Math.max(max, prices[i] - min);
