633596553fb94e53ccfc1ff9cb97ca7a231bd579
C3vin <spacemonkey3@gmail.com>
2016-11-06 19:32:50 -0800

    update 14

--- src/LeetCode/n014_longest_common_prefix.java
+++ src/LeetCode/n014_longest_common_prefix.java
@@ -1,21 +1,18 @@
 package LeetCode;
 
+@Alg(type="String", com="Yelp", level="easy", num=14)
 public class n014_longest_common_prefix {
 	public String longestCommonPrefix(String[] strs) {
 	    if(strs==null || strs.length==0)
 	        return "";
-	 
 	    if(strs.length==1) 
 	        return strs[0];
-	 
-	    int minLen = strs[0].length();
-	    for(String str: strs){
-	        if(minLen > str.length()){
-	            minLen = str.length();
+	    int minLen = strs[0].length();		//Need this for the for loop 
+	    for(String str: strs) {
+	        if(minLen > str.length()) {
+	            minLen = str.length();		//Get the most of the small size of strs, e.g. aaaaaew
 	        }
 	    }
-	    System.out.println(minLen);
-	    
 	    for(int i=0; i<minLen; i++){
 	        for(int j=0; j<strs.length-1; j++){
 	            String s1 = strs[j];
@@ -29,7 +26,7 @@ public class n014_longest_common_prefix {
 	}
 	
 	public static void main(String[] args) {
-		String strs[] = new String[]{"aaaaae", "aaaaabyrrrrrr", "aaaaaew"};
+		String strs[] = new String[]{"aaaaaaaae", "aaaaabyrrrrrr", "aaaaaew"};
 		n014_longest_common_prefix obj = new n014_longest_common_prefix();
 		System.out.println(obj.longestCommonPrefix(strs));
 		
