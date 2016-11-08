3be90b4969e637c29bbf1f3d65204efc2ea70797
C3vin <spacemonkey3@gmail.com>
2016-11-08 15:29:20 -0800

    update 46

--- src/LeetCode/n046_Permutations.java
+++ src/LeetCode/n046_Permutations.java
@@ -7,35 +7,34 @@ import java.util.List;
 @Alg(type="NP", com="LL&M$", level="med", num=46)
 public class n046_Permutations {
 	//Need one res, tmp, used(visited) for dfs
-	 public List<List<Integer>> permute(int[] nums) {
-		 List<List<Integer>> res = new ArrayList<List<Integer>>();
-		 boolean[] used = new boolean[nums.length];
-		 List<Integer> tmp = new LinkedList<Integer>();
-		 helper(nums, res, tmp, used);
-		 return res;
-	 }
-	 //DFS
-	 //Need one list to create space [A,B,C] and add to res
-	 private void helper(int[] nums, List<List<Integer>> res, List<Integer> tmp, boolean[] used) {
-		 if(tmp.size() == nums.length) {
-			 List<Integer> list = new LinkedList<Integer>(tmp);		//why? cuz tmp will be change
-			 res.add(list);
-		 } else {
-			 for(int idx=0; idx<nums.length; idx++) {
-				 if(used[idx])
-					 continue;
-				 //add element and do check next 
-				 used[idx] = true;
-				 tmp.add(nums[idx]);
-				 helper(nums, res, tmp, used);
-				 tmp.remove(tmp.size()-1); 	
-				 used[idx] = false;  //!!! reset
-			 }
-		 }
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		boolean[] used = new boolean[nums.length];
+		List<Integer> tmp = new LinkedList<Integer>();
+		helper(nums, res, tmp, used);
+		return res;
+	}
+	//DFS
+	//Need one list to create space [A,B,C] and add to res
+	private void helper(int[] nums, List<List<Integer>> res, List<Integer> tmp, boolean[] used) {
+		if(tmp.size() == nums.length) {
+			List<Integer> list = new LinkedList<Integer>(tmp);		//why? cuz tmp will be change
+			res.add(list);
+		}
+		for(int i=0; i<nums.length; i++) {
+			if(used[i])
+				continue;
+			//add element and do check next 
+			used[i] = true;
+			tmp.add(nums[i]);
+			helper(nums, res, tmp, used);
+			tmp.remove(tmp.size()-1); 	
+			used[i] = false;  //!!! reset
+		}
 	}
 	public static void main(String[] args) {
-		 n046_Permutations obj = new n046_Permutations();
-		 int[] nums = {1,2,3};
+		n046_Permutations obj = new n046_Permutations();
+		int[] nums = {1,2,3};
 		System.out.println(obj.permute(nums));
-	 }
+	}
 }
