9b6198067972644f76b7736d1da867bf332edf73
C3vin <spacemonkey3@gmail.com>
2016-11-10 18:21:59 -0800

    update 9

--- src/LeetCode/n009_palindrome_number.java
+++ src/LeetCode/n009_palindrome_number.java
@@ -8,18 +8,21 @@ public class n009_palindrome_number {
 		int div = 1;
 		while(div <= x/10) {
 			div = div * 10;
+			
 		}
+		System.out.println(div + " : " + x);
 		while(x > 0) {
 			if(x/div != x%10) 
 				return false;
 			x = (x%div)/10;		//?
-			div = div/10;
+			div = div/100;
+			System.out.println(x + " : " + div);
 		}
 		return true;
 	}
 	
 	public static void main(String[] args) {
 		n009_palindrome_number obj = new n009_palindrome_number();
-		System.out.println(obj.isPailindrome(101011));
+		System.out.println(obj.isPailindrome(10101));
 	}
 }
