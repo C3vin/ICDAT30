0cef3076d70469d1d677c5cec9d750fcb21214a5
C3vin <spacemonkey3@gmail.com>
2016-11-12 16:23:28 -0800

    update 243

--- src/LeetCode/n243_Shortest_Word_Distance.java
+++ src/LeetCode/n243_Shortest_Word_Distance.java
@@ -1,23 +1,23 @@
 package LeetCode;
 
+@Alg(type="Array", com="L", level="easy", num=242)
 public class n243_Shortest_Word_Distance {
 	public int shortestDistance(String[] words, String word1, String word2) {
-		int m=-1;
-		int n=-1;
-		int min = Integer.MAX_VALUE;
+		int idx1=-1;	//default
+		int idx2=-1;
+		int distance = Integer.MAX_VALUE;
 		for(int i=0; i<words.length; i++) {
-			String s = words[i];
-			if(word1.equals(s)) {
-				m = i;
-				if(n!=-1)
-					min = Math.min(min, m-n);
-			}else if(word2.equals(s)) {
-				n = i;
-				if(m!=-1)
-					min = Math.min(min, n-m);
+			if(words[i].equals(word1)) {	//words[i]
+				idx1 = i;
+				if(idx2!=-1)		//need this idx2 != -1
+					distance = Math.min(distance, idx1-idx2);
+			}else if(words[i].equals(word2)) {
+				idx2 = i;
+				if(idx1!=-1)
+					distance = Math.min(distance, idx2-idx1);
 			}
 		}
-		return min;
+		return distance;
 	}
 
 	public static void main(String[] args) {
