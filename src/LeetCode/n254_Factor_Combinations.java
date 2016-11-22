b3c658c8afdcea641836b48ba3594a62ce0b7016
C3vin <spacemonkey3@gmail.com>
2016-11-22 01:58:48 -0800

    add 254

---
+++ src/LeetCode/n254_Factor_Combinations.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class n254_Factor_Combinations {
+	public List<List<Integer>> getFactors(int n) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+
+		helper(2, 1, n, res, tmp);
+		return res;
+	}
+	public void helper(int start, int product, int n, List<List<Integer>> res, List<Integer> tmp) {
+		if(start > n || product > n) 		//need start > n to deal with n=1
+			return;
+		//met req
+		if(product == n) {
+			List<Integer> list = new ArrayList<Integer>(tmp);
+			res.add(list);
+			return;
+		}
+		for(int i=start; i<n; i++) {
+			if(i*product>n)             //Time Limit Exceeded
+				break;
+			if(n%i == 0) {
+				tmp.add(i);
+				helper(i, i*product, n, res, tmp);
+				tmp.remove(tmp.size()-1);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		n254_Factor_Combinations obj = new n254_Factor_Combinations();
+		System.out.println(obj.getFactors(1));
+	}
+}
