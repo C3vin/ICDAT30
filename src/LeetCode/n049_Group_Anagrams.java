3054aa52aec76fb96a41726e7b43c31e42c962ff
C3vin <spacemonkey3@gmail.com>
2017-01-26 00:54:10 -0800

    update 49

--- src/LeetCode/n049_Group_Anagrams.java
+++ src/LeetCode/n049_Group_Anagrams.java
@@ -1,29 +1,31 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 public class n049_Group_Anagrams {
 	public List<List<String>> groupAnagrams(String[] strs) {
 		List<List<String>> res = new ArrayList<List<String>>();
-		HashMap<String,ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
 		
 		for(String str : strs) {
+			//System.out.println(str); eat tea .... 
 			char[] arr = new char[26];
 			for(int i=0; i<str.length(); i++) {
-				arr[str.charAt(i) - 'a']++;
+				arr[str.charAt(i) - 'a']++;		//F: need - 'a'
 			}
 			String ns = new String(arr);
 			if(map.containsKey(ns)) {
 				map.get(ns).add(str);
 			} else {
-				ArrayList<String> al = new ArrayList<String>();
-				al.add(str);
-				map.put(ns, al);
+				ArrayList<String> list = new ArrayList<String>();
+				list.add(str);
+				map.put(ns, list);
 			}
 		}
-		res.addAll(map.values());
+		res.addAll(map.values());	//addAll(map.values) 
 		
 		return res;
 	}
