56420d89b841de2a6115698e137496e3f15be7d1
C3vin <spacemonkey3@gmail.com>
2017-01-30 02:15:55 -0800

    update 43

--- src/LeetCode/n043_Multiply_Strings.java
+++ src/LeetCode/n043_Multiply_Strings.java
@@ -6,17 +6,17 @@ public class n043_Multiply_Strings {
 		if(num1.length() == 0 || num2.length() == 0 || num1 == null || num2 == null) return null;
 		if(num1.equals("0") || num2.equals("0")) return "0";	//F: string use equals()
 		
-		num1 = new StringBuilder(num1).reverse().toString();
-		num2 = new StringBuilder(num2).reverse().toString();	//test if no reverse
+		//num1 = new StringBuilder(num1).reverse().toString();
+		//num2 = new StringBuilder(num2).reverse().toString();	//test if no reverse
 		
-		int[] d = new int[num1.length() + num2.length()];
+		int[] d = new int[num1.length() + num2.length()];	//enough
 		
 		for(int i=0; i<num1.length(); i++) {
-			int a = num1.charAt(i)-'0';		//F:-'0'
+			int a = num1.charAt(num1.length()-1-i)-'0';		//F:-'0' & why need last-1-i, when -i will let element move <---
 			for(int j=0; j<num2.length(); j++) {
-				int b = num2.charAt(j)-'0';
+				int b = num2.charAt(num2.length()-1-j)-'0';
 				d[i+j] = d[i+j] + a * b; 
-			}
+			}	
 		}
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<d.length; i++) {
@@ -27,12 +27,16 @@ public class n043_Multiply_Strings {
 				d[i+1] = d[i+1] + carry;
 			}
 		}
-		if(sb.charAt(0) == '0' && sb.length() > 0)
-			sb = sb.deleteCharAt(0);	//remove the first '0'
+		if(sb.charAt(0) == '0' && sb.length() > 0) {
+			//sb = sb.deleteCharAt(0);	//remove the first '0'
+			return sb.substring(1);
+		}
 		return sb.toString();
 	}
 	public static void main(String[] args) {
 		n043_Multiply_Strings obj = new n043_Multiply_Strings();
-		System.out.println(obj.multiply("9133", "0"));
+		System.out.println(obj.multiply("321", "43"));    //13803
+		System.out.println(obj.multiply("9133", "0"));    //0
+		System.out.println(obj.multiply("98", "9"));     //882
 	}
 }
