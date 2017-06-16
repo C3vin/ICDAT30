9683a46a210765eaf8dc4d6251cd55cab186d6e7
C3vin <spacemonkey3@gmail.com>
2017-06-16 16:01:50 -0700

    DSA add Q16

---
+++ src/DSA/c6/Q16_NumberOfLeavesInBTusingLevelOrder.java
@@ -0,0 +1,54 @@
+package DSA.c6;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Q16_NumberOfLeavesInBTusingLevelOrder {
+	public class TreeNode {
+		int val;
+		String v;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+		TreeNode(String v) { v = null; }
+	}
+	
+	public int numberOfLeavesInBTusingLevelOrder(TreeNode root) {
+		int count = 0;
+		if(root == null) return count;
+		
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();	
+		queue.offer(root);
+		
+		while(!queue.isEmpty()) {
+			TreeNode tmp = queue.poll();
+			if(tmp.left == null && tmp.right == null)
+				count++;
+			if(tmp.left != null)
+				queue.offer(tmp.left);
+			if(tmp.right != null)
+				queue.offer(tmp.right);
+		}
+		return count;
+	}
+	public static void main(String[] args) {
+		Q16_NumberOfLeavesInBTusingLevelOrder obj = new Q16_NumberOfLeavesInBTusingLevelOrder();
+		TreeNode p1 = obj.new TreeNode(1);
+		TreeNode p2 = obj.new TreeNode(2);
+		TreeNode p3 = obj.new TreeNode(3);
+		TreeNode p4 = obj.new TreeNode(4);
+		TreeNode p5 = obj.new TreeNode(5);
+		TreeNode p6 = obj.new TreeNode(6);
+		TreeNode p7 = obj.new TreeNode(7);
+		p1.left = p2;
+		p1.right = p3;
+		p1.left = p2;
+		p2.right = p5;
+		p2.left = p4;
+		p3.right = p7;
+		p3.left = p6;
+
+		System.out.println(p1.val + " " + p1.left.val + " " + p1.right.val + " "+ p2.left.val + " " + p2.right.val + " " + p3.left.val + " " +p3.right.val);
+		System.out.println(obj.numberOfLeavesInBTusingLevelOrder(p1));
+	}
+}
