6715fae05872aa884a52df0abdfe3bfd1071426f
C3vin <spacemonkey3@gmail.com>
2017-07-11 01:42:53 -0700

    update LC 142

--- src/LeetCode/n142_Linked_List_Cycle_II.java
+++ src/LeetCode/n142_Linked_List_Cycle_II.java
@@ -1,13 +1,34 @@
 package LeetCode;
 
+/*Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+Note: Do not modify the linked list.
+Follow up:
+Can you solve it without using extra space?*/
+
 public class n142_Linked_List_Cycle_II {
 	public ListNode detectCycle(ListNode head) {
-		
+		ListNode slow = head;
+		ListNode fast = head; 
+
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if(slow == fast) {				//need to check after find the cycle
+				slow = head;				//reset to head so no need another pointer
+				while(slow != fast) {
+					slow = slow.next;
+					fast = fast.next;
+				}
+				return slow;
+			}
+		}
+		return null;
 	}
 	public static void main(String[] args) {
 		n142_Linked_List_Cycle_II obj = new n142_Linked_List_Cycle_II();
-		ListNode head = ListNode.create(123456);
+		ListNode head = ListNode.create(12345);
 		head.next.next.next = head;
-		System.out.println(obj.detectCycle(head));
+		System.out.println(obj.detectCycle(head));		//can't run
 	}
 }
