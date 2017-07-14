f31e949590d4c020ed4256b09fa28c7134b080e2
C3vin <spacemonkey3@gmail.com>
2017-07-13 23:31:57 -0700

    update LC 24

--- src/LeetCode/n024_Swap_Nodes_in_Pairs.java
+++ src/LeetCode/n024_Swap_Nodes_in_Pairs.java
@@ -13,12 +13,13 @@ public class n024_Swap_Nodes_in_Pairs {
 	      
 	      while(ptr2!=null && ptr2.next!=null){
 	          ListNode nextstart = ptr2.next.next;
+	          
 	          ptr2.next.next = ptr2;	//2->1
 	          ptr1.next = ptr2.next;	//0->2
 	          ptr2.next = nextstart;	//1->3
 	          
-	          ptr1 = ptr2;
-	          ptr2 = ptr2.next;
+	          ptr1 = ptr2;				//after first while, fh->2->1(p1)->3(p2)->4
+	          ptr2 = ptr2.next;			
 	      }
 	    return fakehead.next;
 	  }
