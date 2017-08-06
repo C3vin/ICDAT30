c5aa7da1d7f9a1a883c658e2ebc524914c2cceee
C3vin <spacemonkey3@gmail.com>
2017-08-06 00:57:32 -0700

    update LC 186

--- src/LeetCode/n186_Reverse_Words_in_a_String_II.java
+++ src/LeetCode/n186_Reverse_Words_in_a_String_II.java
@@ -3,25 +3,27 @@ package LeetCode;
 //return "blue is sky the". do in place
 public class n186_Reverse_Words_in_a_String_II {
 	public void reverseWords(char[] s) {
-		if (s.length == 0) return;
-		reverseArray(s, 0, s.length-1);
+		if (s.length == 0) 
+			return;
+		
+		reverseArray(s, 0, s.length-1);			//reverse all
+		
 		int last = 0;
 		for (int i=0; i<s.length; i++) {
-			//System.out.println(i);
 			if (s[i] == ' ') {
-				reverseArray(s, last, i-1);
-				last = i + 1;
-			} else if (i==s.length-1) {
+				reverseArray(s, last, i-1);		//reverse subarray
+				last = i + 1;					//update last
+			}
+			if (i == s.length-1) {		//F: handle abc, hi!, no space value
 				reverseArray(s, last, i);
 			}
 		}
 		for(int i=0; i<s.length; i++)
 			System.out.println(s[i]);
-
 	}
 	public void reverseArray(char[] s, int l, int r) {
 		//swap
-		while (l <= r) {
+		while (l < r) {
 			char temp = s[l];
 			s[l] = s[r];
 			s[r] = temp;
@@ -33,6 +35,7 @@ public class n186_Reverse_Words_in_a_String_II {
 		n186_Reverse_Words_in_a_String_II obj = new n186_Reverse_Words_in_a_String_II();
 		//char[] s = {'t','h','e',' ','s','k','y'};
 		char[] s = {'h','i','!'};
+		//char[] s = {'a','b','c'};
 		obj.reverseWords(s);
 	}
 }
