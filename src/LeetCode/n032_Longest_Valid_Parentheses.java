34e054477ec187937662eabe33ab23d85688df9e
C3vin <spacemonkey3@gmail.com>
2017-08-12 01:40:53 -0700

    add LC 032 with 3 sol

--- src/LeetCode/n032_Longest_Valid_Parentheses.java
+++ src/LeetCode/n032_Longest_Valid_Parentheses.java
@@ -30,15 +30,74 @@ public class n032_Longest_Valid_Parentheses {
 		return stack.isEmpty();	//isempty()
 	}
 	
+	//Stack
 	public int longestValidParentheses2(String s) {
+		int maxLen = 0;
+		LinkedList<Integer> stack = new LinkedList<Integer>();
+		stack.push(-1);		//dummy
 		
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i) == '(') {
+				stack.push(i);
+			} else {
+				stack.pop();
+				if(stack.isEmpty())
+					stack.push(i);
+				
+				maxLen = Math.max(maxLen, i - stack.peek());
+			}
+		}
+		return maxLen;
+	}
+	
+	//Without extra space
+	public int longestValidParentheses3(String s) {
+		int left = 0;
+		int right = 0;
+		int maxLen = 0;
+		
+		//start from left
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i) == '(')
+				left++;
+			else 
+				right++;
+			
+			if(left == right) {
+				maxLen = Math.max(maxLen, right*2);
+			} else if(right >= left) {
+				left = 0;
+				right = 0;
+			}
+		}
+		
+		left = 0; 	//Need to reset!!!
+		right = 0;
+		
+		//start from right
+		for(int i=s.length()-1; i>=0; i--) {
+			if(s.charAt(i) == ')') 
+				right++;
+			else
+				left++;
+			
+			if(left == right) {
+				maxLen = Math.max(maxLen, left*2);
+			} else if(left >= right) {				//handle )((()), from right to left, need to avoid left = 3, right = 3 in wrong order
+				left = 0;
+				right = 0;
+			}
+		}
+		return maxLen;
 	}
 	
 	public static void main(String[] args) {
 		n032_Longest_Valid_Parentheses obj = new n032_Longest_Valid_Parentheses();
-		//String s = ")()())";
+		String s = ")()())";
 		//String s = ")(";
-		String s = "(())";
+		//String s = "(())";
 		System.out.println(obj.longestValidParentheses(s));
+		System.out.println(obj.longestValidParentheses2(s));
+		System.out.println(obj.longestValidParentheses3(s));
 	}
 }
