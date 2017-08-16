695dbf8b3f7e87a3bed280935d0fd93780115655
C3vin <spacemonkey3@gmail.com>
2017-08-16 02:26:27 -0700

    update LC 349

--- src/LeetCode/n349_Intersection_of_Two_Arrays.java
+++ src/LeetCode/n349_Intersection_of_Two_Arrays.java
@@ -2,12 +2,13 @@ package LeetCode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 //Given nums1 = [1,2,2,1,3,4,5,5,6], nums2 = [2, 2, 6], return [2, 6].
 public class n349_Intersection_of_Two_Arrays {
-	//sol1: HashSet
+	//HashSet
 	public int[] intersection(int[] nums1, int[] nums2) {
 		Set<Integer> s1 = new HashSet<Integer>();
 		for(int i : nums1) {
@@ -23,14 +24,38 @@ public class n349_Intersection_of_Two_Arrays {
 		int k=0;
 		for(int n : s2) {
 			res[k++] = n;
-		}
-
+		} 
+			
 		for(int p=0; p<res.length; p++) 
 			System.out.println(res[p]);
 		return res;					//res[0]=2, res[1]=6
 	}
-
-	//sol2: public static int binarySearch(int[] a, int key)
+	//HashMap
+	public int[] intersection3(int[] nums1, int[] nums2) {
+		HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+		HashMap<Integer, Boolean> intermap = new HashMap<Integer, Boolean>();
+		
+		for(int i=0; i<nums1.length; i++) {
+			if(!map.containsKey(nums1[i])) {
+				map.put(nums1[i], true);
+			}
+		}
+		
+		for(int j=0; j<nums2.length; j++) {
+			if(map.containsKey(nums2[j]) && !intermap.containsKey(nums2[j])) {
+				intermap.put(nums2[j], true);
+			}
+		}
+		
+		int[] res = new int[intermap.size()];		//need now create res!
+		int p = 0;
+		for(int value : intermap.keySet()) {
+			res[p++] = value;
+		}
+		return res;
+	}
+	
+	//sort 
 	public int[] intersection2(int[] nums1, int[] nums2) {
 		Arrays.sort(nums1);
 		Arrays.sort(nums2);
@@ -55,7 +80,8 @@ public class n349_Intersection_of_Two_Arrays {
 		n349_Intersection_of_Two_Arrays obj = new n349_Intersection_of_Two_Arrays();
 		int[] nums1 = {1,2,2,1,3,4,5,5,6};
 		int[] nums2 = {2,2,6};
-		System.out.println(obj.intersection(nums1, nums2));
+		//System.out.println(obj.intersection(nums1, nums2));
 		//System.out.println(obj.intersection2(nums1, nums2));
+		System.out.println(obj.intersection3(nums1, nums2));
 	}
 }
