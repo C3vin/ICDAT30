e4dc9c704595b44acadd23aabe347813c4ea47ce
C3vin <spacemonkey3@gmail.com>
2017-08-22 18:00:41 -0700

    update LC 217

--- src/LeetCode/n217_Contains_Duplicate.java
+++ src/LeetCode/n217_Contains_Duplicate.java
@@ -2,8 +2,10 @@ package LeetCode;
 
 import java.util.HashSet;
 
+//Given an array of integers, find if the array contains any duplicates. 
+//Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 public class n217_Contains_Duplicate {
-	//21ms
+	//hashset 21ms
 	public boolean containsDuplicate(int[] nums) {
 		if(nums == null || nums.length == 0)
 			return false;
@@ -15,13 +17,13 @@ public class n217_Contains_Duplicate {
 		}
 		return false;
 	}
-	//14ms
+	//hashset 14ms
 	public boolean containsDuplicate2(int[] nums) {
 		if(nums == null || nums.length == 0)
 			return false;
 		HashSet<Integer> set = new HashSet<Integer>();
 		for(int i=0; i<nums.length; i++) {
-			if(!set.add(nums[i]))
+			if(!set.add(nums[i]))					//If this set already contains the element, the call leaves the set unchanged and returns false.
 				return true;
 		}
 		return false;
