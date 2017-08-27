8b8959ad0b1c99c4da87417d0ed585d0fdd4bb08
C3vin <spacemonkey3@gmail.com>
2017-08-26 17:52:54 -0700

    update LC 242

--- src/LeetCode/n242_Valid_Anagram.java
+++ src/LeetCode/n242_Valid_Anagram.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
 import java.util.Arrays;
+//For example, s = "anagram", t = "nagaram", return true.
+//Follow up:What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
 public class n242_Valid_Anagram {
 	public boolean isAnagram(String s, String t) {
@@ -27,19 +29,22 @@ public class n242_Valid_Anagram {
 		Arrays.sort(sch);				//F: if sort, need to convert to char array first.
 		Arrays.sort(tch);
 		
-		//System.out.println(sch);
-		//System.out.println(tch);
-		
 		if(Arrays.equals(sch, tch))		//compare two char array using Arrays.equals.
 			return true;
+		
+		//if don't want to use Arrays.equals()
+/*		for(int i=0; i<s.length(); i++) {
+			if(sch[i] != tch[i])
+				return false;
+		}*/
 		
 		return false;
 	}
 	public static void main(String[] args) {
 		n242_Valid_Anagram obj = new n242_Valid_Anagram();
-		String s = "anagram"; 
-		String t = "nagaram";
-		//System.out.println(obj.isAnagram(s, t));
-		System.out.println(obj.isAnagram2(s, t));
+		String s = "#anagram@"; 
+		String t = "n#agar@am";
+		//System.out.println(obj.isAnagram(s, t));		//can't handle unicode
+		System.out.println(obj.isAnagram2(s, t));		//OK for unicode
 	}
 }
