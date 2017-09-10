7dc35fb4a1436ff527360e15e932cdd9411a8752
C3vin <spacemonkey3@gmail.com>
2017-09-10 11:38:18 -0700

    update LC567

--- src/LeetCode/n567_Permutation_in_String.java
+++ src/LeetCode/n567_Permutation_in_String.java
@@ -1,8 +1,12 @@
 package LeetCode;
 
-/*Input:s1 = "ab" s2 = "eidbaooo"
-  Output:True
-  Explanation: s2 contains one permutation of s1 ("ba").*/
+/*
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. 
+In other words, one of the first string's permutations is the substring of the second string.
+Input:s1 = "ab" s2 = "eidbaooo"
+Output:True
+Explanation: s2 contains one permutation of s1 ("ba").
+*/
 
 public class n567_Permutation_in_String {
 	public boolean checkInclusion(String s1, String s2) {
@@ -34,7 +38,7 @@ public class n567_Permutation_in_String {
 		}
 		return false;
 	}
-	//Sliding Windowof Space complexity : O(1) s1map, s2map size 26 is used.
+	//Sliding Window Space complexity : O(1) s1map, s2map size 26 is used.
 	public boolean checkInclusion2(String s1, String s2) {
 		if(s1.length() > s2.length()) 
 			return false;
