c19bc3f3aa230d603a20a7cae1871064b6902320
C3vin <spacemonkey3@gmail.com>
2017-09-10 11:34:29 -0700

    update LC567

--- src/LeetCode/n567_Permutation_in_String.java
+++ src/LeetCode/n567_Permutation_in_String.java
@@ -5,39 +5,69 @@ package LeetCode;
   Explanation: s2 contains one permutation of s1 ("ba").*/
 
 public class n567_Permutation_in_String {
-	 public boolean checkInclusion(String s1, String s2) {
-		 int[] target = new int[256];
-		 
-		 for(int i=0; i<s1.length(); i++) {
-			 target[s1.charAt(i)]++;
-		 }
-		 
-		 int left = 0;
-		 int right = 0;
-		 int count = s1.length();
-		 while(right < s2.length()) {
-			 if(target[s2.charAt(right)] >= 1)
-				 count--;
-			 target[s2.charAt(right)]--;
-			 right++;
-			 
-			 if(count == 0)
-				 return true;
-			 
-			 if(right - left == s1.length()) {
-				 if(target[s2.charAt(left)] >= 0)		//>=0, not <=0. Because before at least 1
-					 count++;
-				 
-				 target[s2.charAt(left)]++;
-				 left++;
-			 }
-		 }
-		 return false;
-	 }
-	 public static void main(String[] args) {
-		 n567_Permutation_in_String obj = new n567_Permutation_in_String();
-		 String s1 = "ab";
-		 String s2 = "eidbaooo";
-		 System.out.println(obj.checkInclusion(s1, s2));
-	 }
+	public boolean checkInclusion(String s1, String s2) {
+		int[] target = new int[256];
+
+		for(int i=0; i<s1.length(); i++) {
+			target[s1.charAt(i)]++;
+		}
+
+		int left = 0;
+		int right = 0;
+		int count = s1.length();
+		while(right < s2.length()) {
+			if(target[s2.charAt(right)] >= 1)
+				count--;
+			target[s2.charAt(right)]--;
+			right++;
+
+			if(count == 0)
+				return true;
+
+			if(right - left == s1.length()) {
+				if(target[s2.charAt(left)] >= 0)		//>=0, not <=0. Because before at least 1
+					count++;
+
+				target[s2.charAt(left)]++;
+				left++;
+			}
+		}
+		return false;
+	}
+	//Sliding Windowof Space complexity : O(1) s1map, s2map size 26 is used.
+	public boolean checkInclusion2(String s1, String s2) {
+		if(s1.length() > s2.length()) 
+			return false;
+
+		int[] s1map = new int[26]; 	//index
+		int[] s2map = new int[26];
+
+		for(int i=0; i<s1.length(); i++) {
+			s1map[s1.charAt(i) - 'a']++;
+			s2map[s2.charAt(i) - 'a']++;			//need s2, first window
+		}
+		for(int i=0; i<s2.length()-s1.length(); i++) {
+			if(isMatch(s1map, s2map))
+				return true;
+			//slide window, update s2map
+			s2map[s2.charAt(i+s1.length()) - 'a']++;
+			s2map[s2.charAt(i) - 'a']--;
+		}
+		return isMatch(s1map, s2map);
+	}
+	private boolean isMatch(int[] s1map, int[]s2map) {
+		for (int i = 0; i < 26; i++) {
+			if (s1map[i] != s2map[i])
+				return false;
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		n567_Permutation_in_String obj = new n567_Permutation_in_String();
+		String s1 = "abw";
+		String s2 = "eidbaooo";
+		System.out.println(obj.checkInclusion(s1, s2));
+		System.out.println(obj.checkInclusion2(s1, s2));
+	}
 }
