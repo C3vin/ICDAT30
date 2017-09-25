76d44c272265412cf868d7d4f7ab4f4c2787c7f9
C3vin <spacemonkey3@gmail.com>
2017-09-24 21:51:23 -0700

    update LC 7

--- src/LeetCode/n007_reverse_interger.java
+++ src/LeetCode/n007_reverse_interger.java
@@ -1,12 +1,13 @@
 package LeetCode;
 
-@Alg(type="Math", com="A", level="easy", num=7)
+//Reverse digits of an integer.
+//Example1: x = 123, return 321
+//Example2: x = -123, return -321
 public class n007_reverse_interger {
 	public int reverseInterger(int x) {
 		long result = 0;
 		while (x!= 0) {
 			result = result * 10 + (x%10);
-			System.out.println(result);
 /*			if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE; 
 			if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;*/
 			x = x/10;
@@ -15,7 +16,7 @@ public class n007_reverse_interger {
 	}
 	public static void main(String[] args) {
 		n007_reverse_interger obj = new n007_reverse_interger();
-		int x = 123;//Integer.MAX_VALUE;
+		int x = -123;		//Integer.MAX_VALUE;
 		System.out.println(x + " : "+ obj.reverseInterger(x));
 	}
 }
