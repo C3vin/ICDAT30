ae024bb8a9514cdf6b3bd642682debb071015eae
C3vin <spacemonkey3@gmail.com>
2017-09-24 22:58:57 -0700

    update LC 8 atof

--- src/LeetCode/n008_string_to_integer_atoi.java
+++ src/LeetCode/n008_string_to_integer_atoi.java
@@ -32,8 +32,36 @@ public class n008_string_to_integer_atoi {
 		}
 		return isNeg? -res:res;
 	  }
+	  
+	  public float myAtof(String str) {
+		  float res = 0;
+		  float decimalpoint = 0;
+		  boolean flag = false;
+		  int base = 1;
+		  
+		  for(char c : str.toCharArray()) {
+			  if(c == '.') {
+				  flag = true;
+				  continue;
+			  }
+			  if(c < '0' || c > '9') {
+				  return 0f;			//deal with 123.45w f: float
+			  }
+			  if(flag) {
+				  decimalpoint = decimalpoint * 10 + (c - '0');		//F: need -'0'
+				  base = base *10;
+			  } else {
+				  res = res * 10 + (c - '0');
+			  }
+		  }
+		  if(decimalpoint > 0) {
+			  decimalpoint = decimalpoint / base;
+		  }
+		  return res + decimalpoint;
+	  }
 	  public static void main(String[] args) {
 		  n008_string_to_integer_atoi obj = new n008_string_to_integer_atoi();
 		  System.out.println(obj.myAtoi(" -2147483648"));
+		  System.out.println(obj.myAtof("123.45"));
 	  }
 }
