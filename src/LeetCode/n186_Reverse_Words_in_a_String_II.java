05a3dc2e8baf0fda1c21ad71e991b25351c2bdc4
C3vin <spacemonkey3@gmail.com>
2017-09-29 01:42:30 -0700

    update LC 186

--- src/LeetCode/n186_Reverse_Words_in_a_String_II.java
+++ src/LeetCode/n186_Reverse_Words_in_a_String_II.java
@@ -1,4 +1,6 @@
 package LeetCode;
+//Given an input string, reverse the string word by word. A word is defined as a sequence of non-space characters.
+//The input string does not contain leading or trailing spaces and the words are always separated by a single space.
 //Given s = "the sky is blue",
 //return "blue is sky the". do in place
 public class n186_Reverse_Words_in_a_String_II {
@@ -6,38 +8,42 @@ public class n186_Reverse_Words_in_a_String_II {
 		if (s.length == 0) 
 			return;
 
-		reverseArray(s, 0, s.length-1);			//reverse all
+		//reverse whole sentence 
+		reverseArray(s, 0, s.length-1);			
 
+		//reverse subarray
 		int last = 0;
 		for (int i=0; i<s.length; i++) {
 			if (s[i] == ' ') {
-				reverseArray(s, last, i-1);		//reverse subarray
+				reverseArray(s, last, i-1);	
 				last = i + 1;					//update last
 			}
-			/*if (i == s.length-1) {		//F: handle abc, hi!, no space value
-				reverseArray(s, last, i);
-		}*/
+		}
+		//revere the last word, if there is only one word this will solve the corner case
 		reverseArray(s, last, s.length-1);	
+		
+		for(int i=0; i<s.length; i++)
+			System.out.print(s[i]);
 	}
-	for(int i=0; i<s.length; i++)
-		System.out.println(s[i]);
-}
-public void reverseArray(char[] s, int l, int r) {
-	//swap
-	while (l < r) {
-		char temp = s[l];
-		s[l] = s[r];
-		s[r] = temp;
+	public void reverseArray(char[] s, int l, int r) {
+		//swap
+		while (l < r) {
+			char temp = s[l];
+			s[l] = s[r];
+			s[r] = temp;
 
-		l++;
-		r--;
+			l++;
+			r--;
+		}
 	}
-}
-public static void main(String[] args) {
-	n186_Reverse_Words_in_a_String_II obj = new n186_Reverse_Words_in_a_String_II();
-	//char[] s = {'t','h','e',' ','s','k','y'};
-	char[] s = {'h','i','!'};
-	//char[] s = {'a','b','c'};
-	obj.reverseWords(s);
-}
+	public static void main(String[] args) {
+		n186_Reverse_Words_in_a_String_II obj = new n186_Reverse_Words_in_a_String_II();
+		char[] s = {'t','h','e',' ','s','k','y',' ','h','i','g','h'};
+		obj.reverseWords(s);
+		
+		System.out.println();
+		char[] s1 = {'h','i','!'};
+		//char[] s = {'a','b','c'};
+		obj.reverseWords(s1);
+	}
 }
