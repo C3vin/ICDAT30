ceb9630a00be09b678f018d1fd16a531d5e52a3a
C3vin <spacemonkey3@gmail.com>
2017-12-04 22:48:58 -0800

    add LC 88

--- src/LeetCode/n088_Merge_Sorted_Array.java
+++ src/LeetCode/n088_Merge_Sorted_Array.java
@@ -7,11 +7,32 @@ package LeetCode;
 
 public class n088_Merge_Sorted_Array {
 	public void merge(int[] nums1, int m, int[] nums2, int n) {
-		
+		for(int i=m+n-1; i>=0; i--) {
+			if(m>0 && n>0) {
+				if(nums2[n-1] > nums1[m-1]) {
+					nums1[i] = nums2[n-1];
+					n--;
+				} else {
+					nums1[i] = nums1[m-1];
+					m--;
+				}
+			} else if(m>0) {
+				nums1[i] = nums1[m-1];
+				m--;
+			} else if(n>0) {
+				nums1[i] = nums2[n-1];
+				n--;
+			}
+		}
+		for(int i=0; i<nums1.length; i++)
+			System.out.println(nums1[i]);
 	}
 	
 	public static void main(String[] args) {
 		n088_Merge_Sorted_Array obj = new n088_Merge_Sorted_Array();
-		System.out.println(obj.merge(nums1, m, nums2, n));
+		
+		int[] nums1 = {1,3,5,0,0,0,0};
+		int[] nums2 = {2,4,6,7};
+		obj.merge(nums1, 3, nums2, 4);
 	}
 }
