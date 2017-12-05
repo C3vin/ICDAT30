0aa65413d9ee3f6a1aabc7ddd2e3b995ce0a55bd
C3vin <spacemonkey3@gmail.com>
2017-12-04 16:23:51 -0800

    update LC 144

--- src/LeetCode/n144_Binary_Tree_Preorder_Traversal.java
+++ src/LeetCode/n144_Binary_Tree_Preorder_Traversal.java
@@ -2,6 +2,7 @@ package LeetCode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class n144_Binary_Tree_Preorder_Traversal {
 	public class TreeNode {
@@ -10,6 +11,7 @@ public class n144_Binary_Tree_Preorder_Traversal {
 		TreeNode right;
 		TreeNode(int x) { val = x; }
 	}
+	//Recursion
 	public List<Integer> preorderTraversal(TreeNode root) {
 		List<Integer> res = new ArrayList<Integer>();
 		helper(root, res);
@@ -23,6 +25,23 @@ public class n144_Binary_Tree_Preorder_Traversal {
 		helper(root.right, res);
 	}
 
+	//iterate
+	public List<Integer> preorderTraversal2(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<TreeNode>();	
+		
+		while(root != null || !stack.isEmpty()) {
+			if(root != null) {
+				stack.add(root);
+				res.add(root.val);			//?
+				root = root.left;
+			} else {
+				root = stack.pop();
+				root = root.right;
+			}
+		}
+		return res;
+	}
 	public static void main(String[] args) {
 		n144_Binary_Tree_Preorder_Traversal obj = new n144_Binary_Tree_Preorder_Traversal();
 		TreeNode p1 = obj.new TreeNode(1);
@@ -32,5 +51,6 @@ public class n144_Binary_Tree_Preorder_Traversal {
 		p2.left = p3;
 
 		System.out.println(obj.preorderTraversal(p1));
+		System.out.println(obj.preorderTraversal2(p1));
 	}
 }
