96a2cc643b839aeac166b5e37e610a09c1146b08
C3vin <spacemonkey3@gmail.com>
2019-01-11 04:10:04 -0800

    add LC 929

---
+++ src/LeetCode/n929_Unique_Email_Addresses.java
@@ -0,0 +1,49 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Every email consists of a local name and a domain name, separated by the @ sign.
+For example, in alice@leetcode.com, alice is the local name, and leetcode.com is the domain name.
+Besides lowercase letters, these emails may contain '.'s or '+'s.
+If you add periods ('.') between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name.  For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.  (Note that this rule does not apply for domain names.)
+If you add a plus ('+') in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered, for example m.y+name@email.com will be forwarded to my@email.com.  (Again, this rule does not apply for domain names.)
+It is possible to use both of these rules at the same time.
+Given a list of emails, we send one email to each address in the list.  How many different addresses actually receive mails? 
+
+Example 1:
+Input: ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails
+
+Note:
+1 <= emails[i].length <= 100
+1 <= emails.length <= 100
+Each emails[i] contains exactly one '@' character.
+ */
+public class n929_Unique_Email_Addresses {
+	public int numUniqueEmails(String[] emails) {
+		if(emails == null || emails.length == 0) 
+			return 0;
+		
+		HashSet<String> localNames = new HashSet<>();
+		for(String email : emails) {
+			String[] arr = email.split("@");
+			String localName = arr[0];
+			int indexOfPlus = localName.indexOf("+");
+			if(indexOfPlus != -1) {
+				localName = localName.substring(0, indexOfPlus);
+			}
+			//why replaceAll?
+			localName = localName.replaceAll("\\.", "");
+			localNames.add(localName + "@" + arr[1]);
+		}
+		return localNames.size();
+	}
+	public static void main(String[] args) {
+		n929_Unique_Email_Addresses obj = new n929_Unique_Email_Addresses();
+		String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+		System.out.println(obj.numUniqueEmails(emails));
+	}
+}
