562e29fa34405fe7b5696bb109cdbb55c7daf5a0
C3vin <spacemonkey3@gmail.com>
2019-01-18 02:54:09 -0800

    update LC 859

--- src/LeetCode/n859_Buddy_Strings.java
+++ src/LeetCode/n859_Buddy_Strings.java
@@ -1,5 +1,9 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /*
 Given two strings A and B of lowercase letters, return true if and only if we can swap two letters in A so that the result equals B.
 Example 1:
@@ -42,20 +46,51 @@ public class n859_Buddy_Strings {
 		}
 		
 		for(int i=0; i<a.length; i++) {
-			if(diff == 0 && a[i] > 1) {		//why > 1
+			if(diff == 0 && a[i] > 1) {		//why > 1, cuz a have 26 ch e.g. ['1','1','0'...]
 				return true;
 			}
-			if(a[i] != b[i]) {
+			if(a[i] != b[i]) {				//compare two array value
 				return false;
 			}
 		}
 		return diff == 2;
 	}
+	
+	public boolean buddyStrings2(String A, String B) {	
+		if(A.length() != B.length()) {
+			return false;
+		}
+		
+		//deal ab:ab, aa:aa
+		if(A.equals(B)) {	
+			HashSet<Character> set = new HashSet<Character>();
+			for(char c : A.toCharArray()) {
+				set.add(c);
+			}
+			return set.size() < A.length();
+		}
+		
+		List<Integer> diff = new ArrayList<Integer>();
+		for(int i=0; i<A.length(); i++) {
+			if(A.charAt(i) != B.charAt(i)) {
+				diff.add(i);
+			}
+		}
+		return diff.size() == 2 && A.charAt(diff.get(0)) == B.charAt(diff.get(1)) && A.charAt(diff.get(1)) == B.charAt(diff.get(0));
+	}
+	
 	public static void main(String[] args) {
 		n859_Buddy_Strings obj = new n859_Buddy_Strings();
 		System.out.println(obj.buddyStrings("ab", "ba"));
 		System.out.println(obj.buddyStrings("ab", "ab"));
+		System.out.println(obj.buddyStrings("aa", "aa"));
 		System.out.println(obj.buddyStrings("aaaaaaabc", "aaaaaaacb"));
 		System.out.println(obj.buddyStrings("", "aa"));
+		System.out.println();
+		System.out.println(obj.buddyStrings2("ab", "ba"));
+		System.out.println(obj.buddyStrings2("ab", "ab"));
+		System.out.println(obj.buddyStrings2("aa", "aa"));
+		System.out.println(obj.buddyStrings2("aaaaaaabc", "aaaaaaacb"));
+		System.out.println(obj.buddyStrings2("", "aa"));
 	}
 }
