9067f9ee7ad8085e21316f66e5adf3943ef55857
yangdani <yangdani@USYANGDANI01.microfocus.com>
2019-01-22 16:23:06 -0800

    update LC 067

--- src/LeetCode/n067_Add_Binary.java
+++ src/LeetCode/n067_Add_Binary.java
@@ -1,16 +1,30 @@
 package LeetCode;
 
-//a = "11", b = "1", Return "100".
+/*
+ * Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+ */
 public class n067_Add_Binary {
 	public String addBinary(String a, String b) {
-		if(a==null || a.length() == 0) return b;
-		if(b==null || b.length() == 0) return a;
+		//check a and b first
+		if(a == null || a.length() == 0) 
+			return b;
+		if(b == null || b.length() == 0) 
+			return a;
 		
-		int carry=0;
+		int carry = 0;
 		StringBuilder sb = new StringBuilder();
 		
-		int i=a.length()-1;
-		int j=b.length()-1;
+		int i = a.length()-1;
+		int j = b.length()-1;
 		
 		while(i >=0 || j >=0) {				// || not &&
 			if(i>=0) {
@@ -31,7 +45,7 @@ public class n067_Add_Binary {
 		return sb.toString();
 	}
 	
-	//if don't want to use sb.insert 
+	//if don't want to use sb.insert, just reverse in the end!
 	public String addBinary2(String a, String b) {
 		StringBuilder sb = new StringBuilder();
 		int i = a.length()-1;
@@ -54,11 +68,13 @@ public class n067_Add_Binary {
 		if(carry != 0)
 			sb.append(carry);
 			
-		return sb.reverse().toString();			//still need to use reverse() 
+		return sb.reverse().toString();			//still need to use reverse() !!!
 	}
 	public static void main(String[] args) {
 		n067_Add_Binary obj = new n067_Add_Binary();
 		System.out.println(obj.addBinary("11", "1"));
+		System.out.println(obj.addBinary("1010", "1011"));
 		System.out.println(obj.addBinary2("11", "1"));
+		System.out.println(obj.addBinary2("1010", "1011"));
 	}
 }
