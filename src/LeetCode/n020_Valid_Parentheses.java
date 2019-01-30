a1d36492572e3cc82d4b72dc5c1fb9415875d0f5
C3vin <spacemonkey3@gmail.com>
2019-01-30 03:50:52 -0800

    update LC 20

--- src/LeetCode/n020_Valid_Parentheses.java
+++ src/LeetCode/n020_Valid_Parentheses.java
@@ -1,12 +1,15 @@
 package LeetCode;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 //Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 //The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
 public class n020_Valid_Parentheses {
+	//sol1
 	public boolean isValid(String s) {
-		if(s.length() == 0 || s.length() == 1) return false;
+		if(s.length() == 0 || s.length() == 1) 
+			return false;
 
 		LinkedList<Character> stack = new LinkedList<Character>();
 		for(int i=0; i<s.length(); i++) {
@@ -21,7 +24,7 @@ public class n020_Valid_Parentheses {
 					if(tmp != '(')
 						return false;
 				} else if(s.charAt(i) == ']') {
-					if(tmp != '[')
+					if(tmp != '[') 
 						return false;
 				} else if(s.charAt(i) == '}') {
 					if(tmp != '{')
@@ -35,11 +38,39 @@ public class n020_Valid_Parentheses {
 		return false;
 	}
 
+	//sol2
+	public boolean isValid2(String s) {
+/*		if(s.length() == 0 || s.length() == 1) {
+			return false;			//deal with "" case
+		}*/
+
+		HashMap<Character, Character> map = new HashMap<Character, Character>();
+		map.put(')', '(');
+		map.put(']', '[');
+		map.put('}', '{');
+		
+		LinkedList<Character> stack = new LinkedList<Character>();
+		for(int i=0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			if(c == '(' || c == '[' || c == '{') { 			
+				stack.push(c);
+			} else {
+				if(stack.size() == 0 || stack.pop() != map.get(c)) {
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();
+	}
+
 	public static void main(String[] args) {
 		n020_Valid_Parentheses obj = new n020_Valid_Parentheses();
-		String s = "()[]{}";
-		String s1 = "){";
-		System.out.println(obj.isValid(s));
-		System.out.println(obj.isValid(s1));
+		
+		System.out.println(obj.isValid("()[]{}"));
+		System.out.println(obj.isValid("){"));
+		System.out.println(obj.isValid2("()[]{}"));
+		System.out.println(obj.isValid2("){"));
+		System.out.println(obj.isValid(""));
+		System.out.println(obj.isValid2(""));
 	}
 }
