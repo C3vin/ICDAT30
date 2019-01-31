42bd2544b74cfd84da14a4fba8de76be5184fdcd
C3vin <spacemonkey3@gmail.com>
2019-01-30 22:24:58 -0800

    update LC 680

--- src/LeetCode/n680_Valid_Palindrome_II.java
+++ src/LeetCode/n680_Valid_Palindrome_II.java
@@ -12,23 +12,27 @@ public class n680_Valid_Palindrome_II {
 			i++;
 			j--;
 		}
-		System.out.println(i + " : " + j);
 		//case
 		if(i == j) // F: Need this, e.g. 'aba'
 			return true;
 		
-		if(isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1))
+		if(isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1))	//try left & right delete one char
 			return true;
 		
 		return false;
 	}
 	private boolean isPalindrome(String s, int i, int j) {
 		while(i < j) {
-			if(s.charAt(i) == s.charAt(j)) {
+			if(s.charAt(i) != s.charAt(j)) {
+				return false;
+			}
+			i++;
+			j--;
+		/*	if(s.charAt(i) == s.charAt(j)) {
 				i++;
 				j--;
 			} else 
-				return false;
+				return false; */
 		}
 		return true;
 	}
@@ -37,5 +41,7 @@ public class n680_Valid_Palindrome_II {
 		n680_Valid_Palindrome_II obj = new n680_Valid_Palindrome_II();
 		System.out.println(obj.validPalindrome("aba"));
 		System.out.println(obj.validPalindrome("abca"));
+		System.out.println(obj.validPalindrome("aaaba"));
+		System.out.println(obj.validPalindrome("abcda"));
 	}
 }
