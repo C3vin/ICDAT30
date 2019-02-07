cbaccb2702e76e9a0b44dea8eeaa0a390f44cdff
C3vin <spacemonkey3@gmail.com>
2019-02-07 04:12:53 -0800

    update LC 38

--- src/LeetCode/n038_Count_and_Say.java
+++ src/LeetCode/n038_Count_and_Say.java
@@ -1,9 +1,21 @@
 package LeetCode;
 
+/*
+The count-and-say sequence is the sequence of integers with the first five terms as following:
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+ */
 public class n038_Count_and_Say {
 	public String countAndSay(int n) {
-				if (n <= 0)
+		if (n <= 0) {
 			return null;
+		}
 
 		String result = "1";
 		int i = 1;
