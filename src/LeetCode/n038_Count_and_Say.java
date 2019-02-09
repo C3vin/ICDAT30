a965fa27692921a53711231087b51b9db81198c5
C3vin <spacemonkey3@gmail.com>
2019-02-09 15:03:10 -0800

    update LC 38

--- src/LeetCode/n038_Count_and_Say.java
+++ src/LeetCode/n038_Count_and_Say.java
@@ -18,32 +18,35 @@ public class n038_Count_and_Say {
 		}
 
 		String result = "1";
-		int i = 1;
-		while (i < n) {
+		int round = 1;
+		while (round < n) {
 			StringBuilder sb = new StringBuilder();
 			int count = 1;
-			for (int j=1; j<result.length(); j++) {
-				if (result.charAt(j) == result.charAt(j - 1)) {
+			for (int i=1; i<result.length(); i++) {
+				//same value
+				if (result.charAt(i) == result.charAt(i - 1)) {
 					count++;
 				} else {
+					//new value
 					sb.append(count);
-					sb.append(result.charAt(j - 1));
-					count = 1;
+					sb.append(result.charAt(i - 1));			//need i - 1 
+					count = 1;		//reset
 				}
 			}
 			sb.append(count);
 			sb.append(result.charAt(result.length() - 1));
 			result = sb.toString();
-			i++;
+			round++;				//F: ++
 		}
 		return result;
 	}
-
+	
 	public static void main(String[] args) {
 		n038_Count_and_Say obj = new n038_Count_and_Say();
 		System.out.println(obj.countAndSay(1));
 		System.out.println(obj.countAndSay(2));
 		System.out.println(obj.countAndSay(3));
 		System.out.println(obj.countAndSay(4));
+		System.out.println(obj.countAndSay(5));
 	}
 }
