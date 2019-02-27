e19b6b86d732dde30ea8d8459d4fa17190e06116
yangdani <yangdani@USYANGDANI01.microfocus.com>
2019-02-27 15:39:15 -0800

    update LC 40

--- src/LeetCode/n040_Combination_Sum_II.java
+++ src/LeetCode/n040_Combination_Sum_II.java
@@ -65,11 +65,11 @@ public class n040_Combination_Sum_II {
 	public List<List<Integer>> combinationSum2_2(int[] candidates, int target) {
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
 		Arrays.sort(candidates);
-		backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+		dfs(list, new ArrayList<Integer>(), candidates, target, 0);
 		return list;
 	}
 
-	private void backtrack(List<List<Integer>> res, List<Integer> tmp, int[] candidates, int target, int start) {
+	private void dfs(List<List<Integer>> res, List<Integer> tmp, int[] candidates, int target, int start) {
 		if(target == 0) {
 			res.add(new ArrayList<Integer>(tmp));
 			return;
@@ -77,11 +77,14 @@ public class n040_Combination_Sum_II {
 
 		for(int i=start; i<candidates.length; i++) {
 			//target < candidates[i]
+			if(target < 0) {
+				return;
+			}
 			if(i > start  && candidates[i] == candidates[i-1]) {			//i > start, not the first num			
 				continue;
 			}
 			tmp.add(candidates[i]);
-			backtrack(res, tmp, candidates, target-candidates[i], i+1);		//why i+1, make sure no duplicates
+			dfs(res, tmp, candidates, target-candidates[i], i+1);		//why i+1, make sure no duplicates
 			tmp.remove(tmp.size()-1);
 		}
 	}
@@ -92,5 +95,10 @@ public class n040_Combination_Sum_II {
 		int target = 8;
 		System.out.println(obj.combinationSum2(candidates, target));
 		System.out.println(obj.combinationSum2_2(candidates, target));
+		
+		int[] candidates1 = {14,6,25,9,30,20,33,34,28,30,16,12,31,9,9,12,34,16,25,32,8,7,30,12,33,20,21,29,24,17,27,34,11,17,30,6,32,21,27,17,16,8,24,12,12,28,11,33,10,32,22,13,34,18,12};
+		int target1 = 27;
+		System.out.println(obj.combinationSum2(candidates1, target1));
+		System.out.println(obj.combinationSum2_2(candidates1, target1));
 	}
 }
