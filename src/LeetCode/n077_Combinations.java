1b8765debb802e58a264762c52dbf0897155894f
C3vin <spacemonkey3@gmail.com>
2019-03-02 00:18:56 -0800

    update LC 77 & 78

--- src/LeetCode/n077_Combinations.java
+++ src/LeetCode/n077_Combinations.java
@@ -2,25 +2,42 @@ package LeetCode;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
 
-@Alg(type="DFS", com="NA", level="med", num=77)
+Example:
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+ */
 public class n077_Combinations {
 	public List<List<Integer>> combine(int n, int k) {
-		List<List<Integer>> res = new ArrayList<List<Integer>>();  
-		List<Integer> tmp = new ArrayList<Integer>();  
-		dfs(res, tmp, k, n, 1);  //because it need to begin from 1
-		return res;  
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+		dfs2(n, k, 1, tmp, res);						//because it need to begin from 1~n
+		return res;
 	}
-	public void dfs(List<List<Integer>> res, List<Integer> tmp, int k, int n, int start){  
-		for(int i=start; i<=n; i++) {  //F: need start can't use '1', // try each possibility number in current position
-			tmp.add(i); 	//F: !!! 
+	private void dfs2(int n, int k, int start, List<Integer> tmp, List<List<Integer>> res) {
+		if(tmp.size() > k) {				
+			return;
+		}
+		for(int i=start; i<=n; i++) {
+			tmp.add(i);
 			if(tmp.size() == k) {
-				res.add(new ArrayList<Integer>(tmp));  
+				res.add(new ArrayList<Integer>(tmp));
 			}
-			dfs(res,tmp,k,n,i+1);  // after selecting number for current position, process next position 
-			tmp.remove(tmp.size()-1); //clear the current position to try next possible number 
-		}  
-	} 
+			dfs2(n, k, i+1, tmp, res);					// after selecting number for current position, process next position 
+			tmp.remove(tmp.size()-1);					//clear the current position to try next possible number 
+
+		}
+	}
 	public static void main(String[] args) {
 		n077_Combinations obj = new n077_Combinations();
 		System.out.println(obj.combine(4, 2));
--- src/LeetCode/n078_Subsets.java
+++ src/LeetCode/n078_Subsets.java
@@ -3,9 +3,14 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.List;
 
-@Alg(type="NP", com="AA,FB", level="Med", num=90)
 /**
-nums = [1,2,3]
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
 [
   [3],
   [1],
@@ -15,28 +20,25 @@ nums = [1,2,3]
   [2,3],
   [1,2],
   []
-]  
+]
  */
 public class n078_Subsets {
-	//DFS
 	public List<List<Integer>> subsets(int[] nums) {
-		List<List<Integer>> res = new ArrayList<List<Integer>>();  
-		List<Integer> tmp = new ArrayList<Integer>();  
-		//Arrays.sort(nums);  
-		//res.add(tmp);  
-		dfs(res,tmp,nums,0);  
-		return res;  
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+		dfs(nums, 0, tmp, res);
+		return res;
 	}
-	public void dfs(List<List<Integer>> res, List<Integer> tmp, int[] S, int start){  
-		List<Integer> list = new ArrayList<Integer>(tmp);
-		res.add(list);  
-
-		for(int i=start; i<S.length; i++){  
-			tmp.add(S[i]); 
-			dfs(res, tmp, S, i+1);  
-			tmp.remove(tmp.size()-1);  
-		}  
-	}  
+	//dfs
+	private void dfs(int[] nums, int start, List<Integer> tmp, List<List<Integer>> res) {
+		res.add(new ArrayList<Integer>(tmp));
+		
+		for(int i=start; i<nums.length; i++) {
+			tmp.add(nums[i]);
+			dfs(nums, i+1, tmp, res);
+			tmp.remove(tmp.size()-1);
+		}
+	}
 	public static void main(String[] args) {
 		n078_Subsets obj = new n078_Subsets();
 		int[] nums = {1, 2, 3};
