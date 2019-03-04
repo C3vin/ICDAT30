a3fd6d6508a3f73a720283cb5b98bbc2baa0ffe0
C3vin <spacemonkey3@gmail.com>
2019-03-03 19:35:39 -0800

    update LC 46 & 47

--- src/LeetCode/n046_Permutations.java
+++ src/LeetCode/n046_Permutations.java
@@ -4,7 +4,21 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-@Alg(type="NP", com="LL&M$", level="med", num=46)
+/*
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+ */
 public class n046_Permutations {
 	//Need one res, tmp, used(visited) for dfs
 	public List<List<Integer>> permute(int[] nums) {
@@ -32,9 +46,37 @@ public class n046_Permutations {
 			used[i] = false;  //!!! reset
 		}
 	}
+
+	//sol2
+	public List<List<Integer>> permute2(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+		boolean[] used = new boolean[nums.length];
+		dfs(nums, used, tmp, res);
+		return res;
+	}
+
+	//dfs
+	private void dfs(int[] nums, boolean[] used, List<Integer> tmp, List<List<Integer>> res) {
+		if(tmp.size() == nums.length) {
+			res.add(new ArrayList<Integer>(tmp));
+			return;
+		}
+		for(int i=0; i<nums.length; i++) {
+			if(used[i]) {
+				continue;
+			}
+			tmp.add(nums[i]);
+			used[i] = true;
+			dfs(nums, used, tmp, res);
+			tmp.remove(tmp.size()-1);
+			used[i] = false;
+		}
+	}
 	public static void main(String[] args) {
 		n046_Permutations obj = new n046_Permutations();
 		int[] nums = {1,2,3};
 		System.out.println(obj.permute(nums));
+		System.out.println(obj.permute2(nums));
 	}
 }
--- src/LeetCode/n047_Permutations_II.java
+++ src/LeetCode/n047_Permutations_II.java
@@ -4,8 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+/*
+Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
-@Alg(type="NP", com="LL&M$", level="med", num=47)
+Example:
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+ */
 public class n047_Permutations_II {
 	public List<List<Integer>> permuteUnique(int[] nums) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
@@ -38,9 +48,38 @@ public class n047_Permutations_II {
 		}
 	}
 
+	//sol2
+	public List<List<Integer>> permuteUnique2(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+		boolean[] used = new boolean[nums.length];
+		Arrays.sort(nums);										//need sort first!
+		dfs(nums, used, tmp, res);
+		return res;
+	}
+	//dfs
+	private void dfs(int[] nums, boolean[] used, List<Integer> tmp, List<List<Integer>> res) {
+		if(tmp.size() == nums.length) {
+			res.add(new ArrayList<Integer>(tmp));
+			return;
+		}
+		for(int i=0; i<nums.length; i++) {	
+			//why length-1? cuz handle later array i+1
+			if(used[i] || i<nums.length-1 && nums[i] == nums[i+1] && !used[i+1]) {		
+				continue;										//remove duplicates
+			}
+			tmp.add(nums[i]);
+			used[i] = true;
+			dfs(nums, used, tmp, res);
+			tmp.remove(tmp.size()-1);
+			used[i] = false;
+		}
+	}
+
 	public static void main(String[] args) {
 		n047_Permutations_II obj = new n047_Permutations_II();
 		int[] nums = {1,1,2};
 		System.out.println(obj.permuteUnique(nums));
+		System.out.println(obj.permuteUnique2(nums));
 	}
 }
