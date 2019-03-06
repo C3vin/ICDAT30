e71d0daefd78254fda03e64296284204d2ccf8b7
C3vin <spacemonkey3@gmail.com>
2019-03-06 03:52:18 -0800

    update LC 698

--- src/LeetCode/n698_Partition_to_K_Equal_Sum_Subsets.java
+++ src/LeetCode/n698_Partition_to_K_Equal_Sum_Subsets.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /*
 Given an array of integers nums and a positive integer k, find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
 
@@ -12,27 +15,44 @@ Note:
 1 <= k <= len(nums) <= 16.
 0 < nums[i] < 10000.
  */
-public class n698_Partition_to_K_Equal_Sum_Subsets {
+public class n698_Partition_to_K_Equal_Sum_Subsets {	
 	public boolean canPartitionKSubsets(int[] nums, int k) {
 		int sum = 0;
-		for(int num:nums)sum += num;
-		if(k <= 0 || sum%k != 0)return false;
+		for(int num : nums) {
+			sum += num;
+		}
+		if(k<=0 || sum%k != 0) {
+			return false;
+		}
 		int[] visited = new int[nums.length];
-		return canPartition(nums, visited, 0, k, 0, 0, sum/k);
+		int target = sum/k;
+		
+		Arrays.sort(nums);
+		return dfs(nums, k, 0, visited, target, 0, 0);
 	}
-
-	public boolean canPartition(int[] nums, int[] visited, int start_index, int k, int cur_sum, int cur_num, int target){
-		if(k==1)return true;
-		if(cur_sum == target && cur_num>0)return canPartition(nums, visited, 0, k-1, 0, 0, target);
-		for(int i = start_index; i<nums.length; i++){
-			if(visited[i] == 0){
+	
+	private boolean dfs(int[] nums, int k, int start, int[] visited, int target, int cur_sum, int cur_num) {
+		if(k == 1) {
+			return true;
+		}
+		
+		if(cur_sum == target && cur_num > 0) {
+			System.out.println(k);
+			return dfs(nums, k-1, 0, visited, target, 0, 0);
+		}
+		
+		for(int i=start; i<nums.length; i++) {
+			if(visited[i] == 0) {
 				visited[i] = 1;
-				if(canPartition(nums, visited, i+1, k, cur_sum + nums[i], cur_num++, target))return true;
+				if(dfs(nums, k, i+1, visited, target, cur_sum+nums[i], cur_num++)) {
+					return true;
+				}
 				visited[i] = 0;
 			}
 		}
 		return false;
 	}
+
 	public static void main(String[] args) {
 		n698_Partition_to_K_Equal_Sum_Subsets obj = new n698_Partition_to_K_Equal_Sum_Subsets();
 		int[] nums = {4, 3, 2, 3, 5, 2, 1};
