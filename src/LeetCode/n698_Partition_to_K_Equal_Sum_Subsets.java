59364b889851cc450d6bc2fe5d52476cc81f4c2f
C3vin <spacemonkey3@gmail.com>
2019-03-07 01:08:02 -0800

    update LC 698

--- src/LeetCode/n698_Partition_to_K_Equal_Sum_Subsets.java
+++ src/LeetCode/n698_Partition_to_K_Equal_Sum_Subsets.java
@@ -24,34 +24,36 @@ public class n698_Partition_to_K_Equal_Sum_Subsets {
 		if(k<=0 || sum%k != 0) {
 			return false;
 		}
-		int[] visited = new int[nums.length];
+		boolean[] visited = new boolean[nums.length];
 		int target = sum/k;
 		
-		Arrays.sort(nums);
-		return dfs(nums, k, 0, visited, target, 0, 0);
+		//Arrays.sort(nums);
+		return dfs(nums, k, 0, visited, target, 0);
 	}
 	
 	//Ref: https://www.youtube.com/watch?v=qpgqhp_9d1s
 	//https://github.com/bephrem1/backtobackswe/blob/master/Dynamic%20Programming%2C%20Recursion%2C%20%26%20Backtracking/partitionIntoKEqualSumSubsets.java
-	private boolean dfs(int[] nums, int k, int start, int[] visited, int target, int curSum, int curCount) {
+	private boolean dfs(int[] nums, int k, int start, boolean[] visited, int target, int curSum) {
 		if(k == 1) {
-			//System.out.println(k + " : " +curCount);
 			return true;
 		}
 		
-		//match the target, so k-1 to the next 
-		if(curSum == target && curCount > 0) {
-			return dfs(nums, k-1, 0, visited, target, 0, 0);
+		//Bucket full. continue the recursion with k - 1 as the new k value, 
+		//BUT the target stays the same. We just have 1 less bucket to fill.
+		if(curSum == target) {
+			return dfs(nums, k-1, 0, visited, target, 0);
 		}
 		
+		//Try all values from 'iterationStart' to the end of the array ONLY if:
+		//1.) They have not been used up to this point in the recursion's path
+	    //2.) They do not overflow the current bucket we are filling
 		for(int i=start; i<nums.length; i++) {
-			if(visited[i] == 0) {
-				visited[i] = 1;				//use curCount to record the number of elements in the current subset
-				System.out.println("@: " + curCount);
-				if(dfs(nums, k, i+1, visited, target, curSum+nums[i], curCount+1)) {			//why curCount++? 
+			if(!visited[i] && curSum+nums[i] <= target) {
+				visited[i] = true;		 
+				if(dfs(nums, k, i+1, visited, target, curSum+nums[i])) {			 
 					return true;
 				}
-				visited[i] = 0;
+				visited[i] = false;
 			}
 		}
 		return false;
@@ -61,6 +63,6 @@ public class n698_Partition_to_K_Equal_Sum_Subsets {
 		n698_Partition_to_K_Equal_Sum_Subsets obj = new n698_Partition_to_K_Equal_Sum_Subsets();
 		int[] nums = {4, 3, 2, 3, 5, 2, 1};
 		int k = 4;
-		System.out.println(obj.canPartitionKSubsets(nums, 20));
+		System.out.println(obj.canPartitionKSubsets(nums, 4));
 	}
 }
