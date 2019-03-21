abbcdc0e8f6a84d48a68ba32281e115ce99008b8
yangdani <yangdani@USYANGDANI01.microfocus.com>
2019-03-21 16:51:42 -0700

    update LC 241

--- src/LeetCode/n241_Different_Ways_to_Add_Parentheses.java
+++ src/LeetCode/n241_Different_Ways_to_Add_Parentheses.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -22,13 +23,41 @@ Explanation:
 ((2*(3-4))*5) = -10 
 (2*((3-4)*5)) = -10 
 (((2*3)-4)*5) = 10
-*/
+ */
 public class n241_Different_Ways_to_Add_Parentheses {
 	public List<Integer> diffWaysToCompute(String input) {
-		
+		List<Integer> res = new ArrayList<Integer>();
+		for(int i=0; i<input.length(); i++) {
+			char c = input.charAt(i);
+			if(c == '-' || c == '+' || c == '*') {
+				String a = input.substring(0, i);
+				String b = input.substring(i+1, input.length());
+				List<Integer> a1 = diffWaysToCompute(a);
+				List<Integer> b1 = diffWaysToCompute(b);
+
+				for(int x : a1) {
+					for(int y : b1) {
+						if(c == '-') {
+							res.add(x - y);
+						} else if(c == '+') {
+							res.add(x + y);
+						} else if(c == '*') {
+							res.add(x * y);
+						}
+					}
+				}
+
+			}
+		}
+		if(res.size() == 0) {
+			//res.add(Integer.valueOf(input));
+			res.add(Integer.parseInt(input));
+		}
+		return res;
 	}
 	public static void main(String[] args) {
 		n241_Different_Ways_to_Add_Parentheses obj = new n241_Different_Ways_to_Add_Parentheses();
 		System.out.println(obj.diffWaysToCompute("2-1-1"));
+		System.out.println(obj.diffWaysToCompute("2*3-4*5"));
 	}
 }
