a4978bd7b6f16a3db64acb2f75451de9af4e0b24
yangdani <yangdani@USYANGDANI01.microfocus.com>
2019-03-21 17:58:02 -0700

    add LC 282

---
+++ src/LeetCode/n282_Expression_Add_Operators.java
@@ -0,0 +1,66 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given a string that contains only digits 0-9 and a target value, return all possibilities to 
+add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
+
+Example 1:
+Input: num = "123", target = 6
+Output: ["1+2+3", "1*2*3"] 
+
+Example 2:
+Input: num = "232", target = 8
+Output: ["2*3+2", "2+3*2"]
+
+Example 3:
+Input: num = "105", target = 5
+Output: ["1*0+5","10-5"]
+
+Example 4:
+Input: num = "00", target = 0
+Output: ["0+0", "0-0", "0*0"]
+
+Example 5:
+Input: num = "3456237490", target = 9191
+Output: []
+*/
+public class n282_Expression_Add_Operators {
+	public List<String> addOperators(String num, int target) {
+		List<String> res = new ArrayList<String>();
+		if(num == null || num.length() == 0) {
+			return res;
+		}
+		dfs(num, target, 0, "", 0, 0, res);
+		return res;
+	}
+	
+	private void dfs(String num, int target, int start, String path, long val, long pre, List<String> res) {
+		if(start == num.length()) {
+			if(val == target) {
+				res.add(path);
+				return;
+			}
+		}
+		for(int i=start; i<num.length(); i++) {
+			if(i != start && num.charAt(start) == '0') {
+				break; 			//?
+			}
+			long cur = Long.parseLong(num.substring(start, i+1));
+			if(start == 0) {
+				dfs(num, target, i+1, path+cur, cur, cur, res);
+			} else {
+				dfs(num, target, i+1, path+"+"+cur, val+cur, cur, res);
+				dfs(num, target, i+1, path+"-"+cur, val-cur, -cur, res);
+				dfs(num, target, i+1, path+"*"+cur, val-pre+pre*cur, pre*cur, res);		//?
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		n282_Expression_Add_Operators obj = new n282_Expression_Add_Operators();
+		System.out.println(obj.addOperators("123", 6));
+	}
+}
