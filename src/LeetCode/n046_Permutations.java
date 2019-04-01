b61271fabf27f4658c9f1dcb83ed6e40cf57e9f4
C3vin <spacemonkey3@gmail.com>
2019-03-31 22:10:03 -0700

    add LC 46

--- src/LeetCode/n046_Permutations.java
+++ src/LeetCode/n046_Permutations.java
@@ -20,42 +20,13 @@ Output:
 ]
  */
 public class n046_Permutations {
-	//Need one res, tmp, used(visited) for dfs
 	public List<List<Integer>> permute(int[] nums) {
-		List<List<Integer>> res = new ArrayList<List<Integer>>();
-		boolean[] used = new boolean[nums.length];
-		List<Integer> tmp = new LinkedList<Integer>();
-		helper(nums, res, tmp, used);
-		return res;
-	}
-	//DFS
-	//Need one list to create space [A,B,C] and add to res
-	private void helper(int[] nums, List<List<Integer>> res, List<Integer> tmp, boolean[] used) {
-		if(tmp.size() == nums.length) {
-			List<Integer> list = new LinkedList<Integer>(tmp);		//why? cuz tmp will be change
-			res.add(list);
-		}
-		for(int i=0; i<nums.length; i++) {
-			if(used[i])
-				continue;
-			//add element and do check next 
-			used[i] = true;
-			tmp.add(nums[i]);
-			helper(nums, res, tmp, used);
-			tmp.remove(tmp.size()-1); 	
-			used[i] = false;  //!!! reset
-		}
-	}
-
-	//sol2
-	public List<List<Integer>> permute2(int[] nums) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
 		List<Integer> tmp = new ArrayList<Integer>();
 		boolean[] used = new boolean[nums.length];
 		dfs(nums, used, tmp, res);
 		return res;
 	}
-
 	//dfs
 	private void dfs(int[] nums, boolean[] used, List<Integer> tmp, List<List<Integer>> res) {
 		if(tmp.size() == nums.length) {
@@ -70,13 +41,64 @@ public class n046_Permutations {
 			used[i] = true;
 			dfs(nums, used, tmp, res);
 			tmp.remove(tmp.size()-1);
-			used[i] = false;
+			used[i] = false;							//reset
 		}
+	}
+	
+	//sol2
+	public List<List<Integer>> permute2(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> tmp = new ArrayList<Integer>();
+		dfs2(nums, tmp, res);
+		return res;
+	}
+	private void dfs2(int[] nums, List<Integer> tmp, List<List<Integer>> res) {
+		if(tmp.size() == nums.length) {
+			res.add(new ArrayList<Integer>(tmp));
+			return;
+		}
+		for(int i=0; i<nums.length; i++) {
+			if(tmp.contains(nums[i])) {					//smart! not need used!
+				continue;
+			}
+			tmp.add(nums[i]);
+			dfs2(nums, tmp, res);
+			tmp.remove(tmp.size()-1);
+		}
+		
+	}
+	
+	//sol3
+	public List<List<Integer>> permute3(int[] nums) {
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		dfs3(nums, 0, res);
+		return res;
+	}
+	private void dfs3(int[] nums, int index, List<List<Integer>> res) {
+		if(index == nums.length) {
+			List<Integer> tmp = new ArrayList<Integer>();
+			for(int num : nums) {
+				tmp.add(num);
+			}
+			res.add(new ArrayList<Integer>(tmp));
+			return;
+		}
+		for(int i=index; i<nums.length; i++) {
+			swap(nums, index, i);
+			dfs3(nums, index+1, res);
+			swap(nums, index, i);
+		}
+	}
+	private void swap(int[] nums, int l, int r) {
+		int tmp = nums[l];
+		nums[l] = nums[r];
+		nums[r] = tmp;
 	}
 	public static void main(String[] args) {
 		n046_Permutations obj = new n046_Permutations();
 		int[] nums = {1,2,3};
 		System.out.println(obj.permute(nums));
 		System.out.println(obj.permute2(nums));
+		System.out.println(obj.permute3(nums));
 	}
 }
