542496117a6b7779ea60d01b6d98d9d004cbf3e7
C3vin <spacemonkey3@gmail.com>
2019-04-08 03:50:57 -0700

    update LC 345

--- src/LeetCode/n345_Reverse_Vowels_of_a_String.java
+++ src/LeetCode/n345_Reverse_Vowels_of_a_String.java
@@ -3,11 +3,24 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.List;
 
-//Write a function that takes a string as input and reverse only the vowels of a string.
-//Given s = "hello", return "holle".
-//Given s = "leetcode", return "leotcede".
+/*
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+Input: "hello"
+Output: "holle"
+
+Example 2:
+Input: "leetcode"
+Output: "leotcede"
+
+Note:
+The vowels does not include the letter "y".
+ */
 
 public class n345_Reverse_Vowels_of_a_String {
+	//!!! reverse first and last vowels(a,e,i,o,u), and so on...
+	
 	public String reverseVowels(String s) {
 		List<Character> list = new ArrayList<Character>();
 		list.add('a');
@@ -43,13 +56,7 @@ public class n345_Reverse_Vowels_of_a_String {
 		}
 		return new String(ch);
 	}
-	public boolean checkVowel(char c) {
-		if ('a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c
-				|| 'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c) {
-			return true;
-		}
-		return false;
-	}
+
 	public String reverseVowels2(String s) {
 		int start=0;
 		int end=s.length()-1;
@@ -73,10 +80,18 @@ public class n345_Reverse_Vowels_of_a_String {
 		}
 		return new String(ch);
 	}
+	public boolean checkVowel(char c) {
+		if ('a' == c || 'e' == c || 'i' == c || 'o' == c || 'u' == c
+				|| 'A' == c || 'E' == c || 'I' == c || 'O' == c || 'U' == c) {
+			return true;
+		}
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		n345_Reverse_Vowels_of_a_String obj = new n345_Reverse_Vowels_of_a_String();
-		//System.out.println(obj.reverseVowels("hello"));
-		//System.out.println(obj.reverseVowels("leetcode"));
+		System.out.println(obj.reverseVowels("hello"));
+		System.out.println(obj.reverseVowels("leetcode"));
 		System.out.println(obj.reverseVowels2("hello"));
 		System.out.println(obj.reverseVowels2("leetcode"));
 	}
