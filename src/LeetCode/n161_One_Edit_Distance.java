0a420d7dfb9e39951ddaa1c7faabc78e99b00db2
yangdani <yangdani@USYANGDANI01.microfocus.com>
2019-04-17 10:58:06 -0700

    update LC 161

--- src/LeetCode/n161_One_Edit_Distance.java
+++ src/LeetCode/n161_One_Edit_Distance.java
@@ -24,11 +24,43 @@ Output: true
 Explanation: We can replace '0' with '1' to get t.
  */
 public class n161_One_Edit_Distance {
+	/*
+     i                           i
+S: a c d      replace       S: a b d
+T: a b c d   --------->     T: a b c d    --->  "d" != "cd", no good
+     j                           j
+now we try to insert T(j) to S(i) and we get:
+
+     i                           i
+S: a c d      insert        S: a b c d
+T: a b c d   --------->     T: a b c d    --->  "cd" == "cd", viola!
+     j                           j	 
+To keep the code simple, we make s is always shorter than t, so we don't need to try deletion.	 
+	 */
 	public boolean isOneEditDistance(String s, String t) {
+		if(s == null || t == null) {
+			return false;
+		}
+
+		//handle "acbe", "acb" case
+		if(s.length() > t.length()) {
+			return isOneEditDistance(t, s);
+		}
 		
+		int i=0; 
+		int j=0;
+		while(i<s.length() && j<t.length()) {
+			if(s.charAt(i) != t.charAt(i)) {
+				//replace, insert
+				return (s.substring(i+1).equals(t.substring(j+1)) || (s.substring(i).equals(t.substring(j+1))));
+			}
+			i++;
+			j++;
+		}
+		return t.length() - j == 1;
 	}
 	public static void main(String[] args) {
 		n161_One_Edit_Distance obj = new n161_One_Edit_Distance();
-		System.out.println(obj.isOneEditDistance("ab", "acb"));
+		System.out.println(obj.isOneEditDistance("acbe", "acb"));
 	}
 }
