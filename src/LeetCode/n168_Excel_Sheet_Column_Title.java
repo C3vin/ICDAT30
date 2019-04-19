f8b6186d9a4deb135ffe9defcbe4bdb1820aedfd
yangdani <yangdani@USYANGDANI01.microfocus.com>
2019-04-19 12:21:30 -0700

    add LC 168 & 358

---
+++ src/LeetCode/n168_Excel_Sheet_Column_Title.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+For example:
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+Example 1:
+Input: 1
+Output: "A"
+
+Example 2:
+Input: 28
+Output: "AB"
+
+Example 3:
+Input: 701
+Output: "ZY"
+ */
+public class n168_Excel_Sheet_Column_Title {
+	public String convertToTitle(int n) {
+		
+	}
+}
