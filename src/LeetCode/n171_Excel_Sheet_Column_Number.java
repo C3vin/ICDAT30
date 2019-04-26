80f65b15f7b29a938da9d5c2a238968f15ae6682
C3vin <spacemonkey3@gmail.com>
2019-04-25 22:28:04 -0700

    add LC 171

--- src/LeetCode/n171_Excel_Sheet_Column_Number.java
+++ src/LeetCode/n171_Excel_Sheet_Column_Number.java
@@ -25,8 +25,16 @@ Input: "ZY"
 Output: 701
 */
 public class n171_Excel_Sheet_Column_Number {
+	/*
+	 AB -> 28
+     res = 1 * 26 + 2 = 28
+	*/
 	public int titleToNumber(String s) {
-		
+		int res = 0;
+		for(int i=0; i<s.length(); i++) {
+			res = res * 26 + (s.charAt(i) - 'A' + 1);					//need + 1
+		}
+		return res;
 	}
 	public static void main(String[] args) {
 		n171_Excel_Sheet_Column_Number obj = new n171_Excel_Sheet_Column_Number();
