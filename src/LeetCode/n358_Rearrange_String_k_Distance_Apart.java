a0dbaf922c231d0556257c79081a464b8568e427
C3vin <spacemonkey3@gmail.com>
2019-05-06 03:24:01 -0700

    update LC 358

--- src/LeetCode/n358_Rearrange_String_k_Distance_Apart.java
+++ src/LeetCode/n358_Rearrange_String_k_Distance_Apart.java
@@ -21,6 +21,42 @@ Explanation: The same letters are at least distance 2 from each other.
  */
 public class n358_Rearrange_String_k_Distance_Apart {
 	public String rearrangeString(String s, int k) {
-		
+		if(s == null || s.length() == 0) {
+			return s;
+		}
+		int[] count = new int[26];
+		int[] valid = new int[26];
+		for(char c : s.toCharArray()) {
+			count[c - 'a']++;
+		}
+		StringBuilder res = new StringBuilder();
+		for(int i=0; i<s.length(); i++) {
+			int nextLetter = findNext(count, valid, i);
+			if(nextLetter == -1) {
+				return "";
+			}
+			res.append((char)('a' + nextLetter));
+			valid[nextLetter] = i+k;
+			count[nextLetter]--;
+		}
+		return res.toString();
+	}
+	//find next big count
+	private int findNext(int[] count, int[] valid, int index) {
+		int max = 0;
+		int res = -1;
+		for(int i=0; i<count.length; i++) {
+			if(count[i] > max && valid[i] <= index) {
+				res = i;
+				max = count[i];
+			}
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		n358_Rearrange_String_k_Distance_Apart obj = new n358_Rearrange_String_k_Distance_Apart();
+		System.out.println(obj.rearrangeString("aabbcc", 3));
+		System.out.println(obj.rearrangeString("aaabc", 3));
+		System.out.println(obj.rearrangeString("aaadbbcc", 2));
 	}
 }
