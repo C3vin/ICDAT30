2ca25279e7b795bf1f1bb46b169c18bbe9bf9771
C3vin <spacemonkey3@gmail.com>
2019-09-06 19:39:05 -0700

    add LC 277

--- src/LeetCode/n277_Find_the_Celebrity.java
+++ src/LeetCode/n277_Find_the_Celebrity.java
@@ -9,6 +9,26 @@ celebrity (or verify there is not one) by asking as few questions as possible (i
 You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n). 
 There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. 
 If there is no celebrity, return -1.
+
+Example 1:
+Input: graph = [
+  [1,1,0],
+  [0,1,0],
+  [1,1,1]
+]
+Output: 1
+Explanation: There are three persons labeled with 0, 1 and 2. graph[i][j] = 1 means person i knows person j, 
+otherwise graph[i][j] = 0 means person i does not know person j. The celebrity is the person labeled as 1 because 
+both 0 and 2 know him but 1 does not know anybody.
+
+Example 2:
+Input: graph = [
+  [1,0,1],
+  [1,1,0],
+  [0,1,1]
+]
+Output: -1
+Explanation: There is no celebrity.
 */
 
 /* The knows API is defined in the parent class Relation. 
@@ -50,5 +70,29 @@ public class n277_Find_the_Celebrity {
 	private boolean knows(int i, int j) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	//cs
+	//time:O(n) space:O(1)
+	public int findCelebrity2(int n) {
+		if(n < 2) {
+			return -1;
+		}
+		int possible = 0;
+		for(int i=1; i<n; i++) {
+			if(knows2(possible, i)) {
+				possible = i;
+			}
+		}
+		for(int i=0; i<n; i++) {
+			if(possible != i && (knows2(possible, i) || !knows2(i, possible))) {
+				return -1;
+			}
+		}
+		return possible;
+	}
+	
+	public boolean knows2(int a, int b) {
+		return true;
 	}
 }
