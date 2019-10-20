14afad210253025cfca4e7fbcb83b9864f1653c0
C3vin <spacemonkey3@gmail.com>
2019-10-20 02:59:19 -0700

    update LC 287

--- src/LeetCode/n287_Find_the_Duplicate_Number.java
+++ src/LeetCode/n287_Find_the_Duplicate_Number.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 /*
 Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
 prove that at least one duplicate number must exist. Assume that there is only one duplicate number, 
@@ -20,19 +22,22 @@ Your runtime complexity should be less than O(n2).
 There is only one duplicate number in the array, but it could be repeated more than once.
  */
 public class n287_Find_the_Duplicate_Number {
+	/*    0 1 2 3 4
+	 *    1,3,4,2,2 len=5
+	 *    mid=2, count=3
+	*/
+	//cs time:O(nlogn) space:O(1)
 	public int findDuplicate(int[] nums) {
 		int min = 0;
 		int max = nums.length-1;
 		while(min <= max) {
 			int mid = (max - min)/2 + min;
-			System.out.println("max: "+max+" min: "+min+" mid: "+mid);
 			int count = 0;
 			for(int i=0; i<nums.length; i++) {
 				if(nums[i] <= mid) {
 					count++;
 				}
 			}
-			System.out.println(count);
 			if(count > mid) {
 				max = mid-1;
 			} else {
@@ -41,8 +46,21 @@ public class n287_Find_the_Duplicate_Number {
 		}
 		return min;
 	}
+	//time:O(nlogn) space:O(1)
+	public int findDuplicate2(int[] nums) {
+		Arrays.sort(nums);
+		for(int i=1; i<nums.length; i++) {
+			if(nums[i] == nums[i-1]) {
+				return nums[i];
+			}
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		n287_Find_the_Duplicate_Number obj = new n287_Find_the_Duplicate_Number();
 		System.out.println(obj.findDuplicate(new int[] {1,3,4,2,2}));
+		System.out.println(obj.findDuplicate(new int[] {3,1,3,4,2}));
+		System.out.println(obj.findDuplicate2(new int[] {1,3,4,2,2}));
+		System.out.println(obj.findDuplicate2(new int[] {3,1,3,4,2}));
 	}
 }
