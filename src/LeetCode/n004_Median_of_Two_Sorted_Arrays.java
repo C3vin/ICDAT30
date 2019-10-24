bb7b363417c0d72c72231ff98e540e521c90ad71
C3vin <spacemonkey3@gmail.com>
2019-10-24 02:29:20 -0700

    update LC 004

--- src/LeetCode/n004_Median_of_Two_Sorted_Arrays.java
+++ src/LeetCode/n004_Median_of_Two_Sorted_Arrays.java
@@ -51,11 +51,60 @@ public class n004_Median_of_Two_Sorted_Arrays {
 		else
 			return nums1[astart+partA-1];
 	}
+	
+	//https://www.youtube.com/watch?v=LPFhl65R7ww
+	/*    
+	 *    1  3   8   9   15
+	 * =========================
+	 *   |  |  |   |   |   |   |
+	 *   0  1  2   3   4   5   6  (partition)
+	 * =========================
+	 *    7  11  18  19  21  25
+	 */
+	public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+		if(nums1.length > nums2.length ) {
+			return findMedianSortedArrays2(nums2, nums1);
+		}
+		
+		int x = nums1.length;
+		int y = nums2.length;
+		int start = 0;
+		int end = x;
+		
+		//partitionX + partitionY = (x+y+1) / 2 //L:R or L+1:R
+		while(start <= end) {
+			int partitionX = (start+end)/2;
+			int partitionY = (x+y+1)/2 - partitionX;		
+			
+			int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];  	
+			int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+			
+			int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+			int minRightY = (partitionY == x) ? Integer.MAX_VALUE : nums2[partitionY];
+			
+			if(maxLeftX <= minRightY && maxLeftY <= minRightX) {
+				if((x+y)%2 == 0) {
+					return (double)(Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+				} else {
+					return (double)Math.max(maxLeftX, maxLeftY);
+				}
+			} else if(maxLeftX > minRightY) {
+				end = partitionX - 1;
+			} else {
+				start = partitionX + 1;
+			}
+		}
+		return 0;
+	}
+	
 	public static void main(String[] args) {
 		n004_Median_of_Two_Sorted_Arrays obj = new n004_Median_of_Two_Sorted_Arrays();
 		int[] nums1 = {1,2};
 		int[] nums2 = {3,4};
 		System.out.println(obj.findMedianSortedArrays(nums1, nums2));
+		System.out.println(obj.findMedianSortedArrays2(nums1, nums2));
+		System.out.println(obj.findMedianSortedArrays(new int[] {1,3,8,9,15}, new int[] {7,11,19,21,18,25}));
+		System.out.println(obj.findMedianSortedArrays2(new int[] {1,3,8,9,15}, new int[] {7,11,19,21,18,25}));
 	}
 }
 
