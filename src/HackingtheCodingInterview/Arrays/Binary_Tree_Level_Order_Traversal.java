4abc753687cd7d0ecf2b20877a345c09c959f8a4
yangdani <yangdani@USyangdani02.microfocus.com>
2019-11-01 17:33:22 -0700

    add BST

---
+++ src/HackingtheCodingInterview/Arrays/Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,57 @@
+package HackingtheCodingInterview.Arrays;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+};
+public class Binary_Tree_Level_Order_Traversal {
+	private static List<List<Integer>> traverse(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if(root == null) {
+			return result;
+		}
+
+		//BFS - Queue
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+
+		while(!queue.isEmpty()) {
+			int levelSize = queue.size();
+			List<Integer> currentLevel = new ArrayList<Integer>(levelSize);
+			for(int i=0; i<levelSize; i++) {
+				TreeNode currentNode = queue.poll();
+				currentLevel.add(currentNode.val);
+
+				if(currentNode.left != null) {
+					queue.offer(currentNode.left);
+				}
+				if(currentNode.right != null) {
+					queue.offer(currentNode.right);
+				}
+			}
+			result.add(currentLevel);
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		TreeNode root = new TreeNode(12);
+		root.left = new TreeNode(7);
+		root.right = new TreeNode(1);
+		root.left.left = new TreeNode(9);
+		root.right.left = new TreeNode(10);
+		root.right.right = new TreeNode(5);
+		List<List<Integer>> result = Binary_Tree_Level_Order_Traversal.traverse(root);
+		System.out.println("Level order traversal: " + result);
+	}
+}
