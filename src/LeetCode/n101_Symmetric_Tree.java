52daf178f1aa69737b1631d629ccb782e64429bd
yangdani <yangdani@USyangdani02.microfocus.com>
2019-11-07 15:06:44 -0800

    upadte LC 101

--- src/LeetCode/n101_Symmetric_Tree.java
+++ src/LeetCode/n101_Symmetric_Tree.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 /*
 Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
@@ -28,32 +29,37 @@ public class n101_Symmetric_Tree {
 		TreeNode right;
 		TreeNode(int x) { val = x; }
 	}
+	//O(n) O(n)
 	public boolean isSymmetric(TreeNode root) {
 		if(root == null) return true;
 		return isSymmetricTree(root.left, root.right);
 	}
-	//recursively 
-	private boolean isSymmetricTree(TreeNode left, TreeNode right) {
-		if(left == null && right == null) 
+	//recursively dfs
+	private boolean isSymmetricTree(TreeNode pLeft, TreeNode pRight) {
+		if(pLeft == null && pRight == null) {		//must on the top, handle leaf
 			return true;
-		else if (left == null || right == null) 
-			return false;
-
-		if(left.val == right.val) {
-			return (isSymmetricTree(left.left, right.right) && isSymmetricTree(left.right, right.left));
 		}
-		else 
+		if(pLeft.val != pRight.val) {
 			return false;
+		}
+		if(pLeft == null || pRight == null) {
+			return false;
+		}
+		
+		return isSymmetricTree(pLeft.left, pRight.right) && isSymmetricTree(pLeft.right, pRight.left);
 	}
 
 	//iteratively
 	public boolean isSymmetric2(TreeNode root) {
-		    if(root == null)
+		    if(root == null) {
 		        return true;
-		    if(root.left == null && root.right == null)
+		    }
+		    if(root.left == null && root.right == null) {
 		        return true;
-		    if(root.left == null || root.right == null)
+		    }
+		    if(root.left == null || root.right == null) {
 		        return false;
+		    }
 		    LinkedList<TreeNode> q1 = new LinkedList<TreeNode>();
 		    LinkedList<TreeNode> q2 = new LinkedList<TreeNode>();
 		    q1.add(root.left);
@@ -82,6 +88,32 @@ public class n101_Symmetric_Tree {
 		    return true;
 	}
 	
+	//Iterative //O(n) O(n)
+	public boolean isSymmetric3(TreeNode root) {
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		queue.offer(root);
+		
+		while(!queue.isEmpty()) { 
+			TreeNode t1 = queue.poll();
+			TreeNode t2 = queue.poll();
+			if(t1 == null && t2 == null) {
+				return true;
+			}
+			if(t1 == null || t2 == null) {
+				return false;
+			}
+			if(t1.val != t2.val) {
+				return false;
+			}
+			queue.offer(t1.left);
+			queue.offer(t2.right);
+			queue.offer(t1.right);
+			queue.offer(t2.left);
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		n101_Symmetric_Tree obj = new n101_Symmetric_Tree();
 		TreeNode p1 = obj.new TreeNode(1);
@@ -101,5 +133,6 @@ public class n101_Symmetric_Tree {
 		System.out.println(p1.val + " " + p1.left.val + " " + p1.right.val + " "+ p2.left.val + " " + p2.right.val + " " + p3.left.val + " " +p3.right.val);
 		System.out.println(obj.isSymmetric(p1));
 		System.out.println(obj.isSymmetric2(p1));
+		System.out.println(obj.isSymmetric3(p1));
 	}
 }
