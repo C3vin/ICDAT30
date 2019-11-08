2de0920c6605a86a2115e5f880eeaca0325b0afb
C3vin <spacemonkey3@gmail.com>
2019-11-08 03:22:08 -0800

    update LC 104

--- src/LeetCode/n104_Maximum_Depth_of_Binary_Tree.java
+++ src/LeetCode/n104_Maximum_Depth_of_Binary_Tree.java
@@ -2,7 +2,20 @@ package LeetCode;
 
 import java.util.LinkedList;
 import java.util.Queue;
-@Alg(type="DFS,Tree", com="LL,A", level="easy", num=104)
+/*
+Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+Note: A leaf is a node with no children.
+
+Example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+ */
 public class n104_Maximum_Depth_of_Binary_Tree {
 	public class TreeNode {
 		int val;
@@ -74,24 +87,32 @@ public class n104_Maximum_Depth_of_Binary_Tree {
 		}
 		return depth;
 	}
+	public int maxDepth4(TreeNode root) {
+		if(root == null) {
+			return 0;
+		}
+		System.out.println(root.val);
+		return Math.max(maxDepth4(root.left), maxDepth4(root.right)) + 1;
+	}
 	public static void main(String[] args) {
 		n104_Maximum_Depth_of_Binary_Tree obj = new n104_Maximum_Depth_of_Binary_Tree();
 		TreeNode p1 = obj.new TreeNode(3);
 		TreeNode p2 = obj.new TreeNode(9);
 		TreeNode p3 = obj.new TreeNode(20);
-		TreeNode p4 = obj.new TreeNode(1);
-		TreeNode p5 = obj.new TreeNode(3);
+		//TreeNode p4 = obj.new TreeNode(null);
+		//TreeNode p5 = obj.new TreeNode(null);
 		TreeNode p6 = obj.new TreeNode(15);
 		TreeNode p7 = obj.new TreeNode(7);
 		p1.left = p2;
 		p1.right = p3;
-		p2.right = p5;
-		p2.left = p4;
+		//p2.right = p5;
+		//p2.left = p4;
 		p3.right = p7;
 		p3.left = p6;
-		System.out.println(p1.val + " " + p1.left.val + " " + p1.right.val + " "+ p2.left.val + " " + p2.right.val 
-				+ " " + p3.left.val + " " +p3.right.val);
+		//System.out.println(p1.val + " " + p1.left.val + " " + p1.right.val + " "+ p2.left.val + " " + p2.right.val 
+			//	+ " " + p3.left.val + " " +p3.right.val);
 		System.out.println(obj.maxDepth(p1));
 		System.out.println(obj.maxDepth3(p1));
+		System.out.println(obj.maxDepth4(p1));
 	}
 }
