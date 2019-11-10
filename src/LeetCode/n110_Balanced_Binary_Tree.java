ede905e4808132d524b7ed91637d68f08bdd8697
C3vin <spacemonkey3@gmail.com>
2019-11-09 21:37:57 -0800

    uodate LC 110

--- src/LeetCode/n110_Balanced_Binary_Tree.java
+++ src/LeetCode/n110_Balanced_Binary_Tree.java
@@ -1,5 +1,30 @@
 package LeetCode;
 
+/*
+Given a binary tree, determine if it is height-balanced.
+For this problem, a height-balanced binary tree is defined as:
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+Example 1:
+Given the following tree [3,9,20,null,null,15,7]:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
+ */
 public class n110_Balanced_Binary_Tree {
 	public class TreeNode {
 		int val;
@@ -9,23 +34,38 @@ public class n110_Balanced_Binary_Tree {
 			val = x; 
 		}
 	}
+	
+	//O(n) O(n)
 	public boolean isBalanced(TreeNode root) {
 		if(helper(root) == -1)
 			return false;
 
 		return true;
 	}
+
 	private int helper(TreeNode root) {
-		if(root == null)
+		if(root == null) {
 			return 0;
+		}
+
 		int left = helper(root.left);
-		int right = helper(root.right);
-		
-		if(left == -1 || right == -1 || Math.abs(left - right) > 1)
+		if(left == -1) {
 			return -1;
+		}
 		
-		return Math.max(left, right) + 1;
+		int right = helper(root.right);
+		if(right == -1) {
+			return -1;
+		}
+		
+		int diff = Math.abs(left-right);
+		if(diff > 1) {
+			return -1;
+		} else {
+			return Math.max(left, right) + 1;
+		}
 	}
+
 	public static void main(String[] args) {
 		n110_Balanced_Binary_Tree obj = new n110_Balanced_Binary_Tree();
 		TreeNode p1 = obj.new TreeNode(1);
