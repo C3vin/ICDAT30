d8a86263ddcd16b27c80db921c207db4d5d07876
C3vin <spacemonkey3@gmail.com>
2019-11-11 04:01:40 -0800

    add 105 114

---
+++ src/LeetCode/n105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import LeetCode.n109_Convert_Sorted_List_to_Binary_Search_Tree.TreeNode;
+
+/*
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+For example, given
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ */
+public class n105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	} 
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+	}
+}
---
+++ src/LeetCode/n114_Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+import LeetCode.n105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.TreeNode;
+
+/*
+Given a binary tree, flatten it to a linked list in-place.
+For example, given the following tree:
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+ */
+public class n114_Flatten_Binary_Tree_to_Linked_List {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	} 
+	public void flatten(TreeNode root) {
+
+	}
+}
