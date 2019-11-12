1738ff4295c30c8daeaa6638223bd7101e442f8e
C3vin <spacemonkey3@gmail.com>
2019-11-12 03:02:57 -0800

    update LC 114

--- src/LeetCode/n114_Flatten_Binary_Tree_to_Linked_List.java
+++ src/LeetCode/n114_Flatten_Binary_Tree_to_Linked_List.java
@@ -32,6 +32,38 @@ public class n114_Flatten_Binary_Tree_to_Linked_List {
 		TreeNode(int x) { val = x; }
 	} 
 	public void flatten(TreeNode root) {
-
+		if(root == null) {
+			return;
+		}
+		if(root.left != null) {
+			flatten(root.left);
+		}
+		if(root.right != null) {
+			flatten(root.right);
+		}
+		TreeNode currentNode = root.right;
+		root.right = root.left;
+		root.left = null;
+		
+		while(root.right != null) {
+			root = root.right;
+		}
+		root.right = currentNode;
+	} 
+	public static void main(String[] args) {
+		n114_Flatten_Binary_Tree_to_Linked_List obj = new n114_Flatten_Binary_Tree_to_Linked_List();
+		TreeNode p1 = obj.new TreeNode(1);
+		TreeNode p2 = obj.new TreeNode(2);
+		TreeNode p3 = obj.new TreeNode(3);
+		TreeNode p4 = obj.new TreeNode(4);
+		TreeNode p5 = obj.new TreeNode(5);
+		TreeNode p6 = obj.new TreeNode(6);
+		p1.left = p2;
+		p1.right = p5;
+		p2.left = p3;
+		p2.right = p4;
+		p5.right = p6;
+		
+		obj.flatten(p1);
 	}
 }
