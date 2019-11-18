fd7b7bed75542c12a8081c685dd1f271ebca96ee
C3vin <spacemonkey3@gmail.com>
2019-11-18 03:34:46 -0800

    update LC 109

--- src/LeetCode/n109_Convert_Sorted_List_to_Binary_Search_Tree.java
+++ src/LeetCode/n109_Convert_Sorted_List_to_Binary_Search_Tree.java
@@ -23,7 +23,6 @@ public class n109_Convert_Sorted_List_to_Binary_Search_Tree {
 		ListNode next;
 		ListNode(int x) { val = x; }
 	}
-
 	public class TreeNode {
 		int val;
 		TreeNode left;
@@ -36,7 +35,8 @@ public class n109_Convert_Sorted_List_to_Binary_Search_Tree {
 		}
 		return toBST(head, null);
 	}
-	public TreeNode toBST(ListNode head, ListNode tail) {
+	//top-down
+	private TreeNode toBST(ListNode head, ListNode tail) {
 		if(head == tail) {
 			return null;
 		}
@@ -46,12 +46,70 @@ public class n109_Convert_Sorted_List_to_Binary_Search_Tree {
 			fast = fast.next.next;
 			slow = slow.next;
 		}
-		TreeNode root = new TreeNode(slow.val);
+		System.out.println(slow.val);
+		TreeNode root = new TreeNode(slow.val);		//find the mid by using fast-slow 
 		root.left = toBST(head, slow);
 		root.right = toBST(slow.next, tail);
 		
 		return root; 
 	}
+	
+	public TreeNode sortedListToBST2(ListNode head) {
+		int len = 0;
+		ListNode p = head;
+		while(p != null) {
+			len++;
+			p = p.next;
+		}
+		return toBST2(head, 0, len-1);
+	}
+	private TreeNode toBST2(ListNode head, int start, int end) {
+		if(start == end) {
+			return null;
+		}
+		int mid = (end - start)/2 + start;
+		//int mid = (start + end) >>> 1;
+		System.out.println(mid);
+		TreeNode left = toBST2(head, start, mid);
+		
+		TreeNode parent = new TreeNode(head.val);
+		parent.left = left;
+		head = head.next;
+		TreeNode right = toBST2(head, mid+1, end);
+		parent.right = right;
+		
+		return parent;
+	}
+	
+	ListNode cur = null;
+	public TreeNode sortedListToBST3(ListNode head) {
+		cur = head;
+		int end = 0;
+		while(head != null) {
+			end++;
+			head = head.next;
+		}
+		return toBST3(0, end);
+	}
+	private TreeNode toBST3(int start, int end) {
+		if(start == end) {
+			return null;
+		}
+		int mid = (end - start)/2 + start;
+		//int mid = (start + end) >>> 1;
+		System.out.println(mid);
+		TreeNode left = toBST3(start, mid);
+		
+		TreeNode parent = new TreeNode(cur.val);
+		parent.left = left;
+		cur = cur.next;
+		TreeNode right = toBST3(mid+1, end);
+		parent.right = right;
+		
+		return parent;
+	}
+	//ref:https://leetcode.wang/leetcode-109-Convert-Sorted-List-to-Binary-Search-Tree.html
+	//
 	public static void main(String[] args) {
 		n109_Convert_Sorted_List_to_Binary_Search_Tree obj = new n109_Convert_Sorted_List_to_Binary_Search_Tree();
 		ListNode n1 = obj.new ListNode(-10);
@@ -64,5 +122,7 @@ public class n109_Convert_Sorted_List_to_Binary_Search_Tree {
 		n3.next = n4;
 		n4.next = n5;
 		System.out.println(obj.sortedListToBST(n1));
+		System.out.println(obj.sortedListToBST2(n1));
+		System.out.println(obj.sortedListToBST3(n1));
 	}
 }
