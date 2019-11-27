437be0f0014760ba570e4c49254fa227cea2ee15
yangdani <yangdani@USyangdani02.microfocus.com>
2019-11-26 16:50:58 -0800

    update LC 94

--- src/LeetCode/n094_Binary_Tree_Inorder_Traversal.java
+++ src/LeetCode/n094_Binary_Tree_Inorder_Traversal.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
 public class n094_Binary_Tree_Inorder_Traversal {
 	public class TreeNode {
 		int val;
