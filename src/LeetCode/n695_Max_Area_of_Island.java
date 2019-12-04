fa1c6b5d5bde09ce54281f460f1731d86cd2e651
yangdani <yangdani@USyangdani02.microfocus.com>
2019-12-04 15:32:52 -0800

    update LC 695

--- src/LeetCode/n695_Max_Area_of_Island.java
+++ src/LeetCode/n695_Max_Area_of_Island.java
@@ -61,7 +61,7 @@ public class n695_Max_Area_of_Island {
 		for(int i=0; i<grid.length; i++) {
 			for(int j=0; j<grid[0].length; j++) {
 				if(grid[i][j] == 1) {
-					count[0] = 0;
+					count[0] = 0;					//Default and reset count[0] 'inside' for each island
 					dfs2(grid, i, j, count);
 					res = Math.max(res, count[0]);
 				}
@@ -99,7 +99,7 @@ public class n695_Max_Area_of_Island {
 					{0,0,0,0,0,0,0,0,0,0,1,0,0},
 					{0,0,0,0,0,0,0,1,1,1,0,0,0},
 					{0,0,0,0,0,0,0,1,1,0,0,0,0}};
-		System.out.println(obj.maxAreaOfIsland(grid));
+		//System.out.println(obj.maxAreaOfIsland(grid));
 		System.out.println(obj.maxAreaOfIsland2(grid));
 	}
 }
