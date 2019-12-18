5abcda01e1e10c005e860e17aba97a157619cc76
yangdani <yangdani@USyangdani02.microfocus.com>
2019-12-18 15:30:54 -0800

    update LC 126

--- src/LeetCode/n126_Word_Ladder_II.java
+++ src/LeetCode/n126_Word_Ladder_II.java
@@ -44,89 +44,139 @@ Output: []
 Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
  */
 public class n126_Word_Ladder_II {
-    Set<String> dict;
-    Map<String, List<String>> mutation = new HashMap<>();
-    Map<String, Integer> distance = new HashMap<>();
-    //https://github.com/awangdev/LintCode/blob/master/Java/Word%20Ladder%20II.java
+	Set<String> dict;
+	Map<String, List<String>> mutation = new HashMap<>();
+	Map<String, Integer> distance = new HashMap<>();
+	//https://github.com/awangdev/LintCode/blob/master/Java/Word%20Ladder%20II.java
 	public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        List<List<String>> rst = new ArrayList<>();
-        dict = new HashSet<>(wordList);
-        if (!dict.contains(endWord)) return rst;
+		List<List<String>> rst = new ArrayList<>();
+		dict = new HashSet<>(wordList);
+		if (!dict.contains(endWord)) return rst;
 
-        prep(beginWord);
-        //dfs(start, end, new ArrayList<>(), rst); // option1
-        bfs(beginWord, endWord, rst); // option2
-        return rst;
+		prep(beginWord);
+		//dfs(start, end, new ArrayList<>(), rst); // option1
+		bfs(beginWord, endWord, rst); // option2
+		return rst;
 	}
-	  //BFS/Prep to populate mutation and distance map.
-    public void prep(String start) {
-        //Init
-        Queue<String> queue = new LinkedList<>();
-        dict.add(start);
-        queue.offer(start);
-        distance.put(start, 0);
-        for (String s : dict) {
-            mutation.put(s, new ArrayList<>());
-        }
-        // process queue
-        while(!queue.isEmpty()) {
-            String str = queue.poll();
-            List<String> list = transform(str);
+	//BFS/Prep to populate mutation and distance map.
+	public void prep(String start) {
+		//Init
+		Queue<String> queue = new LinkedList<>();
+		dict.add(start);
+		queue.offer(start);
+		distance.put(start, 0);
+		for (String s : dict) {
+			mutation.put(s, new ArrayList<>());
+		}
+		// process queue
+		while(!queue.isEmpty()) {
+			String str = queue.poll();
+			List<String> list = transform(str);
 
-            for (String s : list) {
-                mutation.get(s).add(str);
-                if (!distance.containsKey(s)) { // only record dist->s once, as shortest
-                    distance.put(s, distance.get(str) + 1);
-                    queue.offer(s);
-                }
-            }
-        }
-    }
-    
-    // Option2: bfs, bi-directional search
-    public void bfs(String start, String end, List<List<String>> rst) {
-        Queue<List<String>> queue = new LinkedList<>();
-        List<String> list = new ArrayList<>();
-        list.add(end);
-        queue.offer(new ArrayList<>(list));
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size > 0) {
-                list = queue.poll();
-                String str = list.get(0);
-                
-                for (String s : mutation.get(str)) {//All prior-mutation sources
-                    list.add(0, s);
-                    if (s.equals(start)) {
-                        rst.add(new ArrayList<>(list));
-                    } else if (distance.containsKey(s) && distance.get(str) - 1 == distance.get(s)) {
-                        //Only pick those that's 1 step away: keep minimum steps for optimal solution
-                        queue.offer(new ArrayList<>(list));
-                    }
-                    list.remove(0);
-                }
-                size--;
-            }
-        }
-    }
-    //Generate all possible mutations for word. Check against dic and skip word itself.
-    private List<String> transform(String word) {
-        List<String> candidates = new ArrayList<>();
-        StringBuffer sb = new StringBuffer(word);
-        for (int i = 0; i < sb.length(); i++) {
-            char temp = sb.charAt(i);
-            for (char c = 'a'; c <= 'z'; c++) {
-                if (temp == c) continue;
-                sb.setCharAt(i, c);
-                String newWord = sb.toString();
-                if (dict.contains(newWord)) {
-                    candidates.add(newWord);
-                }
-            }
-            sb.setCharAt(i, temp);//backtrack
-        }    
-        return candidates;
-    }
+			for (String s : list) {
+				mutation.get(s).add(str);
+				if (!distance.containsKey(s)) { // only record dist->s once, as shortest
+					distance.put(s, distance.get(str) + 1);
+					queue.offer(s);
+				}
+			}
+		}
+	}
+
+	// Option2: bfs, bi-directional search
+	public void bfs(String start, String end, List<List<String>> rst) {
+		Queue<List<String>> queue = new LinkedList<>();
+		List<String> list = new ArrayList<>();
+		list.add(end);
+		queue.offer(new ArrayList<>(list));
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			while (size > 0) {
+				list = queue.poll();
+				String str = list.get(0);
+
+				for (String s : mutation.get(str)) {//All prior-mutation sources
+					list.add(0, s);
+					if (s.equals(start)) {
+						rst.add(new ArrayList<>(list));
+					} else if (distance.containsKey(s) && distance.get(str) - 1 == distance.get(s)) {
+						//Only pick those that's 1 step away: keep minimum steps for optimal solution
+						queue.offer(new ArrayList<>(list));
+					}
+					list.remove(0);
+				}
+				size--;
+			}
+		}
+	}
+	//Generate all possible mutations for word. Check against dic and skip word itself.
+	private List<String> transform(String word) {
+		List<String> candidates = new ArrayList<>();
+		StringBuffer sb = new StringBuffer(word);
+		for (int i = 0; i < sb.length(); i++) {
+			char temp = sb.charAt(i);
+			for (char c = 'a'; c <= 'z'; c++) {
+				if (temp == c) continue;
+				sb.setCharAt(i, c);
+				String newWord = sb.toString();
+				if (dict.contains(newWord)) {
+					candidates.add(newWord);
+				}
+			}
+			sb.setCharAt(i, temp);//backtrack
+		}    
+		return candidates;
+	}
+
+	public List<List<String>> findLadders2(String beginWord, String endWord, List<String> wordList) {
+		List<List<String>> res = new ArrayList<List<String>>();
+		Set<String> dict = new HashSet<String>(wordList);
+		List<String> list = new ArrayList<String>();
+		list.add(beginWord);
+
+		Queue<List<String>> queue = new LinkedList<List<String>>();
+		queue.offer(list);
+
+		int level = 1;
+		int minLevel = Integer.MAX_VALUE;
+
+		Set<String> words = new HashSet<String>();
+
+		while(!queue.isEmpty()) {
+			List<String> cur = queue.poll();
+			if(cur.size() > level) {
+				for(String w : words) {
+					dict.remove(w);
+				}
+				words.clear();
+				level = cur.size();
+				if(level > minLevel) {
+					break;
+				}
+			}
+			String last = cur.get(cur.size()-1);
+			for(int i=0; i<last.length(); i++) {
+				StringBuilder newWord = new StringBuilder(last);
+				for(char ch='a'; ch<='z'; ch++) {
+					newWord.setCharAt(i, ch);
+					if (!dict.contains(newWord.toString())) {
+						continue;
+					}
+					words.add(newWord.toString());
+					List<String> nextPath = cur;
+					nextPath.add(newWord.toString());
+					if(newWord.toString().equals(endWord)) {
+						res.add(nextPath);
+						minLevel = level;
+					} else {
+						queue.offer(nextPath);
+					}
+				}
+			}
+
+		}
+		return res;
+	}
 
 	public static void main(String[] args) {
 		n126_Word_Ladder_II obj = new n126_Word_Ladder_II();
@@ -138,5 +188,6 @@ public class n126_Word_Ladder_II {
 		wordList1.add("log");
 		wordList1.add("cog");
 		System.out.println(obj.findLadders("hit", "cog", wordList1));
+		System.out.println(obj.findLadders2("hit", "cog", wordList1));
 	}
 }
