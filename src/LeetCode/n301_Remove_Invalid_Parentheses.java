c478730c8960e1605da38595bed424f2c518a2ea
C3vin <spacemonkey3@gmail.com>
2020-01-15 03:47:49 -0800

    update LC 301

--- src/LeetCode/n301_Remove_Invalid_Parentheses.java
+++ src/LeetCode/n301_Remove_Invalid_Parentheses.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -21,14 +22,45 @@ Output: [""]
  */
 public class n301_Remove_Invalid_Parentheses {
 	public List<String> removeInvalidParentheses(String s) {
-		
+		List<String> res = new ArrayList<String>();
+		dfs(res, s, 0, 0, '(', ')');
+
+		return res;
+	}
+	private void dfs(List<String> res, String s, int x, int y, char open, char close) {
+		for(int count=0, i=x; i<s.length(); i++) {
+			if(s.charAt(i) == open) {
+				count++;
+			}
+			if(s.charAt(i) == close) {
+				count--;
+			}
+
+			if(count<0) {
+				for(int j=y; j<=i; j++) {
+					if(s.charAt(j) == close && (j == y || s.charAt(j-1) != close)) {
+						dfs(res, s.substring(0, j)+s.substring(j+1), i, j, open, close);
+					}
+				}
+				return;		//must
+			}
+		}
+
+		String reverse = new StringBuilder(s).reverse().toString();
+		if(open == '(') {
+			dfs(res, reverse, 0, 0, close, open);
+		} else {
+			res.add(reverse);
+		}
 	}
 	
+	//https://segmentfault.com/a/1190000013676627  good!
 	public static void main(String[] args) {
 		n301_Remove_Invalid_Parentheses obj = new n301_Remove_Invalid_Parentheses();
-		System.out.println(obj.removeInvalidParentheses("()())()"));
+		/*System.out.println(obj.removeInvalidParentheses("()())()"));
 		System.out.println(obj.removeInvalidParentheses("(a)())()"));
-		System.out.println(obj.removeInvalidParentheses(")("));
+		System.out.println(obj.removeInvalidParentheses(")("));*/
+		System.out.println(obj.removeInvalidParentheses("()(()"));
 	}
-	
+
 }
