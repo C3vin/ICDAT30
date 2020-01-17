ea87a8dd6d6d2e5b7f66785a2e27bf9ac5b84c64
C3vin <spacemonkey3@gmail.com>
2020-01-17 04:21:47 -0800

    update LC 301

--- src/LeetCode/n301_Remove_Invalid_Parentheses.java
+++ src/LeetCode/n301_Remove_Invalid_Parentheses.java
@@ -38,6 +38,7 @@ public class n301_Remove_Invalid_Parentheses {
 
 			if(count<0) {
 				for(int j=y; j<=i; j++) {
+					//((s.charAt(j) == par[1] && s.charAt(j - 1) != par[1]) || (s.charAt(j) == par[1] && j == last_j))
 					if(s.charAt(j) == close && (j == y || s.charAt(j-1) != close)) {
 						dfs(res, s.substring(0, j)+s.substring(j+1), i, j, open, close);
 					}
@@ -57,10 +58,11 @@ public class n301_Remove_Invalid_Parentheses {
 	//https://segmentfault.com/a/1190000013676627  good!
 	public static void main(String[] args) {
 		n301_Remove_Invalid_Parentheses obj = new n301_Remove_Invalid_Parentheses();
-		/*System.out.println(obj.removeInvalidParentheses("()())()"));
-		System.out.println(obj.removeInvalidParentheses("(a)())()"));
-		System.out.println(obj.removeInvalidParentheses(")("));*/
-		System.out.println(obj.removeInvalidParentheses("()(()"));
+		System.out.println(obj.removeInvalidParentheses("()())()"));
+		//System.out.println(obj.removeInvalidParentheses("(a)())()"));
+		//System.out.println(obj.removeInvalidParentheses(")("));
+		//System.out.println(obj.removeInvalidParentheses("()(()"));
+		//System.out.println(obj.removeInvalidParentheses("()"));
 	}
 
 }
