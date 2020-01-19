64d9646d84ba6805dc421de6a28dd093936e5645
C3vin <spacemonkey3@gmail.com>
2020-01-19 01:59:13 -0800

    update LC 20

--- src/LeetCode/n020_Valid_Parentheses.java
+++ src/LeetCode/n020_Valid_Parentheses.java
@@ -1,43 +1,39 @@
 package LeetCode;
 
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+Input: "()"
+Output: true
+
+Example 2:
+Input: "()[]{}"
+Output: true
+
+Example 3:
+Input: "(]"
+Output: false
+
+Example 4:
+Input: "([)]"
+Output: false
+
+Example 5:
+Input: "{[]}"
+Output: true
+ */
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Stack;
 //Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 //The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 
 public class n020_Valid_Parentheses {
-	//sol1
-	public boolean isValid(String s) {
-		if(s.length() == 0 || s.length() == 1) 
-			return false;
-
-		LinkedList<Character> stack = new LinkedList<Character>();
-		for(int i=0; i<s.length(); i++) {
-			if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-				stack.push(s.charAt(i));
-			} else {
-				if(stack.size() == 0)			//F: Need to handle this, '){'
-					return false;
-
-				char tmp = stack.pop();
-				if(s.charAt(i) == ')') {
-					if(tmp != '(')
-						return false;
-				} else if(s.charAt(i) == ']') {
-					if(tmp != '[') 
-						return false;
-				} else if(s.charAt(i) == '}') {
-					if(tmp != '{')
-						return false;
-				}
-			}	
-		}
-		if(stack.size() == 0)
-			return true;
-		//return stack.size() == 0;					//F: using size !!!!!!!!!!!!!!! 
-		return false;
-	}
-
 	//sol2
 	public boolean isValid2(String s) {
 /*		if(s.length() == 0 || s.length() == 1) {
@@ -63,14 +59,39 @@ public class n020_Valid_Parentheses {
 		return stack.isEmpty();
 	}
 
+	public boolean isValid3(String s) {
+		if(s == null || s.length() == 0) {
+			return true;
+		}
+		
+		Stack<Character> stack = new Stack<Character>();
+		for(char ch : s.toCharArray()) {
+			if(ch == '(') {
+				stack.push(')');
+			} else if(ch == '[') {
+				stack.push(']');
+			} else if(ch == '{') {
+				stack.push('}');
+			} else {
+				if(stack.isEmpty() || stack.pop() != ch) {	//pop will remove the stack and check with ch value
+					return false;
+				}
+			}
+		}
+		return stack.isEmpty();		//handle "[" case
+	}
+	
 	public static void main(String[] args) {
 		n020_Valid_Parentheses obj = new n020_Valid_Parentheses();
 		
-		System.out.println(obj.isValid("()[]{}"));
-		System.out.println(obj.isValid("){"));
 		System.out.println(obj.isValid2("()[]{}"));
 		System.out.println(obj.isValid2("){"));
-		System.out.println(obj.isValid(""));
 		System.out.println(obj.isValid2(""));
+		System.out.println(obj.isValid2("["));
+		
+		System.out.println(obj.isValid3("()[]{}"));
+		System.out.println(obj.isValid3("){"));
+		System.out.println(obj.isValid3(""));
+		System.out.println(obj.isValid3("["));
 	}
 }
