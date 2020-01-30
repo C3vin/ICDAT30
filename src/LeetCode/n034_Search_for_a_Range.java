42a96021d131d7f1566dc1346d8ea02d7965c46b
C3vin <spacemonkey3@gmail.com>
2020-01-30 02:39:07 -0800

    update LC 34

--- src/LeetCode/n034_Search_for_a_Range.java
+++ src/LeetCode/n034_Search_for_a_Range.java
@@ -2,7 +2,21 @@ package LeetCode;
 
 import java.util.Arrays;
 
+/*
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+Your algorithm's runtime complexity must be in the order of O(log n).
+If the target is not found in the array, return [-1, -1].
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+ */
 public class n034_Search_for_a_Range {
+	//https://leetcode.wang/leetCode-34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.html
 	public int[] searchRange(int[] nums, int target) {
 		int[] res = {-1,-1};
 		if(nums==null || nums.length==0) return res; 
