9495d0d6bf9352a30adc61454a0b715c3b118e26
yangdani <yangdani@USyangdani02.microfocus.com>
2020-02-01 17:17:01 -0800

    update LC 35

--- src/LeetCode/n035_Search_Insert_Position.java
+++ src/LeetCode/n035_Search_Insert_Position.java
@@ -1,22 +1,46 @@
 package LeetCode;
 
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 4:
+Input: [1,3,5,6], 0
+Output: 0
+ */
 public class n035_Search_Insert_Position {
 	public int searchInsert(int[] nums, int target) {
-		if(nums == null || nums.length == 0) return 0;
-		
-		int L = 0;
-		int R = nums.length-1;
-		
-		while(L <= R) {
-			int Mid = (L+R)/2;
-			if(nums[Mid] == target)
-				return Mid;
-			if(nums[Mid] < target) {
-				L = Mid+1;
-			} else
-				R = Mid-1;
+		if(nums == null || nums.length == 0) {
+			return 0;
 		}
-		return L;
+		
+		int left = 0;
+		int right = nums.length-1;
+		
+		while(left <= right) {
+			int Mid = (left+right)/2;
+			if(nums[Mid] == target) {
+				return Mid;
+			}
+			if(nums[Mid] < target) {
+				left = Mid+1;
+			} else
+				right = Mid-1;
+		}
+		return left;								//return left
 	}
 	
 	public static void main(String[] args) {
