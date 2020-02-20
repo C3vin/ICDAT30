d839350960be319a8f01b2fe8db7ab5574e7cdc9
C3vin <spacemonkey3@gmail.com>
2020-02-20 01:21:51 -0800

    update LC 165

--- src/LeetCode/n165_Compare_Version_Numbers.java
+++ src/LeetCode/n165_Compare_Version_Numbers.java
@@ -1,28 +1,61 @@
 package LeetCode;
 
-@Alg(type="String", com="A,M$", level="easy", num=165)
+/*
+Compare two version numbers version1 and version2.
+If version1 > version2 return 1; if version1 < version2 return -1;otherwise return 0.
+You may assume that the version strings are non-empty and contain only digits and the . character.
+The . character does not represent a decimal point and is used to separate number sequences.
+For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+You may assume the default revision number for each level of a version number to be 0. 
+For example, version number 3.4 has a revision number of 3 and 4 for its first and second level revision number. 
+Its third and fourth level revision number are both 0.
+
+Example 1:
+Input: version1 = "0.1", version2 = "1.1"
+Output: -1
+
+Example 2:
+Input: version1 = "1.0.1", version2 = "1"
+Output: 1
+
+Example 3:
+Input: version1 = "7.5.2.4", version2 = "7.5.3"
+Output: -1
+
+Example 4:
+Input: version1 = "1.01", version2 = "1.001"
+Output: 0
+Explanation: Ignoring leading zeroes, both “01” and “001" represent the same number “1”
+
+Example 5:
+Input: version1 = "1.0", version2 = "1.0.0"
+Output: 0
+Explanation: The first version number does not have a third level revision number, which means its third level revision number is default to "0"
+
+Note:
+Version strings are composed of numeric strings separated by dots . and this numeric strings may have leading zeroes.
+Version strings do not start or end with dots, and they will not be two consecutive dots.
+ */
 public class n165_Compare_Version_Numbers {
+	//cs 
 	public int compareVersion(String version1, String version2) {
-		String[] v1 = version1.split("\\.");		//F: need "\\."
+		String[] v1 = version1.split("\\.");							//F: need "\\." can't use . only
 		String[] v2 = version2.split("\\.");
-		int length1 = v1.length;
-		int length2 = v2.length;
-		int maxLen = Math.max(length1, length2);
-		for(int i=0; i<maxLen; i++) {
-			int tmp1 = 0;
-			int tmp2 = 0;
-			if(i<length1)		//need this in case i> v1.length 
-				tmp1 = Integer.parseInt(v1[i]);
-			if(i<length2)
-				tmp2 = Integer.parseInt(v2[i]);
+		
+		for(int i=0; i<Math.max(v1.length, v2.length); i++) {
+			int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;		//need this in case i> v1.length
+			int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
 			
-			if(tmp1 > tmp2) 
-				return 1;
-			else if(tmp1 < tmp2)
+			if(num1 < num2) {
 				return -1;
+			} else if(num1 > num2) {
+				return 1;
+			}
 		}
+		
 		return 0;
 	}
+	
 	public static void main(String[] args) {
 		n165_Compare_Version_Numbers obj = new n165_Compare_Version_Numbers();
 		String version1 = "1";
