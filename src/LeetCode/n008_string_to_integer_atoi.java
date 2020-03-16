7b2d582cb1e947e51a01d12375942d66a31a98d8
C3vin <spacemonkey3@gmail.com>
2020-03-16 00:38:36 -0700

    update LC 8

--- src/LeetCode/n008_string_to_integer_atoi.java
+++ src/LeetCode/n008_string_to_integer_atoi.java
@@ -1,13 +1,13 @@
 package LeetCode;
 
 public class n008_string_to_integer_atoi {
-	  public int myAtoi(String str) {
+	public int myAtoi(String str) {
 		if(str == null)
 			return 0;
 		str = str.trim();
 		if(str.length() == 0)
 			return 0;
-		
+
 		boolean isNeg = false;
 		int i = 0;
 		if(str.charAt(0) == '+' || str.charAt(0) == '-') {
@@ -15,7 +15,7 @@ public class n008_string_to_integer_atoi {
 			if(str.charAt(0) == '-')
 				isNeg = true;
 		}
-		
+
 		int res  = 0;
 		while(i < str.length()) {
 			if(str.charAt(i) < '0' || str.charAt(i) > '9')
@@ -31,37 +31,80 @@ public class n008_string_to_integer_atoi {
 			i++;
 		}
 		return isNeg? -res:res;
-	  }
-	  
-	  public float myAtof(String str) {
-		  float res = 0;
-		  float decimalpoint = 0;
-		  boolean flag = false;
-		  int base = 1;
-		  
-		  for(char c : str.toCharArray()) {
-			  if(c == '.') {
-				  flag = true;
-				  continue;
-			  }
-			  if(c < '0' || c > '9') {
-				  return 0f;			//deal with 123.45w f: float
-			  }
-			  if(flag) {
-				  decimalpoint = decimalpoint * 10 + (c - '0');		//F: need -'0'
-				  base = base *10;
-			  } else {
-				  res = res * 10 + (c - '0');
-			  }
-		  }
-		  if(decimalpoint > 0) {
-			  decimalpoint = decimalpoint / base;
-		  }
-		  return res + decimalpoint;
-	  }
-	  public static void main(String[] args) {
-		  n008_string_to_integer_atoi obj = new n008_string_to_integer_atoi();
-		  System.out.println(obj.myAtoi(" -2147483648"));
-		  System.out.println(obj.myAtof("123.45"));
-	  }
+	}
+
+	public int myAtoi2(String str) {
+		str = str.trim();				//handle " " case, before if check
+		
+		if(str == null || str.length() == 0) {
+			return 0;
+		}
+		
+
+		long res = 0;
+		int sign = 1;
+		int start = 0;
+
+		char firstChar = str.charAt(0);	//need to avoid '+' or '-' cases
+		
+		if(firstChar == '+') {			//need before '-'
+			sign = 1;
+			start++;
+		}
+		if(firstChar == '-') {
+			sign = -1;
+			start++;
+		}
+
+		for(int i=start; i<str.length(); i++) {
+			if(!Character.isDigit(str.charAt(i))) {
+				return (int)res * sign;
+			}
+			res = res * 10 + str.charAt(i) - '0';
+			if(sign == 1 && res > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+			if(sign == -1 && res > Integer.MAX_VALUE) {
+				return Integer.MIN_VALUE;
+			}
+		}
+
+		return (int)res * sign;
+	}
+
+	public float myAtof(String str) {
+		float res = 0;
+		float decimalpoint = 0;
+		boolean flag = false;
+		int base = 1;
+
+		for(char c : str.toCharArray()) {
+			if(c == '.') {
+				flag = true;
+				continue;
+			}
+			if(c < '0' || c > '9') {
+				return 0f;			//deal with 123.45w f: float
+			}
+			if(flag) {
+				decimalpoint = decimalpoint * 10 + (c - '0');		//F: need -'0'
+				base = base *10;
+			} else {
+				res = res * 10 + (c - '0');
+			}
+		}
+		if(decimalpoint > 0) {
+			decimalpoint = decimalpoint / base;
+		}
+		return res + decimalpoint;
+	}
+	public static void main(String[] args) {
+		n008_string_to_integer_atoi obj = new n008_string_to_integer_atoi();
+		System.out.println(obj.myAtoi("-"));
+		System.out.println(obj.myAtof("123.45"));
+
+		System.out.println(obj.myAtoi2(" "));
+		System.out.println(obj.myAtoi2("-"));
+		System.out.println(obj.myAtoi2("123.45"));
+	}
 }
