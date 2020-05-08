84cfa9ed08144a688173c30387f936e58999a759
C3vin <spacemonkey3@gmail.com>
2020-05-08 14:11:24 -0700

    update LC 235

--- src/LeetCode/n235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
+++ src/LeetCode/n235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -34,15 +34,23 @@ public class n235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree {
 	}
 	
 	//LC236 is Binary Tree. LC235 Binary Search Tree (left node < right node)
+	//3 cases: root, left or right 
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		if(root.val > p.val && root.val < q.val) { 
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;		//actually we don't need to check (root.val > p.val && root.val < q.val) 
+        }
+		/*		if(root.val > p.val && root.val < q.val) { 
 			return root;
 		} else if(root.val > p.val && root.val > q.val) {
 			return lowestCommonAncestor(root.left, p, q);
 		} else if(root.val < p.val && root.val < q.val) {
 			return lowestCommonAncestor(root.right, p, q);
 		}
-		return root;
+		return root;*/
 	}
 
 	public static void main(String[] args) {
