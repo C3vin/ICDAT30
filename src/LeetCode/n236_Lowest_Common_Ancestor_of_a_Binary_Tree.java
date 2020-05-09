74dc339adff1766db18cf1cb9fb24fc71908ff79
C3vin <spacemonkey3@gmail.com>
2020-05-09 03:55:35 -0700

    update LC 236

--- src/LeetCode/n236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ src/LeetCode/n236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -1,5 +1,30 @@
 package LeetCode;
 
+/*
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node 
+in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
+		   3
+		 /   \
+		5     1
+	   / \   / \
+	  6  2  0  8
+	    / \
+	   7  4
+	   
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+Output: 5
+Explanation: The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition.
+ */
+
 public class n236_Lowest_Common_Ancestor_of_a_Binary_Tree {
 	public class TreeNode {
 		int val;
@@ -13,7 +38,8 @@ public class n236_Lowest_Common_Ancestor_of_a_Binary_Tree {
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 		if(root == null || root == p || root == q) 
 			return root;
-		TreeNode left = lowestCommonAncestor(root.left, p, q);
+
+		TreeNode left = lowestCommonAncestor(root.left, p, q);		//need TreeNode
 		TreeNode right = lowestCommonAncestor(root.right, p, q);
 		
 		//nodes are each on a separate branch
@@ -23,6 +49,30 @@ public class n236_Lowest_Common_Ancestor_of_a_Binary_Tree {
 		//either one node is on one branch, or none was found in any of the branches 
 		return left == null ? right : left;
 	}
+	
+	
+	public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+		if(root == null || root == p || root == q) {
+			return root;
+		}
+		
+		TreeNode left = lowestCommonAncestor2(root.left, p, q);
+		TreeNode right = lowestCommonAncestor2(root.right, p, q);
+		
+		//can't find in the left tree, must in the right tree
+		if(left == null) {
+			return right;
+		}
+
+		//can't find in the right tree, must in the left tree
+		if(right == null) {
+			return left;
+		}
+		
+		//not in left or right, must node root
+		return root;
+	}
+	
 	public static void main(String[] args) {
 		n236_Lowest_Common_Ancestor_of_a_Binary_Tree obj = new n236_Lowest_Common_Ancestor_of_a_Binary_Tree();
 		TreeNode p1 = obj.new TreeNode(3);
@@ -45,7 +95,10 @@ public class n236_Lowest_Common_Ancestor_of_a_Binary_Tree {
 		System.out.println(p1.val + " " + p1.left.val + " " + p1.right.val + " "+ p2.left.val + " " + p2.right.val 
 				+ " " + p3.left.val + " " +p3.right.val);
 
-		System.out.println(obj.lowestCommonAncestor(p1, p2, p3).val);
-		System.out.println(obj.lowestCommonAncestor(p1, p2, p9).val);
+		/*System.out.println(obj.lowestCommonAncestor(p1, p2, p3).val);
+		System.out.println(obj.lowestCommonAncestor(p1, p2, p9).val);*/
+		
+		//System.out.println(obj.lowestCommonAncestor2(p1, p2, p3).val);
+		System.out.println(obj.lowestCommonAncestor2(p1, p8, p9).val);
 	}
 }
