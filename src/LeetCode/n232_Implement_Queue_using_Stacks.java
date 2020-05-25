7c44936956fcdc6ce7e30beeecf9e511ca200c55
C3vin <spacemonkey3@gmail.com>
2020-05-25 14:30:06 -0700

    update LC 232

--- src/LeetCode/n232_Implement_Queue_using_Stacks.java
+++ src/LeetCode/n232_Implement_Queue_using_Stacks.java
@@ -2,38 +2,69 @@ package LeetCode;
 
 import java.util.Stack;
 
+/*
+Implement the following operations of a queue using stacks.
+
+push(x) -- Push element x to the back of queue.
+pop() -- Removes the element from in front of queue.
+peek() -- Get the front element.
+empty() -- Return whether the queue is empty.
+Example:
+
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);  
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+Notes:
+
+You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+ */
 public class n232_Implement_Queue_using_Stacks {
-	Stack<Integer> s1 = new Stack<Integer>();
-	Stack<Integer> s2 = new Stack<Integer>();
-	 // Push element x to the back of queue.
-    public void push(int x) {
-        s1.push(x);
-    }
-
-    // Removes the element from in front of queue.
-    public void pop() {
-        if(!s2.isEmpty())
-        	s2.pop();
-        else {
-        	while(!s1.isEmpty())
-        		s2.push(s1.pop());
-        	s2.pop();	//F: inside the loop
-        }
-    }
-
-    // Get the front element.
-    public int peek() {
-        if(!s2.isEmpty())
-        	return s2.peek();
-        else {
-        	while(!s1.isEmpty())
-        		s2.push(s1.pop());
-        	return s2.peek();
-        }
-    }
-
-    // Return whether the queue is empty.
-    public boolean empty() {
-        return s1.empty() && s2.empty();
-    }
+	 Stack<Integer> s1;
+	    Stack<Integer> s2;
+	    
+	    /** Initialize your data structure here. */
+	    public n232_Implement_Queue_using_Stacks() {
+	        s1 = new Stack<Integer>();
+	        s2 = new Stack<Integer>();
+	    }
+	    
+	    /** Push element x to the back of queue. */
+	    public void push(int x) {
+	        s1.push(x);
+	    }
+	    
+	    /** Removes the element from in front of queue and returns that element. */
+	    public int pop() {
+	        if(!s2.isEmpty()) {
+	            return s2.pop();
+	        } else {
+	            while(!s1.isEmpty()) {
+	                s2.push(s1.pop());
+	            }
+	            return s2.pop();
+	        }
+	    }
+	    
+	    /** Get the front element. */
+	    public int peek() {
+	        if(!s2.isEmpty()) {
+	            return s2.peek();
+	        } else {
+	            while(!s1.isEmpty()) {
+	                s2.push(s1.pop());
+	            }
+	            return s2.peek();
+	        }
+	    }
+	    
+	    /** Returns whether the queue is empty. */
+	    public boolean empty() {
+	        return s1.isEmpty() && s2.isEmpty();
+	    }
 }
