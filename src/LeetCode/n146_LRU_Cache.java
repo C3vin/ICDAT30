811cc246f3d55f06ed3071f17d77c4114dbb7d3a
C3vin <spacemonkey3@gmail.com>
2020-06-02 01:57:16 -0700

    update LC 146

--- src/LeetCode/n146_LRU_Cache.java
+++ src/LeetCode/n146_LRU_Cache.java
@@ -2,6 +2,31 @@ package LeetCode;
 
 import java.util.HashMap;
 
+/*
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, 
+it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+LRUCache cache = new LRUCache( 2 /capacity/ );
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+*/
+
 public class n146_LRU_Cache {
 	class Node {
 		int key;
@@ -13,48 +38,54 @@ public class n146_LRU_Cache {
 			value = v;
 		}
 	}
-	int capacity;
-	HashMap<Integer, Node> map = new HashMap<Integer, Node>();
-	Node head = null;
-	Node end = null;
+	
+	private int capacity;
+	private HashMap<Integer, Node> map;
+	private Node head;
+	private Node tail;
 
 	public n146_LRU_Cache(int capacity) {		//constructor
 		this.capacity = capacity;
+		map = new HashMap<Integer, Node>();
+		head = null;
+		tail = null;
 	}
 
 	public int get(int key) {
 		if(map.containsKey(key)) {
-			Node n = map.get(key);
-			remove(n);
-			setHead(n);
-			return n.value;
+			Node node = map.get(key);
+			remove(node);
+			setHead(node);
+			return node.value;
 		}
 		return -1;
 	}
-	private void remove(Node n) {
-		if(n.pre!=null) {			 
-			n.pre.next = n.next;
+	private void remove(Node node) {
+		if(node.pre != null) {			 
+			node.pre.next = node.next;
 		} else {
-			head = n.next;	 		//F: means n is head
+			head = node.next;	 		//F: means n is head
 		}
-		if(n.next!=null) {
-			n.next.pre = n.pre;
+		if(node.next != null) {
+			node.next.pre = node.pre;
 		} else {
-			end = n.pre;
+			tail = node.pre;
 		}
 	}
-	private void setHead(Node n) {
-		n.next = head;
-		n.pre = null;
-		if(head!=null)
-			head.pre = n;
-
-		head=n;		 
+	private void setHead(Node node) {
+		node.next = head;
+		node.pre = null;
+		if(head != null) {
+			head.pre = node;
+		}
 		
-		if(end==null)
-			end = head;
+		head = node;		 
+		
+		if(tail == null) {
+			tail = head;				//F: must have this do deal with first time put
+		}
 	}
-	public void set(int key, int value) {
+	public void put(int key, int value) {
 		if(map.containsKey(key)) {
 			Node old = map.get(key);
 			old.value = value;			//F: need to assign new value!!!
@@ -63,8 +94,8 @@ public class n146_LRU_Cache {
 		} else {
 			Node created = new Node(key, value);
 			if(map.size() >= capacity) { 	//map.size, >= !!!
-				map.remove(end.key);  		//map.remove
-				remove(end); 				//remove (Node)
+				map.remove(tail.key);  		//map.remove
+				remove(tail); 				//remove (Node)
 				setHead(created);
 			} else {
 				setHead(created);
@@ -74,13 +105,22 @@ public class n146_LRU_Cache {
 	}
 	
 	public static void main(String[] args) {
-		n146_LRU_Cache obj = new n146_LRU_Cache(3);
+/*		n146_LRU_Cache obj = new n146_LRU_Cache(2);
 		obj.set(1, 23);
 		obj.set(2, 33);
 		obj.set(3, 56);
 		System.out.println(obj.get(2));
 		obj.set(4, 99);
-		System.out.println(obj.get(4));
-		
+		System.out.println(obj.get(4));*/
+		n146_LRU_Cache obj = new n146_LRU_Cache(2);
+		obj.put(1, 1);
+		obj.put(2, 2);
+		System.out.println(obj.get(1));       // returns 1
+		obj.put(3, 3);    // evicts key 2
+		System.out.println(obj.get(2));       // returns -1 (not found)
+		obj.put(4, 4);    // evicts key 1
+		System.out.println(obj.get(1));       // returns -1 (not found)
+		System.out.println(obj.get(3));       // returns 3
+		System.out.println(obj.get(4));       // returns 4
 	}
 }
