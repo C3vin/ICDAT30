d118aec925579e2585adf03b5a51455c7df366ee
C3vin <spacemonkey3@gmail.com>
2020-06-05 03:28:51 -0700

    update LC 56

--- src/LeetCode/n056_Merge_Intervals.java
+++ src/LeetCode/n056_Merge_Intervals.java
@@ -1,12 +1,28 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
+/*
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+Example 2:
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+ */
 public class n056_Merge_Intervals {
-	public class Interval {
+	/*	public class Interval {
 		int start;
 		int end;
 		Interval() { 
@@ -20,41 +36,49 @@ public class n056_Merge_Intervals {
 		public String toString() {
 			return "[" + start + ", " + end + "]";
 		}
-	}
-	public List<Interval> merge(List<Interval> intervals) {
-		List<Interval> res = new ArrayList<Interval>();
+	}*/
 
-		Collections.sort(intervals, new Comparator<Interval>(){     //??
-			public int compare(Interval i1, Interval i2){
-				if(i1.start!=i2.start)
-					return i1.start-i2.start;
-				else
-					return i1.end-i2.end;
-			}
+	class Interval {
+		int start;
+		int end;
+
+		public Interval(int s, int e) {
+			start = s;
+			end = e;
+		}
+	}
+
+	public int[][] merge(int[][] intervals) {
+		LinkedList<int[]> merged = new LinkedList<int[]>();		//need 'LinkedList' cuz getLast() and toArray (convert Linkedlist to Array!)
+
+		Arrays.sort(intervals, (int[] a, int[] b) -> {			//Arrays for int array
+			return a[0] - b[0];
 		});
-		Interval pre = intervals.get(0);
-		for(int i=0; i<intervals.size(); i++) {			//need i=0 ?
-			Interval curr = intervals.get(i);
-			if(curr.start > pre.end) {
-				res.add(pre);
-				pre = curr;
+
+		for(int[] interval : intervals) {
+			if(merged.isEmpty() || merged.getLast()[1] < interval[0]) {			//compare with interval[0] not 1
+				merged.add(interval);
 			} else {
-				System.out.println(pre + " : " + curr);
-				Interval merge = new Interval(pre.start,Math.max(pre.end, curr.end));		//F: need Mach.max
-				pre = merge;
+				merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
 			}
 		}
-		res.add(pre);
-		return res;
+
+
+/*		int[][] res = new int[merged.size()][];
+		for(int i=0; i<merged.size(); i++) {
+			res[i] = merged.get(i);
+		}*/
+		
+		return merged.toArray(new int[merged.size()][]);						//LinkedList convert to Array!
 	}
+	
 	public static void main(String[] args) {
 		n056_Merge_Intervals obj = new n056_Merge_Intervals();
-		List<Interval> intervals = new ArrayList<Interval>();
-		intervals.add(obj.new Interval(1, 3));
-		intervals.add(obj.new Interval(2, 6));
-		intervals.add(obj.new Interval(8, 10));
-		intervals.add(obj.new Interval(15, 18));
-		System.out.println(intervals);
+		int[][] intervals = 
+			       {{8,10},
+					{1,3},
+					{2,6},
+					{15,18}};
 		System.out.println(obj.merge(intervals));
 	}
 }
