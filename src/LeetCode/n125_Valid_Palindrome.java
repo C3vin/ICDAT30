77a64a8c77255a29682cc184947ad6dfd1811673
C3vin <spacemonkey3@gmail.com>
2020-06-21 14:17:04 -0700

    update LC 125

--- src/LeetCode/n125_Valid_Palindrome.java
+++ src/LeetCode/n125_Valid_Palindrome.java
@@ -1,44 +1,56 @@
 package LeetCode;
 
-//Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
-//"A man, a plan, a canal: Panama" is a palindrome.
-//"race a car" is not a palindrome.
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+ */
 public class n125_Valid_Palindrome {
 	public boolean isPalindrome(String s) {
-		if(s.length() == 0 || s == null || s.length() < 2) return true;
+		if(s.length() == 0 || s == null || s.length() < 2) {
+			return true;
+		}
+		
         int i=0;
         int j=s.length()-1;
         s = s.toLowerCase();		//F: change to low case, because 'A' == 'a' in this case
+        
         while(i < j) {
         	char c1 = s.charAt(i);
         	char c2 = s.charAt(j);
 
-        	if (!((c1 >= '0' && c1 <= '9') || (c1>='a' && c1 <= 'z') || (c1>='A' && c1 <= 'Z'))) {		//skip special char
+        	if (!((c1 >= '0' && c1 <= '9') || (c1>='a' && c1 <= 'z'))) {		//skip special char, but need 0-9 becasue it is not special char
 				i++;
 				continue;
 			}
-        	if (!((c2 >= '0' && c2 <= '9') || (c2>='a' && c2 <= 'z') || (c2>='A' && c2 <= 'Z'))) {
+        	if (!((c2 >= '0' && c2 <= '9') || (c2>='a' && c2 <= 'z'))) {
 				j--;
 				continue;
 			}
-		/*	if(c1>='A' && c1<='Z') {
-				c1 = (char) (c1 + 'a' - 'A');	//97(a)-65(A)=32 or c1+=32
-			}
-			if(c2>='A' && c2<='Z') {
-				c2 += 'a' - 'A';				//P->p
-			}*/
+
 			if(c1 == c2) {
 				i++;
 				j--;
-			} else 
+			} else {
 				return false;
+			}
 		}
+        
 		return true;
 	}
 	
 	public static void main(String[] args) {
 		n125_Valid_Palindrome obj = new n125_Valid_Palindrome();
 		System.out.println(obj.isPalindrome("A man, a plan, a canal: Panama"));
-		System.out.println(obj.isPalindrome("ab"));
+		System.out.println(obj.isPalindrome("0P "));
 	}
 }
