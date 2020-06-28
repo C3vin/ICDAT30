623ffd2e2f2d5fdc4b27e387b9132cb015110701
C3vin <spacemonkey3@gmail.com>
2020-06-28 04:12:54 -0700

    update LC 379

--- src/LeetCode/n379_Design_Phone_Directory.java
+++ src/LeetCode/n379_Design_Phone_Directory.java
@@ -2,28 +2,32 @@ package LeetCode;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class n379_Design_Phone_Directory {
 	int max;
 	HashSet<Integer> set;
-	LinkedList<Integer> queue;
+	Queue<Integer> queue;
+	
 	/** Initialize your data structure here
     @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
 	public n379_Design_Phone_Directory(int maxNumbers) {
         set = new HashSet<Integer>();
-        queue = new LinkedList<Integer>();
+        queue = new LinkedList<Integer>();	
         
-        for(int i=0; i<maxNumbers; i++){
+        this.max = maxNumbers;
+        
+        for (int i = 0; i < maxNumbers; i++) {
             queue.offer(i);
         }
-        max=maxNumbers-1;
 	}
 
 	/** Provide a number which is not assigned to anyone.
     @return - Return an available number. Return -1 if none is available. */
 	public int get() {
-		if(queue.isEmpty())
-			return -1;
+        if(queue.isEmpty()) {
+            return -1;
+        }
 
 		int num = queue.poll();
 		set.add(num);
@@ -32,16 +36,17 @@ public class n379_Design_Phone_Directory {
 
 	/** Check if a number is available or not. */
 	public boolean check(int number) {
-		if(!set.contains(number) && number <= max)		//F: !
-			return true;
-		return false;
+        if(set.contains(number)) {
+            return false;
+        } 
+        
+        return true;
 	}
 
 	/** Recycle or release a number. */
 	public void release(int number) {
-		if(set.contains(number)) {
-			set.remove(number);
-			queue.offer(number);		//F: why need queue.offer? because need to add to queue for check()
-		}
+        if(set.remove(number)) {
+            queue.offer(number);
+        }
 	}
 }
