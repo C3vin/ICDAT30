d35d4e0df161764759883b3b3d994253cb2a3bd1
C3vin <spacemonkey3@gmail.com>
2020-07-08 00:04:32 -0700

    update LC 7

--- src/LeetCode/n007_reverse_interger.java
+++ src/LeetCode/n007_reverse_interger.java
@@ -1,18 +1,33 @@
 package LeetCode;
 
-//Reverse digits of an integer.
-//Example1: x = 123, return 321
-//Example2: x = -123, return -321
+/*
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+ */
 public class n007_reverse_interger {
 	public int reverseInterger(int x) {
-		long result = 0;
-		while (x!= 0) {
-			result = result * 10 + (x%10);
-/*			if (result > Integer.MAX_VALUE) return Integer.MAX_VALUE; 
-			if (result < Integer.MIN_VALUE) return Integer.MIN_VALUE;*/
-			x = x/10;
-		}
-		return (int)result;
+        long res = 0;				//must use long to handle larger num
+        
+        while (x != 0) {			//while !!!
+            res = res * 10 + x % 10;
+            x = x / 10;
+            
+            if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) {
+            	return 0;
+            }
+        }
+        return (int)res;		//convert
 	}
 	public static void main(String[] args) {
 		n007_reverse_interger obj = new n007_reverse_interger();
