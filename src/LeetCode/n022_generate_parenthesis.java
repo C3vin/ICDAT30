6945c602851a9a8dcd6bfba1773c822f62228cb0
C3vin <spacemonkey3@gmail.com>
2020-07-17 21:35:56 -0700

    update LC 22

--- src/LeetCode/n022_generate_parenthesis.java
+++ src/LeetCode/n022_generate_parenthesis.java
@@ -3,33 +3,42 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.List;
 
-@Alg(type="NP", com="G", level="med", num=22)
-//[((())), (()()), (())(), ()(()), ()()()]
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+ */
 public class n022_generate_parenthesis {
 	public List<String> generateParenthesis(int n) {
 		List<String> res = new ArrayList<String>();
-		String tmp = new String();
+		String path = new String();
 		//if(n <= 0) return res;
-		helper(n, n, res, tmp);
+		helper(n, n, res, path);
 		return res;
 	}
-	private void helper(int left, int right, List<String> res, String tmp) {
+	private void helper(int left, int right, List<String> res, String path) {
 		//handle ")(" e.g. ()())(
 		if(left > right) 
 			return;
 		
 		//base case
 		if(left == 0 && right == 0) {
-/*			String s = new String(tmp);
-			res.add(s);*/
-			res.add(new String(tmp));
+			res.add(path);
 			return;
 		}
 		if(left > 0) {
-			helper(left-1, right, res, tmp+"(");
+			helper(left-1, right, res, path+"(");
 		}
 		if(right > 0) {
-			helper(left, right-1, res, tmp+")");
+			helper(left, right-1, res, path+")");
 		}
 	}
 	public static void main(String[] args) {
