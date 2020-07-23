1f760769a880de2fe1509567b8a72e1b38358bbb
C3vin <spacemonkey3@gmail.com>
2020-07-23 15:29:59 -0700

    update LC 48

--- src/LeetCode/n041_First_Missing_Positive.java
+++ src/LeetCode/n041_First_Missing_Positive.java
@@ -18,11 +18,17 @@ Note: Your algorithm should run in O(n) time and uses constant extra space.
 public class n041_First_Missing_Positive {
 	public int firstMissingPositive(int[] nums) {
 		//https://leetcode.wang/leetCode-41-First-Missing-Positive.html
+		//Constant space means all your data structures must have size that's O(1) !!! (Request!)
+		
 		if(nums == null || nums.length == 0) {
 			return 1;
 		}
+		
 		for(int i=0; i<nums.length; i++) {
 			//while not if, cuz need to keep check even after swap
+			//about the nums[nums[i] - 1] != nums[i]. 
+			//index: 0 1 2 3          n[i]          n[1] = 2
+			//value: 1 2 3 4          n[n[i] - 1]   n[n[1] - 1] = n[2 - 1] = n[1] = 2
 			while(nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
 				int tmp = nums[nums[i] - 1];
 				nums[nums[i] - 1] = nums[i];
@@ -34,7 +40,7 @@ public class n041_First_Missing_Positive {
 				return i+1;
 			}
 		}
-		return nums.length + 1;		//case []
+		return nums.length + 1;		//handle last element case
 	} 
 
 	public static void main(String[] args) {
