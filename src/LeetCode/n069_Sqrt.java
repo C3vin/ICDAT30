af29d5a00ba136aaf80a61e23d9c62b030647d5f
C3vin <spacemonkey3@gmail.com>
2020-08-17 13:24:38 -0700

    update 69 70

--- src/LeetCode/n069_Sqrt.java
+++ src/LeetCode/n069_Sqrt.java
@@ -1,31 +1,45 @@
 package LeetCode;
 
-//Implement int sqrt(int x).
-//Compute and return the square root of x.
-//x is guaranteed to be a non-negative integer.
-//Input: 4 => Output: 2
-//Input: 8 => Output: 2
+/*
+Implement int sqrt(int x).
+Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+
+Example 1:
+Input: 4
+Output: 2
+
+Example 2:
+Input: 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since 
+             the decimal part is truncated, 2 is returned.
+ */
 public class n069_Sqrt {
 	//in LC, need to use long to handle big number
 	public int mySqrt(int x) {
-		int low = 0;
-		int hight = x;
-
-		while(low <= hight) {
-			int mid = (low+hight)/2;
-
-			if(mid*mid < x) {
-				low = mid+1;
-			} else if(mid*mid > x) {
-				hight = mid-1;
-			} else 
-				return mid;
+		int left = 0;
+		int right = x;
+		
+		while(left <= right) {
+			long mid = left + (right-left)/2;		//use long to handle case 2147395599
+			
+			if(mid*mid == x) {
+				return (int)mid;
+			} else if(mid*mid < x){					// x not right
+				left = (int)mid+1;
+			} else {
+				right = (int)mid-1;
+			}
 		}
-		return hight;
+
+		return (int)right;					//right e.g. 8
 	}
+	
 	public static void main(String[] args) {
 		n069_Sqrt obj = new n069_Sqrt();
 		System.out.println(obj.mySqrt(4));
 		System.out.println(obj.mySqrt(8));
+		System.out.println(obj.mySqrt(2147395599));
 	}
 }
