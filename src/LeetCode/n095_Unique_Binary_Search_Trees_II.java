14f4610d57a62aaea553737565ae98a4b9d97652
C3vin <spacemonkey3@gmail.com>
2020-09-07 23:24:42 -0700

    update LC95 LC96

--- src/LeetCode/n095_Unique_Binary_Search_Trees_II.java
+++ src/LeetCode/n095_Unique_Binary_Search_Trees_II.java
@@ -1,9 +1,32 @@
 package LeetCode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
-@Alg(type="DP,Tree", com="NA", level="med", num=95)
+/*
+Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1 ... n.
+
+Example:
+
+Input: 3
+Output:
+[
+  [1,null,3,2],
+  [3,2,null,1],
+  [3,1,null,null,2],
+  [2,1,3],
+  [1,null,2,null,3]
+]
+Explanation:
+The above output corresponds to the 5 unique BST's shown below:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+ */
 public class n095_Unique_Binary_Search_Trees_II {
 	public class TreeNode {
 		int val;
@@ -15,30 +38,49 @@ public class n095_Unique_Binary_Search_Trees_II {
 		}
 	}
 	public List<TreeNode> generateTrees(int n) {
+		if(n == 0) {
+			return new ArrayList<>();
+		}
+
 		return helper(1,n);		//form 1 to n
 	}
-	private List<TreeNode> helper(int left, int right) {
+	private List<TreeNode> helper(int start, int end) {
 		List<TreeNode> res = new ArrayList<TreeNode>();	//why need to res in here, cuz we only have partial res
-		if(left > right) {
+
+		if(start > end) {
 			res.add(null);
+
 			return res;
 		}
-		for(int i=left; i<right+1; i++) {
-			List<TreeNode> lefts = helper(left, i-1);
-			List<TreeNode> rights = helper(i+1, right);
+
+		for(int i=start; i<=end; i++) {
+			//if i is root, left subtree: [start, i-1], right subtree: [i+1, end]
+			List<TreeNode> lefts = helper(start, i-1);		
+			List<TreeNode> rights = helper(i+1, end);
 			
-			for(int j=0; j<lefts.size(); j++) {
+			for(TreeNode left : lefts) {
+				for(TreeNode right : rights) {
+					TreeNode root = new TreeNode(i);
+					root.left = left;
+					root.right = right;
+					
+					res.add(root);
+				}
+			}
+			/*for(int j=0; j<lefts.size(); j++) {
 				for(int k=0; k<rights.size(); k++)  {
 					TreeNode root = new TreeNode(i);
 					root.left = lefts.get(j);
 					root.right = rights.get(k);
+
 					res.add(root);	//create tree
 				}
-			}
+			}*/
 		}
-		//System.out.println(res);
+		
 		return res;
 	}
+
 	public static void main(String[] args) {
 		n095_Unique_Binary_Search_Trees_II obj = new n095_Unique_Binary_Search_Trees_II();
 		System.out.println(obj.generateTrees(3));
--- src/LeetCode/n096_Unique_Binary_Search_Trees.java
+++ src/LeetCode/n096_Unique_Binary_Search_Trees.java
@@ -1,14 +1,22 @@
 package LeetCode;
 
-@Alg(type="DP,Tree", com="Snap", level="med", num=96)
-/**
- * 
- 1               1                   2                     3             3
-  \               \                /   \                  /             / 
-    3              2              1     3                2             1
-   /                 \                                  /               \
- 2                    3                                1                 2 
- *
+import java.util.HashMap;
+
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+Example:
+Input: 3
+Output: 5
+Explanation:
+Given n = 3, there are a total of 5 unique BST's:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+   
  *  Count[3] = Count[0]*Count[2]  (1 is root) left 0 node, right 2 node
                + Count[1]*Count[1]  (2 is root) left 1 node, right 1 node
                + Count[2]*Count[0]  (3 is root) left 2 node, right 0 node
@@ -27,8 +35,47 @@ public class n096_Unique_Binary_Search_Trees {
 		return c[n];
 	}
 	
+	//good DP sol: https://medium.com/@bill800227/leetcode-96-unique-binary-search-trees-abce6e62a7a0
+	
+	//better to understand!
+	//https://leetcode.com/problems/unique-binary-search-trees/discuss/493155/Simple-recursion-with-Memoization-in-Java-with-comments
+	HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	public int numTrees2(int n) {
+		int res = 0;
+		
+		//Empty tree
+		if(n == 0) {
+			return 1;
+		}
+		
+		//One tree with root as 1
+		if(n == 1) {
+			return 1;
+		}
+		
+		//Two trees 
+		if(n == 2) {
+			return 2;
+		}
+		
+		if(map.containsKey(n)) {
+			return map.get(n);
+		}
+		
+		for(int i=1; i<=n; i++) {		//not <, need <=, cuz n=3, root=1, right: 3-1 = 2
+			int left = numTrees2(i-1);	//when i is root then we can have as many as i-1 as nodes on the left
+			int right = numTrees2(n-i); //as many as n-i as right subtree nodes
+										//F(i,n) = G(i - 1) x G(n - i)
+			res = res + left*right;		//take the Cartesian product or all the combinations for every left and right subtree
+		}
+		map.put(n, res);
+		
+		return res;
+	}
+	
 	public static void main(String[] args) {
 		n096_Unique_Binary_Search_Trees obj = new n096_Unique_Binary_Search_Trees();
 		System.out.println(obj.numTrees(3));
+		System.out.println(obj.numTrees2(3));
 	}
 }
