7318ae9cb5b2881c777c516253efe61647493d1e
C3vin <spacemonkey3@gmail.com>
2020-09-27 14:30:50 -0700

    update LC114

--- src/LeetCode/n114_Flatten_Binary_Tree_to_Linked_List.java
+++ src/LeetCode/n114_Flatten_Binary_Tree_to_Linked_List.java
@@ -31,6 +31,35 @@ public class n114_Flatten_Binary_Tree_to_Linked_List {
 		TreeNode right;
 		TreeNode(int x) { val = x; }
 	} 
+
+	//BT template 
+	public void flatten3(TreeNode root) {
+		//base case
+		if(root == null) {
+			return;
+		}
+
+		//1. start flatten into a linked list
+		flatten3(root.left);		//need to be first
+		flatten3(root.right);
+
+		//post-order !
+		TreeNode left = root.left;
+		TreeNode right = root.right;
+
+		//2. move left under right node, replace the original right 
+		root.left = null;    
+		root.right = left;
+
+		//3. get the original right node under new right node
+		TreeNode tmp = root;
+		while(tmp.right != null) {
+			tmp = tmp.right;    
+		}
+		tmp.right = right;
+		
+	}
+
 	public void flatten(TreeNode root) {
 		//Preorder 
 		if(root == null) {
@@ -42,24 +71,25 @@ public class n114_Flatten_Binary_Tree_to_Linked_List {
 		if(root.right != null) {
 			flatten(root.right);
 		}
-		
+
 		TreeNode currentNode = root.right;
 		root.right = root.left;
 		root.left = null;
-		
+
 		while(root.right != null) {
 			root = root.right;
 		}
 		root.right = currentNode;
 	} 
-	
+
+	//not use
 	public void flatten2(TreeNode root) {
 		if(root == null) {
 			return; 
 		}
 		Stack<TreeNode> stack = new Stack<TreeNode>();
 		stack.push(root);
-		
+
 		while(!stack.isEmpty()) {
 			TreeNode currentNode = stack.pop();
 			if(currentNode.right != null) {
@@ -74,7 +104,7 @@ public class n114_Flatten_Binary_Tree_to_Linked_List {
 			currentNode.left = null;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		n114_Flatten_Binary_Tree_to_Linked_List obj = new n114_Flatten_Binary_Tree_to_Linked_List();
 		TreeNode p1 = obj.new TreeNode(1);
@@ -88,9 +118,10 @@ public class n114_Flatten_Binary_Tree_to_Linked_List {
 		p2.left = p3;
 		p2.right = p4;
 		p5.right = p6;
-		
+
 		//obj.flatten(p1);
-		obj.flatten2(p1);
+		//obj.flatten2(p1);
+		obj.flatten3(p1);
 		System.out.println(p1.val + " -> " + p1.right.val);
 	}
 }
