7a9ae2da2c09732fb394271d6a5aeee2ac6a854e
C3vin <spacemonkey3@gmail.com>
2021-02-04 23:43:50 -0800

    update LC 350

--- src/LeetCode/n350_Intersection_of_Two_Arrays_II.java
+++ src/LeetCode/n350_Intersection_of_Two_Arrays_II.java
@@ -15,8 +15,8 @@ public class n350_Intersection_of_Two_Arrays_II {
 		}
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(int i=0; i<nums2.length; i++) {
-			if(map.containsKey(nums2[i])) {
-				if(map.get(nums2[i]) > 0) {			//need to check containsKey first, java.lang.NullPointerException
+			if(map.containsKey(nums2[i])) { //need to check containsKey first, java.lang.NullPointerException
+				if(map.get(nums2[i]) > 0) {	//need to check > 0, because might added already		
 					map.put(nums2[i], map.get(nums2[i])-1);
 					list.add(nums2[i]);
 				}
@@ -32,6 +32,7 @@ public class n350_Intersection_of_Two_Arrays_II {
 		n350_Intersection_of_Two_Arrays_II obj = new n350_Intersection_of_Two_Arrays_II();
 		int[] nums1 = {1,2,2,1};
 		int[] nums2 = {2,2};
-		System.out.println(obj.intersect(nums1, nums2));
+		//System.out.println(obj.intersect(nums1, nums2));
+		System.out.println(obj.intersect(new int[] {4,9,5}, new int[] {9,4,9,8,4}));
 	}
 }
