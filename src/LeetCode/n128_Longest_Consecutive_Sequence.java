09b41e7c15e06882f7530e30c55e4400479ee66a
C3vin <spacemonkey3@gmail.com>
2021-02-10 17:38:34 -0800

    update LC 128

--- src/LeetCode/n128_Longest_Consecutive_Sequence.java
+++ src/LeetCode/n128_Longest_Consecutive_Sequence.java
@@ -1,5 +1,6 @@
 package LeetCode;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 /*
@@ -12,37 +13,65 @@ Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
  */
 public class n128_Longest_Consecutive_Sequence {
-	//cs time:O(n) space:O(n)
+	//O(n)
 	public int longestConsecutive(int[] nums) {
-		if(nums == null || nums.length == 0) {
-			return 0;
-		}
 		HashSet<Integer> set = new HashSet<Integer>();
-		int res = 0;
-		
 		for(int num : nums) {
 			set.add(num);
 		}
-		
+
+		int max = 0;
+
 		for(int i=0; i<nums.length; i++) {
-			int down = nums[i]-1;
-			while(set.contains(down)) {
-				set.remove(down);
-				down--;
+			int num = nums[i];				//need to get first!!!
+
+			if(!set.contains(num -1)) {		//will block until it find the smallest, e.g 987654, only 4 will go into while loop
+				int count = 0;
+
+				while(set.contains(num)) {
+					count++;
+
+					num = num + 1;  		//from smallest to large
+				}
+
+				max = Math.max(max, count);
 			}
-			
-			int up = nums[i]+1;
-			while(set.contains(up)) {
-				set.remove(up);
-				up++;
-			}
-			
-			res = Math.max(res, up - down -1);		//at least res = 1 (itself)
 		}
-		return res;
+
+		return max;
 	}
+
+	//O(nlogn)
+	public int longestConsecutive2(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		Arrays.sort(nums);
+
+		int maxCount = 1;
+		int count = 1;
+
+		for(int i=1; i<nums.length; i++) {
+			if(nums[i] != nums[i-1]) {					//to ignore duplicate element e.g second '0'
+				if(nums[i] - nums[i-1] == 1) {
+					count++;
+				} else {
+					maxCount = Math.max(maxCount, count);
+					count = 1;							//reset count to default
+				}
+			} 
+		}
+
+
+		return Math.max(maxCount, count);
+	}
+
 	public static void main(String[] args) {
 		n128_Longest_Consecutive_Sequence obj = new n128_Longest_Consecutive_Sequence();
 		System.out.println(obj.longestConsecutive(new int[] {100,4,200,1,3,2}));
+		System.out.println(obj.longestConsecutive2(new int[] {100,4,200,1,3,2}));
+		System.out.println(obj.longestConsecutive(new int[] {0,3,7,2,5,8,4,6,0,1}));
+		System.out.println(obj.longestConsecutive2(new int[] {0,3,7,2,5,8,4,6,0,1}));
 	}
 }
