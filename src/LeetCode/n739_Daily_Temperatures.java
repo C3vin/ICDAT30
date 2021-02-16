942d61f4866ca12c065acf1de550f40c8725e1f1
C3vin <spacemonkey3@gmail.com>
2021-02-16 10:19:41 -0800

    add LC 739

---
+++ src/LeetCode/n739_Daily_Temperatures.java
@@ -0,0 +1,62 @@
+package LeetCode;
+
+import java.util.Stack;
+
+/*
+ * Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+
+Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+ */
+public class n739_Daily_Temperatures {
+	public int[] dailyTemperatures(int[] T) {
+		int[] res = new int[T.length];
+
+		int idx = 0;
+		for(int i=0; i<T.length; i++) {
+			idx = i;
+			for(int j=i+1; j<T.length; j++) {
+				if(T[j] > T[i]) {
+					idx = j;
+					break;
+				}
+			}
+			res[i] = idx - i;
+		}
+
+		for(int r : res) {
+			System.out.print(r);
+		}
+		
+		return res;
+	}
+
+	//Stack Good! 
+	public int[] dailyTemperatures2(int[] T) {
+		int[] res = new int[T.length];
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(int i=0; i<T.length; i++) {
+			while(!stack.isEmpty() && T[i] > T[stack.peek()]) {			//while!!! not if
+				int idx = stack.pop();
+				res[idx] = i - idx;										//idx not store to i (e.g. 6(76) - 2(75) = 4) 
+			}
+			
+			stack.push(i);												//need to add all elements
+		}
+		
+		for(int r : res) {
+			System.out.print(r);
+		}
+		
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		n739_Daily_Temperatures obj = new n739_Daily_Temperatures();
+		System.out.println(obj.dailyTemperatures(new int[] {73, 74, 75, 71, 69, 72, 76, 73}));
+		System.out.println(obj.dailyTemperatures2(new int[] {73, 74, 75, 71, 69, 72, 76, 73}));
+	}
+}
