f534cb0b1778e30cb19818a6e54e2bacc9c5883c
C3vin <spacemonkey3@gmail.com>
2021-02-16 15:25:39 -0800

    update LC 237

--- src/LeetCode/n237_Delete_Node_in_a_Linked_List.java
+++ src/LeetCode/n237_Delete_Node_in_a_Linked_List.java
@@ -6,21 +6,45 @@ public class n237_Delete_Node_in_a_Linked_List {
 		ListNode next;
 		ListNode(int x) { val = x; }
 	}
+	
+	/*
+	 * [4,5,1',2] | [4,1,1',2] | [4,1,1',2] |       [4,1,2]   node = 5  
+          ^           ^             |____|
+         node       node      node.next linked       1' lost linked
+         
+	 */
+	
 	public void deleteNode(ListNode node) {
-		if(node == null) 
+		if(node == null) {
 			return;
+		}
+		
 		node.val = node.next.val;			//update node val
 		node.next = node.next.next;			//update next node
 	}
 	
+	//Good red: https://leetcode.com/problems/delete-node-in-a-linked-list/discuss/461683/java-100-both-or-Solution-Explained
+	public void deleteNode2(ListNode node) {
+		while (true) {
+			node.val = node.next.val;
+			if (node.next.next == null) {
+				node.next = null;
+				return;
+			}
+			node = node.next;
+		}
+	}
+	
 	public static void main(String[] args) {
 		n237_Delete_Node_in_a_Linked_List obj = new n237_Delete_Node_in_a_Linked_List();
-		ListNode p1 = obj.new ListNode(1);
-		ListNode p2 = obj.new ListNode(2);
-		ListNode p3 = obj.new ListNode(3);
-		ListNode p4 = obj.new ListNode(4);
+		ListNode p1 = obj.new ListNode(4);
+		ListNode p2 = obj.new ListNode(5);
+		ListNode p3 = obj.new ListNode(1);
+		ListNode p4 = obj.new ListNode(2);
 		p1.next = p2;
 		p2.next = p3;
 		p3.next = p4;
+		
+		obj.deleteNode(p1);
 	}
 }
