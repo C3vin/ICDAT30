e6ee635ee9e0cf7d8810f9d674f63b5a836e97b0
C3vin <spacemonkey3@gmail.com>
2021-03-04 12:00:29 -0800

    update LC 434

--- src/LeetCode/n434_Number_of_Segments_in_a_String.java
+++ src/LeetCode/n434_Number_of_Segments_in_a_String.java
@@ -4,10 +4,14 @@ package LeetCode;
 public class n434_Number_of_Segments_in_a_String {
 	  public int countSegments(String s) {
 		  int count=0;
+		  
 		  for(int i=0; i<s.length(); i++) {
-			  if(s.charAt(i)==' ')		//F: ' '(char) not " "(string)
+			  if(s.charAt(i)==' ') {		//F: ' '(char) not " "(string)
 				  continue;
+			  }
+			  
 			  count++;
+			  
 			  while(s.charAt(i) != ' ' && i<s.length()-1) {		//F: need -1, cuz index range
 				  i++;
 			  }
@@ -16,6 +20,7 @@ public class n434_Number_of_Segments_in_a_String {
 	  }
 	  public static void main(String[] args) {
 		  n434_Number_of_Segments_in_a_String obj = new n434_Number_of_Segments_in_a_String();
-		  System.out.println(obj.countSegments("Hello, my name is John"));
+		  //System.out.println(obj.countSegments("Hello, my name is John"));
+		  System.out.println(obj.countSegments(", , , ,        a, eaefa"));
 	  }
 }
