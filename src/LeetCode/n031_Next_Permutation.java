63d50a56c346bb3c73092f641935501beedc710a
C3vin <spacemonkey3@gmail.com>
2021-03-12 23:42:49 -0800

    update LC 31

--- src/LeetCode/n031_Next_Permutation.java
+++ src/LeetCode/n031_Next_Permutation.java
@@ -28,26 +28,33 @@ e.g.
  */
 public class n031_Next_Permutation {
 	public void nextPermutation(int[] nums) {
+
 		int i = nums.length-2;
+
 		while(i>=0 && nums[i+1] <= nums[i]) {	//find out first non-increasing 'index', keep while looping
 			i--;
 		}
-		
+
 		//handle special case
 		if(i < 0) {
 			reverse(nums, 0, nums.length-1); 	//3,2,1
+
+			for(int n : nums)
+				System.out.print(n);
+			System.out.print("\n");
+
 			return;								//need this return to stop here
 		}
-		
+
 		int j = nums.length-1;
 		while(j>=0 && nums[j] <= nums[i]) {		//find out first 'index' large than nums[i]
 			j--;
 		}
-		
+
 		swap(nums, i, j);
 		reverse(nums, i+1, nums.length-1);
-		
-		for(int n:nums)
+
+		for(int n : nums)
 			System.out.print(n);
 		System.out.print("\n");
 	}
@@ -60,18 +67,47 @@ public class n031_Next_Permutation {
 		}
 	}
 	private void swap(int[] nums, int start, int end) {
-		int tmp = nums[end];
-		nums[end] = nums[start];
-		nums[start] = tmp;
+		int tmp = nums[start];
+		nums[start] = nums[end];
+		nums[end] = tmp;
 	}
-	
+
+	//Good!!! 
+	public void nextPermutation2(int[] nums) {
+		if(nums == null || nums.length <= 1) {
+			return;
+		}
+
+		int i = nums.length - 2;					//why -2? cuz from back to front, compare n[i](n.length-2) vs n[i+1](n.length-1) 
+		while(i >= 0 && nums[i] >= nums[i + 1]) {   //{1, 2, 7, 4, 3, 1}
+			i--; 									// Find 1st id i that breaks descending order
+		}
+
+		if(i >= 0) {                           		// If not entirely descending
+			int j = nums.length - 1;              	// Start from the end
+			while(nums[j] <= nums[i]) {				// handle this case e.g. {1, 2, 7, 4, 3, 1} 
+				j--;           						// Find rightmost first larger id j
+			}
+
+			swap(nums, i, j);                     	// Switch i and j
+		}
+		reverse(nums, i + 1, nums.length - 1);      // Reverse the descending sequence
+		
+		for(int n : nums) {
+			System.out.print(n);
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
 		n031_Next_Permutation obj = new n031_Next_Permutation();
-		obj.nextPermutation(new int[] {1});
-		obj.nextPermutation(new int[] {1, 2, 3});
-		obj.nextPermutation(new int[] {3, 2, 1});
-		obj.nextPermutation(new int[] {1, 2, 7, 4, 3, 1});
-		obj.nextPermutation(new int[] {1, 2, 0, 3, 0, 1, 2, 4});
-		obj.nextPermutation(new int[] {5,4,7,5,3,2});
+//		obj.nextPermutation2(new int[] {1, 2, 3});
+//		obj.nextPermutation2(new int[] {3, 2, 1});
+//		obj.nextPermutation2(new int[] {1, 1, 5});
+//		obj.nextPermutation2(new int[] {1});
+
+		obj.nextPermutation2(new int[] {1, 2, 7, 4, 3, 1});
+		obj.nextPermutation2(new int[] {1, 2, 0, 3, 0, 1, 2, 4});
+		obj.nextPermutation2(new int[] {5, 4, 7, 5, 3, 2});
 	}
 }
