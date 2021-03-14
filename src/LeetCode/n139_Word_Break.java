c6b84489a8c6b5ef37bdf8b72939a4e0febdb454
C3vin <spacemonkey3@gmail.com>
2021-03-13 16:03:28 -0800

    add LC 139

--- src/LeetCode/n139_Word_Break.java
+++ src/LeetCode/n139_Word_Break.java
@@ -3,8 +3,9 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
+import java.util.Queue;
 
 /*
 Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented 
@@ -47,9 +48,69 @@ public class n139_Word_Break {
 		return dp[s.length()];
 	}
 	
+	//https://leetcode.com/problems/word-break/discuss/43886/Evolve-from-brute-force-to-optimal-a-review-of-all-solutions
+	//BFS better! Good
+	public boolean wordBreak2(String s, List<String> wordDict) {
+		HashSet<String> dict = new HashSet(wordDict);				//don't forget put wordDict
+		Queue<Integer> queue = new LinkedList<Integer>();
+		queue.offer(0);
+		boolean[] visited = new boolean[s.length()];
+		
+		while(!queue.isEmpty()) {
+			int levelSize = queue.size();
+			
+			for(int i=0; i<levelSize; i++) {
+				int curIdx = queue.poll();
+				
+				if(curIdx == s.length()) {
+					return true;
+				}
+				
+				if(!visited[curIdx]) {
+					for(int nextIdx=curIdx+1; nextIdx<=s.length(); nextIdx++) {				//need <= cuz curIdx+1 and substring end boundary 
+						if(dict.contains(s.substring(curIdx, nextIdx))) {
+							queue.offer(nextIdx);
+							visited[curIdx] = true;
+						}
+					}
+				}
+			}
+		}
+		
+		return false;	
+	}
+	
+	//DFS
+	public boolean wordBreak3(String s, List<String> wordDict) {
+		HashSet<String> dict = new HashSet<String>(wordDict);	//don't forget put wordDict
+		int curIdx = 0;
+		
+		return dfs(s, dict, curIdx);
+	}
+	
+	private boolean dfs(String s, HashSet<String> dict, int curIdx) {
+		if(curIdx == s.length()) {
+			return true;
+		}
+		
+		for(int nextIdx=curIdx+1; nextIdx<=s.length(); nextIdx++) {
+			if(dict.contains(s.substring(curIdx, nextIdx)) && dfs(s, dict, nextIdx)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
 	public static void main(String[] args) {
 		n139_Word_Break obj = new n139_Word_Break();
 		System.out.println(obj.wordBreak("leetcode", new ArrayList<String>(Arrays.asList("leet", "code"))));
 		System.out.println(obj.wordBreak("catsandog", new ArrayList<String>(Arrays.asList("cats","dog","sand","and","cat"))));
+		
+		System.out.println(obj.wordBreak2("leetcode", new ArrayList<String>(Arrays.asList("leet", "code"))));
+		System.out.println(obj.wordBreak2("catsandog", new ArrayList<String>(Arrays.asList("cats","dog","sand","and","cat"))));
+		
+		System.out.println(obj.wordBreak3("leetcode", new ArrayList<String>(Arrays.asList("leet", "code"))));
+		System.out.println(obj.wordBreak3("catsandog", new ArrayList<String>(Arrays.asList("cats","dog","sand","and","cat"))));
 	}
 }
