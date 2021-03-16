51a5d73a2999d48bfc3ee4a5d32d6a23d9439770
C3vin <spacemonkey3@gmail.com>
2021-03-15 17:11:54 -0700

    update LC 81

--- src/LeetCode/n081_Search_in_Rotated_Sorted_Array_II.java
+++ src/LeetCode/n081_Search_in_Rotated_Sorted_Array_II.java
@@ -18,6 +18,15 @@ This is a follow up problem to Search in Rotated Sorted Array, where nums may co
 Would this affect the run-time complexity? How and why?
  */
 public class n081_Search_in_Rotated_Sorted_Array_II {
+	
+	//Good!!! 
+	/* https://www.cnblogs.com/grandyang/p/4325840.html
+	现在数组中允许出现重复数字，这个也会影响我们选择哪半边继续搜索，由于之前那道题不存在相同值，
+	我们在比较中间值和最右值时就完全符合之前所说的规律：如果中间的数小于最右边的数，则右半段是有序的，
+	若中间数大于最右边数，则左半段是有序的。而如果可以有重复值，就会出现来面两种情况，[3 1 1] 和 [1 1 3 1]，
+	对于这两种情况中间值等于最右值时，目标值3既可以在左边又可以在右边，那怎么办么，对于这种情况其实处理非常简单，
+	只要把最右值向左一位即可继续循环，如果还相同则继续移，直到移到不同值为止，然后其他部分还采用 Search in Rotated Sorted Array 中的方法
+	 */
 	public boolean search(int[] nums, int target) {
 		int left=0;
 		int right=nums.length-1;
