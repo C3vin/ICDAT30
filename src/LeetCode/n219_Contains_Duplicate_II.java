6543e7d36089642ced348d9b663b1360a5d85f47
C3vin <spacemonkey3@gmail.com>
2021-03-22 23:50:48 -0700

    update LC 219

--- src/LeetCode/n219_Contains_Duplicate_II.java
+++ src/LeetCode/n219_Contains_Duplicate_II.java
@@ -3,8 +3,8 @@ package LeetCode;
 import java.util.HashMap;
 import java.util.HashSet;
 /*
-Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the 
-array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j 
+in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
 Example 1:
 Input: nums = [1,2,3,1], k = 3
