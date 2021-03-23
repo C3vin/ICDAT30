10786b21e07c89e1eaedda94a4d34139d629e7a5
C3vin <spacemonkey3@gmail.com>
2021-03-22 23:29:14 -0700

    update LC 216

--- src/LeetCode/n216_Combination_Sum_III.java
+++ src/LeetCode/n216_Combination_Sum_III.java
@@ -19,6 +19,33 @@ Input: k = 3, n = 9
 Output: [[1,2,6], [1,3,5], [2,3,4]]
 */
 public class n216_Combination_Sum_III {
+	//Good!
+	public List<List<Integer>> combinationSum3_2(int k, int n) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> tmp = new ArrayList<Integer>();
+        
+        int start = 1;
+        
+        helper(k, n, tmp, res, start);
+        
+        return res;
+	}
+	
+	private void helper(int k, int n, List<Integer> tmp, List<List<Integer>> res, int start) {
+        if(tmp.size() == k && n == 0) {
+            res.add(new ArrayList<Integer>(tmp));
+            return;
+        }
+        
+        for(int i=start; i<=9; i++) {
+            tmp.add(i);
+            
+            helper(k, n-i, tmp, res, i+1);
+            
+            tmp.remove(tmp.size()-1);
+        }
+    }
+	
 	public List<List<Integer>> combinationSum3(int k, int n) {
 		List<List<Integer>> res = new ArrayList<List<Integer>>();
 		List<Integer> tmp = new ArrayList<Integer>();
@@ -51,5 +78,9 @@ public class n216_Combination_Sum_III {
 		n216_Combination_Sum_III obj = new n216_Combination_Sum_III();
 		System.out.println(obj.combinationSum3(3, 7));
 		System.out.println(obj.combinationSum3(3, 9));
+		
+		System.out.println(obj.combinationSum3_2(3, 7));
+		System.out.println(obj.combinationSum3_2(3, 9));
+		
 	}
 }
