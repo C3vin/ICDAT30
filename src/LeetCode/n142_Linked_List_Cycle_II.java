a10f9ca117a94cfe7e02238960e847f85eeaf877
C3vin <spacemonkey3@gmail.com>
2021-04-01 20:51:45 -0700

    update LC 142 LC 287

--- src/LeetCode/n142_Linked_List_Cycle_II.java
+++ src/LeetCode/n142_Linked_List_Cycle_II.java
@@ -6,6 +6,15 @@ Follow up:
 Can you solve it without using extra space?*/
 
 public class n142_Linked_List_Cycle_II {
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
 	public ListNode detectCycle(ListNode head) {
 		ListNode slow = head;
 		ListNode fast = head; 
@@ -20,17 +29,26 @@ public class n142_Linked_List_Cycle_II {
 					slow = slow.next;
 					fast = fast.next;
 				}
-				
+				//System.out.println(slow.val);
 				return slow;
 			}
 		}
-		
+
 		return null;
 	}
 	public static void main(String[] args) {
 		n142_Linked_List_Cycle_II obj = new n142_Linked_List_Cycle_II();
-		ListNode head = ListNode.create(12345);
-		head.next.next.next = head;
-		System.out.println(obj.detectCycle(head));		//can't run
+		//ListNode head = ListNode.create();
+		//head.next.next.next = head;
+		ListNode p1 = obj.new ListNode(3);
+		ListNode p2 = obj.new ListNode(2);
+		ListNode p3 = obj.new ListNode(0);
+		ListNode p4 = obj.new ListNode(-4);
+		p1.next = p2;
+		p2.next = p3;
+		p3.next = p4;
+		p4.next = p2;
+		
+		System.out.println(obj.detectCycle(p1));		//can't run
 	}
 }
--- src/LeetCode/n287_Find_the_Duplicate_Number.java
+++ src/LeetCode/n287_Find_the_Duplicate_Number.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /*
 Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
@@ -22,33 +24,10 @@ Your runtime complexity should be less than O(n2).
 There is only one duplicate number in the array, but it could be repeated more than once.
  */
 public class n287_Find_the_Duplicate_Number {
-	/*    0 1 2 3 4
-	 *    1,3,4,2,2 len=5
-	 *    mid=2, count=3
-	*/
-	//cs time:O(nlogn) space:O(1)
-	public int findDuplicate(int[] nums) {
-		int min = 0;
-		int max = nums.length-1;
-		while(min <= max) {
-			int mid = (max - min)/2 + min;
-			int count = 0;
-			for(int i=0; i<nums.length; i++) {
-				if(nums[i] <= mid) {
-					count++;
-				}
-			}
-			if(count > mid) {
-				max = mid-1;
-			} else {
-				min = mid+1;
-			}
-		}
-		return min;
-	}
 	//time:O(nlogn) space:O(1)
 	public int findDuplicate2(int[] nums) {
 		Arrays.sort(nums);
+		
 		for(int i=1; i<nums.length; i++) {
 			if(nums[i] == nums[i-1]) {
 				return nums[i];
@@ -57,31 +36,76 @@ public class n287_Find_the_Duplicate_Number {
 		return -1;
 	}
 	
+	//O(n) O(n) space		//LC 142
+	public int findDuplicate4(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return num;
+            }
+            
+            set.add(num);
+        }
+
+        return -1;
+    }
+	
 	//Floyd's Tortoise and Hare (Cycle Detection)
+	//O(n) O(1) space !!!
 	public int findDuplicate3(int[] nums) {
-		int fast = nums[0];
-		int slow = nums[nums[0]];		//s&f not the same!
+		int slow = nums[0];
+		int fast = nums[nums[0]];		//s&f not the same!!!
 		
 		while(slow != fast) {			//!= not <
 			slow = nums[slow];
 			fast = nums[nums[fast]];
 		}
 		
-		fast = 0;						//0 not nums[0]
+		//fast = nums[0]; //0;						//0 not nums[0]
+		slow = 0;
 		while(slow != fast) {
 			slow = nums[slow];
 			fast = nums[fast];
 		}
+		
 		return slow;
+	}
+	
+	/*    0 1 2 3 4
+	 *    1,3,4,2,2 len=5
+	 *    mid=2, count=3
+	*/
+	//cs time:O(nlogn) space:O(1)    Don't use it
+	public int findDuplicate(int[] nums) {
+		int left = 0;
+		int right = nums.length-1;
+		while(left <= right) {
+			int mid = left + (right - left)/2;
+			int count = 0;
+			
+			for(int i=0; i<nums.length; i++) {
+				if(nums[i] <= mid) {
+					count++;
+				}
+			}
+			
+			if(count > mid) {
+				right = mid-1;
+			} else {
+				left = mid+1;
+			}
+		}
+		return left;
 	}
 	
 	public static void main(String[] args) {
 		n287_Find_the_Duplicate_Number obj = new n287_Find_the_Duplicate_Number();
-		System.out.println(obj.findDuplicate(new int[] {1,3,4,2,2}));
-		System.out.println(obj.findDuplicate(new int[] {3,1,3,4,2}));
-		System.out.println(obj.findDuplicate2(new int[] {1,3,4,2,2}));
-		System.out.println(obj.findDuplicate2(new int[] {3,1,3,4,2}));
-		System.out.println(obj.findDuplicate3(new int[] {1,3,4,2,2}));
+//		System.out.println(obj.findDuplicate(new int[] {1,3,4,2,2}));
+//		System.out.println(obj.findDuplicate(new int[] {3,1,3,4,2}));
+//		System.out.println(obj.findDuplicate2(new int[] {1,3,4,2,2}));
+//		System.out.println(obj.findDuplicate2(new int[] {3,1,3,4,2}));
+//		System.out.println(obj.findDuplicate3(new int[] {1,3,4,2,2}));
 		System.out.println(obj.findDuplicate3(new int[] {3,1,3,4,2}));
 	}
 }
