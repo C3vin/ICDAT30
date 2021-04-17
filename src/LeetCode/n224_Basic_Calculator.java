49cc34f1539fa8442ea9b0f19166d364f30fe3aa
C3vin <spacemonkey3@gmail.com>
2021-04-17 16:07:37 -0700

    update LC 224

--- src/LeetCode/n224_Basic_Calculator.java
+++ src/LeetCode/n224_Basic_Calculator.java
@@ -32,6 +32,52 @@ if c == '(': Push this context sign to stack;
 if c == ')': Pop this context and we come back to last context;
 Add the last num. This is because we only add number after '+' / '-'.
 	 */
+	//Good
+	public int calculate2(String s) {
+		Stack<Integer> stack = new Stack<Integer>();
+		int operand = 0;
+		int res = 0;
+		int sign = 1;
+
+		for(int i=0; i<s.length(); i++) {
+			char c = s.charAt(i);
+
+			if(Character.isDigit(c)) {
+				operand = operand * 10 + (int)(c - '0');
+			} else if(c == '+') {
+				res = res + sign * operand;		//if we use operand need reset later
+
+				sign = 1;			//set '+' sign
+				operand = 0;		//reset
+			} else if(c == '-') {
+				res = res + sign * operand;
+
+				sign = -1;			//set '-' sign
+				operand = 0;		//reset	operand
+			} else if(c == '(') {
+				// Push the result and sign on to the stack, for later
+				// We push the result first, then sign
+				stack.push(res);	//1. res
+				stack.push(sign);	//2. sign
+
+				sign = 1;			//reset sign
+				res = 0;			//reset res !!! not operand
+
+			} else if(c == ')') {
+				res = res + sign * operand;
+
+				res = res * stack.pop();		//this is sign, so *
+				res = res + stack.pop();		//this is res, so +
+
+				operand = 0;		//reset operand
+			}
+		}
+
+		res = res + sign * operand;	//handle last element without ')' e.g. 1+1
+
+		return res;
+	}
+
 	public int calculate(String s) {
 		if(s == null) {
 			return 0;
@@ -69,58 +115,12 @@ Add the last num. This is because we only add number after '+' / '-'.
 		return res;
 	}
 
-	//Good
-	public int calculate2(String s) {
-		Stack<Integer> stack = new Stack<Integer>();
-		int operand = 0;
-		int res = 0;
-		int sign = 1;
-
-		for(int i=0; i<s.length(); i++) {
-			char c = s.charAt(i);
-
-			if(Character.isDigit(c)) {
-				operand = operand * 10 + (int)(c - '0');
-			} else if(c == '+') {
-				res = res + sign * operand;		//if we use operand need reset later
-
-				sign = 1;			//set '+' sign
-				operand = 0;		//reset
-			} else if(c == '-') {
-				res = res + sign * operand;
-
-				sign = -1;			//set '-' sign
-				operand = 0;		//reset	operand
-			} else if(c == '(') {
-				// Push the result and sign on to the stack, for later
-				// We push the result first, then sign
-				stack.push(res);
-				stack.push(sign);
-
-				sign = 1;			//reset sign
-				res = 0;			//reset res !!! not operand
-
-			} else if(c == ')') {
-				res = res + sign * operand;
-
-				res = res * stack.pop();
-				res = res + stack.pop();
-
-				operand = 0;		//reset operand
-			}
-		}
-
-		res = res + sign * operand;	//handle last element without ')' e.g. 1+1
-
-		return res;
-	}
-
 	public static void main(String[] args) {
 		n224_Basic_Calculator obj = new n224_Basic_Calculator();
-		System.out.println(obj.calculate("1+1"));
-		//		System.out.println(obj.calculate("2-1+2"));
-		System.out.println(obj.calculate("(4-(1+2)+6)"));
-		System.out.println(obj.calculate("(1+(4+5+2)-3)+(6+8)"));
+		//System.out.println(obj.calculate("1+1"));
+//		System.out.println(obj.calculate("2-1+2"));
+//		System.out.println(obj.calculate("(4-(1+2)+6)"));
+//		System.out.println(obj.calculate("(1+(4+5+2)-3)+(6+8)"));
 
 		System.out.println(obj.calculate2("1+1"));
 		System.out.println(obj.calculate2("2-1+2"));
