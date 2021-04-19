36a5b528de5a04dc0f5da84340bffa7f454b62d9
C3vin <spacemonkey3@gmail.com>
2021-04-19 11:12:45 -0700

    update LC 91

--- src/LeetCode/n091_Decode_Ways.java
+++ src/LeetCode/n091_Decode_Ways.java
@@ -51,6 +51,11 @@ public class n091_Decode_Ways {
 
 	//better, but need to understand
 	public int numDecodings2(String s) {
+		/*
+		空间复杂度为 O(1) 的解法，用两个变量 a, b 来分别表示 s[i-1] 和 s[i-2] 的解码方法，然后从 i=1 开始遍历，也就是字符串的第二个字符，
+		判断如果当前字符为 '0'，说明当前字符不能单独拆分出来，只能和前一个字符一起，先将 a 赋为0，然后看前面的字符，
+		如果前面的字符是1或者2时，就可以更新 a = a + b，然后 b = a - b，其实 b 赋值为之前的 a，如果不满足这些条件的话，那么 b = a
+		 */
 		if(s.length() == 0 || s == null || s.charAt(0) == '0') {
 			return 0;
 		}
@@ -63,7 +68,8 @@ public class n091_Decode_Ways {
 				c1 = 0;
 			}
 
-			if(s.charAt(i-1) == '1' || s.charAt(i-1) == '2' && s.charAt(i) <= '6') {
+			//1.handle 1x e.g.12 or 13 2.handle double digit mapping only 11-26
+			if(s.charAt(i-1) == '1' || s.charAt(i-1) == '2' && s.charAt(i) <= '6') {	
 				c1 = c1 + c2;
 				c2 = c1 - c2;
 			} else {
@@ -76,10 +82,13 @@ public class n091_Decode_Ways {
 
 	public static void main(String[] args) {
 		n091_Decode_Ways obj = new n091_Decode_Ways();
-		System.out.println("12: "+obj.numDecodings("12"));
-		System.out.println("01: "+obj.numDecodings("01"));
-		System.out.println("226: "+obj.numDecodings("226"));
+//		System.out.println("12: "+obj.numDecodings("12"));
+//		System.out.println("01: "+obj.numDecodings("01"));
+//		System.out.println("226: "+obj.numDecodings("226"));
 
+		System.out.println("123: "+obj.numDecodings2("123"));	//(1,2,3)(12,3)(1,23)
+		System.out.println("212: "+obj.numDecodings2("212"));	//(2,1,2)(21,2)(2,12)
+		System.out.println("211: "+obj.numDecodings2("211"));
 		System.out.println("12: "+obj.numDecodings2("12"));
 		System.out.println("01: "+obj.numDecodings2("01"));
 		System.out.println("226: "+obj.numDecodings2("226"));
