c10c40c398ab5aff5ec1c5f6740f79cbc318b905
C3vin <spacemonkey3@gmail.com>
2021-04-22 11:29:33 -0700

    update LC 349

--- src/LeetCode/n349_Intersection_of_Two_Arrays.java
+++ src/LeetCode/n349_Intersection_of_Two_Arrays.java
@@ -8,16 +8,19 @@ import java.util.Set;
 
 //Given nums1 = [1,2,2,1,3,4,5,5,6], nums2 = [2, 2, 6], return [2, 6].
 public class n349_Intersection_of_Two_Arrays {
-	//HashSet
+	//Good 2 HashSet
 	public int[] intersection(int[] nums1, int[] nums2) {
 		Set<Integer> s1 = new HashSet<Integer>();
+		Set<Integer> s2 = new HashSet<Integer>();
+		
 		for(int i : nums1) {
 			s1.add(i);
 		}
-		Set<Integer> s2 = new HashSet<Integer>();
+		
 		for(int j : nums2) {
-			if(s1.contains(j))
+			if(s1.contains(j)) {
 				s2.add(j);
+			}
 		}
 
 		int[] res = new int[s2.size()];
@@ -26,8 +29,8 @@ public class n349_Intersection_of_Two_Arrays {
 			res[k++] = n;
 		} 
 			
-		for(int p=0; p<res.length; p++) 
-			System.out.println(res[p]);
+//		for(int p=0; p<res.length; p++) 
+//			System.out.println(res[p]);
 		return res;					//res[0]=2, res[1]=6
 	}
 	
