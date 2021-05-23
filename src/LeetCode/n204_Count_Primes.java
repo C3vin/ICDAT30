5ef17e574a5f91c3fa7c9a86e1ef6488fae9baa5
C3vin <spacemonkey3@gmail.com>
2021-05-22 19:44:04 -0700

    update LC204

--- src/LeetCode/n204_Count_Primes.java
+++ src/LeetCode/n204_Count_Primes.java
@@ -3,6 +3,33 @@ package LeetCode;
 import java.util.Arrays;
 
 public class n204_Count_Primes {
+	public int countPrimes2(int n) {
+		if(n <= 2) {
+			return 0;
+		}
+		
+		boolean[] prime = new boolean[n];
+		Arrays.fill(prime, true);
+		
+		int res = n - 2;	//not count 1 and n itself
+		
+		for(int i=2; i*i<n; i++) {
+			if(!prime[i*i]) {
+				continue;
+			}
+			
+			for(int j=i; j*i<n; j++) {
+				if(prime[j*i]) {
+					prime[j*i] = false;
+					res--;
+				}
+			}
+		}
+		
+		return res;
+	}
+	
+	
 	public int countPrimes(int n) {
 		//an integer to be prime it must be greater than 1
 		boolean[] prime = new boolean[n];
@@ -26,5 +53,8 @@ public class n204_Count_Primes {
 	public static void main(String[] args) {
 		n204_Count_Primes obj = new n204_Count_Primes();
 		System.out.println(obj.countPrimes(100));
+		
+		System.out.println(obj.countPrimes2(10));
+		System.out.println(obj.countPrimes2(100));
 	}
 }
