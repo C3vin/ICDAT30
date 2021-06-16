41773a6b3fd6c30310bc28154a14fc7ace9c0bd7
C3vin <spacemonkey3@gmail.com>
2021-06-15 21:39:01 -0700

    update LC230

--- src/LeetCode/n230_Kth_Smallest_Element_in_a_BST.java
+++ src/LeetCode/n230_Kth_Smallest_Element_in_a_BST.java
@@ -1,5 +1,8 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
 
@@ -40,7 +43,30 @@ public class n230_Kth_Smallest_Element_in_a_BST {
 			this.right = right;
 		}
 	}
-
+	
+	//Good! in-order, also if ask find kth largest element sol inside
+	public int kthSmallest2(TreeNode root, int k) {
+        List<Integer> res = new ArrayList<Integer>();
+        
+        helper(root, res);
+        
+        return res.get(k-1); 
+	}
+	
+	//inorder [LC94]
+	private void helper(TreeNode root, List<Integer> res) {
+        if(root == null) {
+            return;
+        }
+        
+        helper(root.left, res);
+        
+        res.add(root.val);
+        
+        helper(root.right, res);
+    }
+	
+	
 	//global 
 	int res = 0;
 	int rank = 0;
@@ -78,6 +104,20 @@ public class n230_Kth_Smallest_Element_in_a_BST {
 		//p2.left = p4;
 		p2.right = p5;
 		
-		System.out.println(obj.kthSmallest(p1, 1));
+		//System.out.println(obj.kthSmallest(p1, 1));
+		System.out.println(obj.kthSmallest2(p1, 1));
+		
+		TreeNode a1 = new TreeNode(5);
+		TreeNode a2 = new TreeNode(3);
+		TreeNode a3 = new TreeNode(6);
+		TreeNode a4 = new TreeNode(2);
+		TreeNode a5 = new TreeNode(4);
+		TreeNode a6 = new TreeNode(1);
+		a1.left = a2;
+		a1.right = a3;
+		a2.left = a4;
+		a2.right = a5;
+		a4.left = a6;
+		System.out.println(obj.kthSmallest2(a1, 3));
 	}
 }
